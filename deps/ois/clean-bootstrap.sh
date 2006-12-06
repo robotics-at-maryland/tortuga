af81d8d295b23d1d646d05d3ffcdfcdf05210028
Joseph Lisee <jlisee@gmail.com>
2006-12-06 22:52:41 +0000

    Moved major dependencies from deps-build brank to dependecy trunk

--- deps/ois/clean-bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-rm -rf configure Makefile.in depcomp config.guess config.sub ltmain.sh \
-autom4te.cache missing install-sh src/Makefile.in includes/Makefile.in \
-includes/config.h.in demos/Makefile.in
-svn revert aclocal.m4
