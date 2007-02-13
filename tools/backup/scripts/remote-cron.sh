f0f99316da82a79d5f1807ba60e3c98cd95c09e1
Joseph Lisee <jlisee@gmail.com>
2007-02-13 04:42:10 +0000

    '''Tools''' update remove backup script to properly sync rdiff directories

--- tools/backup/scripts/remote-cron.sh
+++ tools/backup/scripts/remote-cron.sh
@@ -3,12 +3,13 @@ BACKUP_LOC=/var/backups/ram
 BACKUP_USER=rambackup
 BACKUP_TARGET=ram.umd.edu
 
-BACKUP_REMOTE_DIR=/var/backup/ram
+BACKUP_REMOTE_DIR=/var/backups/ram
 BACKUP_DIRS="svn trac"
 
 # Wait for the remove server to do its backup (15 minutes)
 sleep 900
 
 for i in $BACKUP_DIRS; do
+    rm -rf $BACKUP_LOC/$i/incremental/rdiff-backup-data
     rsync -ave ssh --delete $BACKUP_USER@$BACKUP_TARGET:$BACKUP_REMOTE_DIR/$i $BACKUP_LOC
 done
