10df1faec2a8ca5ad90f8449017ed28618cffc77
Joseph Lisee <jlisee@gmail.com>
2007-05-14 01:06:26 +0000

    '''wxOgre''' updating extension to reflect new method of choosing control

--- packages/extensions/wxogre/src/wxogre_util.cpp
+++ packages/extensions/wxogre/src/wxogre_util.cpp
@@ -11,7 +11,7 @@ extern "C"
      @param window
          Must be a pointer to a wxWindow or its subclass.
      @return
-        The window handle in its display:xid form. 
+        The window handle in its display:xid form.
    */
   char* get_window_handle_str(void* window);
 }
@@ -26,9 +26,12 @@ extern "C"
 
 // Platform specific includes
 #ifdef __WXGTK__
-#include <wx/glcanvas.h>
+// Needed for random GTK/GDK macros and functions
 #include <gdk/gdkx.h>
 #include <gtk/gtk.h>
+
+// Needed for crazy GTK_PIZZA stuff
+#include <wx/gtk/win_gtk.h>
 #endif
 
 char* get_window_handle_str(void* _window)
@@ -43,30 +46,39 @@ char* get_window_handle_str(void* _window)
   {
     std::string handle;
 #ifdef __WXGTK__
+    // Should help reduce flickering
+    //SetBackgroundStyle(wxBG_STYLE_CUSTOM);
     std::stringstream handleStream;
-    GtkWidget* widget =  window->GetHandle();
-  
+
+    // wxWidgets uses serverl internal GtkWidgets, the GetHandle method returns
+    // a different one then this, but wxWidgets GLCanvas uses this one to
+    // interact with GLX with, so we will do the same.
+    GtkWidget* private_handle =  window->m_wxwindow;
+
+    // Prevents flicker
+    gtk_widget_set_double_buffered( private_handle, FALSE );
+
+    // Grabs the window for use in the below macros
+    GdkWindow *window = GTK_PIZZA(private_handle)->bin_window;
+    Display* display = GDK_WINDOW_XDISPLAY(window);
+    Window wid = GDK_WINDOW_XWINDOW(window);
+
     // Display
-    Display* display = GDK_WINDOW_XDISPLAY( widget->window );
     handleStream << (unsigned long)display << ':';
-/*  
+
+/*#if OGRE_PATCH_VERSION == 0
     // Screen (returns ":display.screen   ")
     std::string screenStr = DisplayString(display);
     int dotpos = screenStr.find(".", 0);
     screenStr = screenStr.substr(dotpos + 1, screenStr.length() - dotpos);
     handleStream << screenStr << ':';
+#endif
 */
     // XID (typedef of an unsigned int)
-    Window wid = GDK_WINDOW_XWINDOW( widget->window );
     handleStream << wid;
-
-    // Now lets get our XVisualInfo
-/*    void* vi = wxGLCanvas::ChooseGLVisual(NULL);
-    handleStream << (unsigned long)vi;*/
-    
     handle = handleStream.str();
 #else
-#error External/Parent Window handle not supported on this platform
+    #error External/Parent Window handle not supported on this platform
 #endif
 
     // Allocate string for the return
