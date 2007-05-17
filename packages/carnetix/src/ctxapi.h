2e0a2b55a7632d97125c214374fecf70a02130b1
Steve Mo <stevenm86@gmail.com>
2007-05-17 03:29:27 +0000

    '''Carnetix''' Add some documentation, preparing for release.

--- packages/carnetix/src/ctxapi.h
+++ packages/carnetix/src/ctxapi.h
@@ -1,22 +1,48 @@
+/*****************************************************************************
+ * Steve Moskovchenko  (aka evil wombat)                                     *
+ * University of Maryland                                                    *
+ * stevenm86 at gmail dot com                                                *
+ *                                                                           *
+ * Linux and Mac OS API for the Carnetix P2140 power supply.                 *
+ * The program uses libusb, which works on Mac OS X, Linux, and Windows.     *
+ *                                                                           *
+ * Released under the BSD License.                                           *
+ *                                                                           *
+ *                                                                           *
+ * If you get compile errors about missing libusb symbols, make sure you     *
+ * have -lusb in your compile flags.                                         *
+ *                                                                           *
+ * Enjoy!                                                                    *
+ *                                                                           *
+ ****************************************************************************/
+
+
+ /* Structure that holds the status and voltage/current readings
+  * Counter and Fault Code are missing as of right now, since I do
+  * not know how they are sent back.
+  */
 struct ctxValues
 {
-    double battVoltage;
-    double battCurrent;
+    double battVoltage;    /* In Volts */
+    double battCurrent;    /* In Amps  */
 
-    double priVoltage;
-    double priCurrent;
+    double priVoltage;     /* In Volts */
+    double priCurrent;     /* In Amps  */
 
-    double secVoltage;
-    double secCurrent;
+    double secVoltage;     /* In Volts */
+    double secCurrent;     /* In Amps  */
 
-    double temperature;
+    double temperature;    /* In Degrees C */
 
-    unsigned int ledValue; /* See Below */
-    unsigned int state;
+    unsigned int ledValue; /* See defines below */
+    unsigned int state;    /* See array below */
 
 };
 
-/* Since LED LSB and MSB are combined into one value, these are combined as well */
+/*
+ * These LED names came out of the C# API provided by Carnetix
+ * Since LED LSB and MSB are combined into one value, these are combined as well
+ */
 #define LED_IGNITION    0x0001
 #define LED_PULSESTART  0x0002
 #define LED_FANFAULT    0x0004
@@ -37,16 +63,19 @@ struct ctxValues
 
 
 
-/* Jumper Names */
-/* From the P2140 C# API Manual:
- Bit 0 = Secondary Output ON Control (0=Follow IGN, 1=Follow DLYON)
- Bit 1 = Secondary Output OFF Control (0=Follow IGN, 1=Follow SDLO)
- Bit 2 = not used
- Bit 3 = Ignore Fan Fault (0=do not ingore, 1 = ignore)
- Bit 4 = Shutdown Control Signal (0=No not use USB shutdown control, 1=Use USB shutdown control)
- Bit 5 = not used
- Bit 6 = Shutdown Mode msb (modes: 0x00=hibernate, 0x01=standby, 0x02=laptop)
- Bit 7 = Shutdown Mode lsb (modes: 0x00=hibernate, 0x01=standby, 0x02=laptop)
+/*
+ * Jumper Names
+ * From the P2140 C# API Manual
+ * Bit 0 = Secondary Output ON Control (0=Follow IGN, 1=Follow DLYON)
+ * Bit 1 = Secondary Output OFF Control (0=Follow IGN, 1=Follow SDLO)
+ * Bit 2 = not used
+ * Bit 3 = Ignore Fan Fault (0=do not ingore, 1 = ignore)
+ * Bit 4 = Shutdown Control Signal (0=No not use USB shutdown control, 1=Use USB shutdown control)
+ * Bit 5 = not used
+ * Bit 6 = Shutdown Mode msb (modes: 0x00=hibernate, 0x01=standby, 0x02=laptop)
+ * Bit 7 = Shutdown Mode lsb (modes: 0x00=hibernate, 0x01=standby, 0x02=laptop)
+ *
+ * These are also defined below.
  */
 #define JUMPER_SEC_OUT_ON_FOLLOW_DLYON     0x01
 #define JUMPER_SEC_OUT_ON_FOLLOW_IGN       0x00
@@ -62,13 +91,19 @@ struct ctxValues
 #define JUMPER_SHUTDOWN_MODE_STANDBY       0x40
 #define JUMPER_SHUTDOWN_MODE_LAPTOP        0x80
 
-
+/* Names of the states for the state field. These also came from the C# API */
 static const char * ctxStateNames[] =
 {
     "Idle", "Power PSU", "Power PC", "Bootup Lockout", "Run PC", "Shutdown Delay",
     "ACPI Pulse", "Shutdown Lockout", "Standby/Sleep", "Forced Shutdown", "Initialization"
 };
 
+
+/* Structure to store the supply parameters (configuration).
+ * These are pretty much the same parameters as displayed by PSUmoni
+ * This structure can be used for reading and writing parameters.
+ * But, currently they are NOT range checked, so use at your own risk.
+ */
 struct ctxParams
 {
     double sd_dly;   /* in seconds */
@@ -84,15 +119,70 @@ struct ctxParams
     double lowTemp;     /* in degrees C */
 };
 
+
+/*
+ * Opens the device and returns a device handle. If the supply was not found
+ * or somehow a connection could not be made, returns NULL.
+ *
+ * the device type is usb_dev_handle* and is provided in usb.h from libusb
+ */
 usb_dev_handle * ctxInit();
+
+/*
+ * Fills the passed-in string buffer with a string representation of the
+ * firmware version. Len is the length of the buffer passed in. The buffer
+ * length must be at least 12 bytes, or nothing will happen and an error
+ * will be returned. Returns 0 on success and -1 on error.
+ *
+ * hDev is the device handle, buf is a pointer to where the version is to
+ * be written, and len is the length of the buffer
+ */
 int ctxGetFWVersion(usb_dev_handle * hDev, char * buf, int len);
+
+
+/*
+ * Polls the supply and fills the passed-in structure with the readings
+ * from the supply. Returns 0 on success or -1 on error.
+ *
+ * hDev is the device handle. val is a pointer to the ctxValues structure
+ * where the values are to be written.
+ */
 int ctxReadValues(usb_dev_handle * hDev, struct ctxValues * val);
+
+/*
+ * Polls the supply and fills the passed-in structure with the supply's
+ * configuration parameters. Returns 0 on success or -1 on error.
+ *
+ * hDev is the device handle. prm is a pointer to the ctxParams structure
+ * where the parameters are to be written.
+ */
 int ctxReadParams(usb_dev_handle * hDev, struct ctxParams * prm);
+
+
+/*
+ * Writes the given parameters to the power supply. Returns 0 on success
+ * and -1 on failure. The parameters are NOT range checked yet, so the values
+ * must be within the proper range (I'm not sure what that is.. maybe the
+ * C# API gives the ranges).
+ *
+ * Note that all the parameters are written. So if you need to change just
+ * one parameter, you probably should read the parameters into a ctxParams
+ * structure first, change the values you want, and write the structure back.
+ *
+ * hDev is the device handle. prm is a pointer to the structure containing
+ * the parameters that are to be sent.
+ */
 int ctxWriteParams(usb_dev_handle * hDev, struct ctxParams * prm);
+
+
 int ctxPriOn(usb_dev_handle * hDev);
 int ctxPriOff(usb_dev_handle * hDev);
 int ctxSecOn(usb_dev_handle * hDev);
 int ctxSecOff(usb_dev_handle * hDev);
 int ctxP5VOn(usb_dev_handle * hDev);
 int ctxP5VOff(usb_dev_handle * hDev);
+
+/*
+ * Closes the given device handle, disconnecting from the power supply.
+ */
 void ctxClose(usb_dev_handle * hDev);
