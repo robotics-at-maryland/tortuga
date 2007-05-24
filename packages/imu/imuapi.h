9cccbe3476aff855e764b2439724f114885464b0
Steve Mo <stevenm86@gmail.com>
2007-05-24 03:47:43 +0000

    '''IMU''' Create structure. Write code tomorrow.

--- packages/imu/imuapi.h
+++ packages/imu/imuapi.h
@@ -2,6 +2,20 @@
 
 struct imuMeasurements
 {
-    int blah;
-
+    int devID;
+    int messageID;
+    int sampleTimer;
+    int gyroX;
+    int gyroY;
+    int gyroZ;
+    int accelX;
+    int accelY;
+    int accelZ;
+    int magX;
+    int magY;
+    int magZ;
+    int tempX;
+    int tempY;
+    int tempZ;
+    int checksumValid;
 };
