a3623a943608238a485090ce70d939e06eeb4dd8
Steve Mo <stevenm86@gmail.com>
2007-05-31 04:40:21 +0000

    '''IMU''' FIRST ATTEMPT at port initialization.

--- packages/imu/imuapi.c
+++ packages/imu/imuapi.c
@@ -6,7 +6,17 @@
 #include <sys/stat.h>
 #include <sys/ioctl.h>
 #include <errno.h>
+#include <termios.h>
+#include <unistd.h>
+#include <string.h>
+#include <linux/serial.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <bits/types.h>
+//#include <bits/termios.h>
 #include "imuapi.h"
+
 
 unsigned char waitByte(int fd)
 {
@@ -18,13 +28,16 @@ unsigned char waitByte(int fd)
 int waitSync(int fd)
 {
     int fs=0;
+    int crap=0;
     while(fs != 4)
     {
         if(waitByte(fd) == 0xFF)
             fs++;
         else
             fs=0;
+        crap++;
     }
+    printf("%d  ", crap);
 }
 
 int convert16(unsigned char msb, unsigned char lsb)
@@ -74,7 +87,7 @@ int readIMUData(int fd, struct imuMeasurements * imu)
 
     imu->checksumValid = (imuData[33] == (sum&0xFF));
 
-
+/*
     imu->angleMagX=(atan2(imu->magY, imu->magX)*(180.0/M_PI));
     imu->angleMagY=(atan2(imu->magZ, imu->magY)*(180.0/M_PI));
     imu->angleMagZ=(atan2(imu->magX, imu->magZ)*(180.0/M_PI));
@@ -82,14 +95,100 @@ int readIMUData(int fd, struct imuMeasurements * imu)
     imu->angleAccX=(atan2(imu->accelY, imu->accelX)*(180.0/M_PI));
     imu->angleAccY=(atan2(imu->accelZ, imu->accelY)*(180.0/M_PI));
     imu->angleAccZ=(atan2(imu->accelX, imu->accelZ)*(180.0/M_PI));
+*/
 
     return imu->checksumValid;
 }
 
-
+/*
 int openIMU(const char * devName)
 {
-    int fd = open("/dev/ttyUSB0", O_RDWR);
+    int fd = open(devName, O_RDWR, O_ASYNC); // | O_ASYNC); //, O_RDWR, O_NONBLOCK);
+
+    if(fd == -1)
+        return -1;
+
+    printf("FD=%d\n", fd);
+
+    struct termios info;
+
+    memset((void *)&info, 0, sizeof(struct termios));      // clear all
+    tcgetattr(fd, &info);
+    cfsetispeed(&info, B115200); //SPEED_DATA[speed].cflag);
+    cfsetospeed(&info, B115200); //SPEED_DATA[speed].cflag);
+ //   info.c_cflag &= ~(CSIZE|PARENB|PARODD|HUPCL|CSTOPB);
+  //  info.c_cflag = 0;
+ //   info.c_iflag &= ~(ISTRIP|INPCK|IGNCR|INLCR|ICRNL|IXOFF|IXON);
+ //   info.c_iflag |= iflag;
+  //  info.c_oflag &= ~OPOST;
+  //  info.c_lflag &= ~(ICANON|ECHO|ECHONL|ISIG|IEXTEN|TOSTOP);
+ //   info.c_cc[VMIN]  = 0;               // wait for 1 char or timeout
+    //info.c_cc[VTIME] = readTimeout/100; // wait for in deciseconds
+
+
+   // info.c_ispeed = B115200;
+  //  info.c_ospeed = B115200;
+    printf("\nReturn is %d\n", tcsetattr(fd, TCSANOW, &info));
+    return fd;
+}*/
+
+
+
+/* Some code from cutecom, which in turn may have come from minicom */
+int openIMU(const char * devName)
+{
+   int fd = open(devName, O_RDWR, O_ASYNC); // | O_ASYNC); //, O_RDWR, O_NONBLOCK);
+
+    if(fd == -1)
+        return -1;
+
+    printf("FD=%d\n", fd);
+    struct termios newtio;
+    if (tcgetattr(fd, &newtio)!=0)
+        printf("\nFirst stuff failed\n");
+
+    unsigned int _baud=B115200;
+    cfsetospeed(&newtio, _baud);
+    cfsetispeed(&newtio, _baud);
+
+
+    newtio.c_cflag = (newtio.c_cflag & ~CSIZE) | CS8;
+    newtio.c_cflag |= CLOCAL | CREAD;
+    newtio.c_cflag &= ~(PARENB | PARODD);
+
+    newtio.c_cflag &= ~CRTSCTS;
+    newtio.c_cflag &= ~CSTOPB;
+
+    newtio.c_iflag=IGNBRK;
+    newtio.c_iflag &= ~(IXON|IXOFF|IXANY);
+
+    newtio.c_lflag=0;
+
+    newtio.c_oflag=0;
+
+
+    newtio.c_cc[VTIME]=1;
+    newtio.c_cc[VMIN]=60;
+
+//   tcflush(m_fd, TCIFLUSH);
+    if (tcsetattr(fd, TCSANOW, &newtio)!=0)
+        printf("tsetaddr1 failed!\n");
+
+    int mcs=0;
+
+    ioctl(fd, TIOCMGET, &mcs);
+    mcs |= TIOCM_RTS;
+    ioctl(fd, TIOCMSET, &mcs);
+
+    if (tcgetattr(fd, &newtio)!=0)
+        printf("tcgetattr() 4 failed\n");
+
+    newtio.c_cflag &= ~CRTSCTS;
+
+    if (tcsetattr(fd, TCSANOW, &newtio)!=0)
+      printf("tcsetattr() 2 failed\n");
+
+
 
     return fd;
 }
--- packages/imu/imutest.c
+++ packages/imu/imutest.c
@@ -13,16 +13,22 @@ int main(int argc, char ** argv)
 {
     int fd = openIMU("/dev/ttyUSB0");
 
+    if(fd == -1)
+    {
+        printf("Could not find device!\n");
+        exit(1);
+    }
+
     struct imuMeasurements imuData;
 
     while(1)
     {
 
         readIMUData(fd, &imuData);
-        //printf("%9f %9f %9f %11f %11f %11f  %11f %11f %11f  \n", imuData.accelX, imuData.accelY, imuData.accelZ, imuData.gyroX, imuData.gyroY, imuData.gyroZ, imuData.magX, imuData.magY, imuData.magZ);
+        printf("%9f %9f %9f %11f %11f %11f  %11f %11f %11f  \n", imuData.accelX, imuData.accelY, imuData.accelZ, imuData.gyroX, imuData.gyroY, imuData.gyroZ, imuData.magX, imuData.magY, imuData.magZ);
 
 
-        printf("%11f %11f %11f  %11f %11f %11f  \n", imuData.angleMagX, imuData.angleMagY, imuData.angleMagZ, imuData.angleAccX, imuData.angleAccY, imuData.angleAccZ);
+        //printf("%11f %11f %11f  %11f %11f %11f  \n", imuData.angleMagX, imuData.angleMagY, imuData.angleMagZ, imuData.angleAccX, imuData.angleAccY, imuData.angleAccZ);
 //        printf("%11f %11f %11f\n", imuData.gyroX, imuData.gyroY, imuData.gyroZ);
 //        printf("%9f\n", imuData.gyroX);
     }
