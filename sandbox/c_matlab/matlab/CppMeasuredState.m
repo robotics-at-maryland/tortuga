9b2146f324a46b44fc97d19fb917314bc639b56c
Joseph Lisee <jlisee@gmail.com>
2007-06-29 01:50:07 +0000

    '''Controller''' got desired and measured state working with C++, manually packing static arrays is ugly

---
+++ sandbox/c_matlab/matlab/CppMeasuredState.m
@@ -0,0 +1,21 @@
+function convertedMeasuredState = CppMeasuredState(MeasuredState, too_from)
+
+% We are converting from Matlab to CPP format
+if too_from == 1
+    a = MeasuredState.linearAcceleration;
+    q = MeasuredState.quaternion;
+    w = MeasuredState.angularRate;
+
+    % Convert static array to individual numbers (HACK!)
+    convertedMeasuredState = struct('depth',MeasuredState.depth, ...
+        'a0', a(1), 'a1', a(2), 'a2', a(3), ...
+        'q0', q(1), 'q1', q(2), 'q2', q(3), 'q3', q(4),  ...
+        'w0', w(1), 'w1', w(2), 'w2', w(3));
+    
+else
+    % Converting from CPP to Matlab
+    convertedMeasuredState = struct('depth', MeasuredState.depth, ...
+        'linearAcceleration', [MeasuredState.a0 MeasuredState.a1 DesiredState.a2]',...
+        'quaternion', [MeasuredState.q0 MeasuredState.q1 MeasuredState.q2 MeasuredState.q3]',...
+        'angularRate',[MeasuredState.w0 MeasuredState.w1 MeasuredState.w2]');
+end
--- sandbox/c_matlab/matlab/test.m
+++ sandbox/c_matlab/matlab/test.m
@@ -10,14 +10,25 @@ testStructPtr = libpointer('TestStruct', testStruct);
 % Call library and grab results
 calllib('C_Matlab', 'testStruct', testStructPtr);
 
-get(testStructPtr, 'Value')
+teststruct = get(testStructPtr, 'Value')
 
 % Test Desired State
 DesiredState = struct('speed',1,'depth',1,'quaternion',[1 2 3 4]', ...
                       'angularRate',[1 2 3]');
                       
-desiredState = M2CppDesiredState(DesiredState);
+desiredState = CppDesiredState(DesiredState,1);
 desiredStatePtr = libpointer('DesiredState', desiredState);
 calllib('C_Matlab', 'testDesired', desiredStatePtr);
 
-get(desiredStatePtr, 'Value')
+desired = get(desiredStatePtr, 'Value')
+
+
+% Test Measured State
+MeasuredState = struct('depth',1,'linearAcceleration',[1 2 3]', ...
+    'quaternion',[1 2 3 4]','angularRate',[1 2 3]');
+                      
+measuredState = CppMeasuredState(MeasuredState,1);
+measuredStatePtr = libpointer('MeasuredState', measuredState);
+calllib('C_Matlab', 'testMeasured', measuredStatePtr);
+
+measured = get(measuredStatePtr, 'Value')
