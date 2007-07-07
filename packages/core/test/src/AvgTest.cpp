725c6288a48d109a41b6703264e9a4dfe76b5c68
Joseph Lisee <jlisee@gmail.com>
2007-07-07 01:17:37 +0000

    '''Core''' fixed broken demo

--- packages/core/test/src/AvgTest.cpp
+++ packages/core/test/src/AvgTest.cpp
@@ -4,14 +4,14 @@ using namespace std;
 using namespace ram::core;
 int main()
 {
-	int total = 0;
-	AveragingQueue* q=new AveragingQueue(10);
-	for (int i=1;i<25;i++)
-	{
-		total += i;
-		cout<<"adding:"<<i<<endl;
-		q->addValue(i);
-		cout<<"Average (Total: " << "):"<<q->getAverage()<<endl;
-	}
-	return 0;
+    int total = 0;
+    AveragingQueue* q=new AveragingQueue(10);
+    for (int i=1;i<25;i++)
+    {
+        total += i;
+        cout<<"adding:"<<i<<endl;
+        q->addValue(i);
+        cout<<"Average (Total: " << "):"<<q->getAverage()<<endl;
+    }
+    return 0;
 }
