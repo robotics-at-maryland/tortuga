da1a0f49e0ab635fb8d304a202e5fa91333e708e
Joseph Lisee <jlisee@gmail.com>
2007-07-07 21:19:44 +0000

    '''Control''' created controller interface

---
+++ packages/control/include/IController.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2007 Robotics at Maryland
+ * Copyright (C) 2007 Joseph Lisee <jlisee@umd.edu>
+ * All rights reserved.
+ *
+ * Author: Joseph Lisee <jlisee@umd.edu>
+ * File:  packages/control/include/Controller.h
+ */
+
+#ifndef RAM_CONTROL_ICONTROL_07_03_2007
+#define RAM_CONTROL_ICONTROL_07_03_2007
+
+// Project Includes
+#include "vehicle/include/Common.h"
+#include "pattern/include/Subject.h"
+
+namespace ram {
+namespace control {
+
+class IController : public pattern::Subject
+{
+public:
+    enum UPDATE_EVENTS {
+        SPEED_UPDATE,
+        HEADING_UPDATE,
+        DEPTH_UPDATE
+    };
+    
+    /** Set the current speed, clamped between -5 and 5 */
+    virtual void setSpeed(int speed) = 0;
+
+    /** Sets the current heading in degrees off north */
+    virtual void setHeading(double degrees) = 0;
+
+    /** Sets the current depth of the sub in meters */
+    virtual void setDepth(double depth) = 0;
+
+    /** Set the current speed, clamped between -5 and 5 */
+    virtual int getSpeed() = 0;
+
+    /** Sets the current heading in degrees off north */
+    virtual double getHeading() = 0;
+
+    /** Sets the current depth of the sub in meters */
+    virtual double getDepth() = 0;    
+};
+    
+} // namespace control
+} // namespace ram
+
+#endif // RAM_CONTROL_ICONTROL_07_03_2007
