478f71fc4509cf34e3f5dc32ff7d47678cfa214e
matbak87 <software@ram.umd.edu>
2007-07-13 09:22:06 +0000

    sleeping scheudler has been updated and ready for action

--- packages/python/scheduler.py
+++ packages/python/scheduler.py
@@ -22,3 +22,36 @@ class scheduler:
                     #print "Sytem Time:",currTime - self.startTime
                     element.object.update(currTime - lastUpdated)
                     element.lastUpdated = currTime
+                    
+    def nextUp(self):
+        currTime = time.time()
+        waits = []
+        for element in self.objects:
+            lastUpdated = element.lastUpdated
+            updateInterval = element.updateInterval
+            timeTill = updateInterval - (currTime - lastUpdated)
+            waitInfo = (element,timeTill)
+            waits.append(waitInfo)
+        minWait = 9999
+        minElement = None
+        for element in waits:
+            obj = element[0]
+            t = element[1]
+            if t < minWait:
+                minWait = t
+                minElement = obj
+        return (minElement,minWait)
+    
+    def complexOperate(self):
+        while True:
+            next = self.nextUp()
+            element = next[0]
+            sleepTime = next[1]
+            if sleepTime < 0:
+                time.sleep(0)
+            else:
+                time.sleep(sleepTime)
+            currTime = time.time()
+            print "Sytem Time:",currTime - self.startTime
+            element.object.update(currTime - element.lastUpdated)
+            element.lastUpdated = currTime
