19df2597acfee36058db0a89cab482b6517cd31c
Leo Singer <aronnax@umd.edu>
2007-07-13 19:08:06 +0000

    Added shell script to send a batch of packets

---
+++ sandbox/jaus/send_test_suite.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+./send_packet packets/064.dat
+./send_packet packets/066.dat
+./send_packet packets/108.dat
+./send_packet packets/110.dat
+./send_packet packets/126.dat
+./send_packet packets/128.dat
+./send_packet packets/133.dat
+./send_packet packets/135.dat
+./send_packet packets/148.dat
+./send_packet packets/150.dat
+./send_packet packets/157.dat
+./send_packet packets/161.dat
+./send_packet packets/165.dat
+./send_packet packets/167.dat
+./send_packet packets/169.dat
+./send_packet packets/171.dat
