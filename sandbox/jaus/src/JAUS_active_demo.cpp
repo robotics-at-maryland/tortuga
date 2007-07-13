27728d62858ebd0043505b741477f8980fa32f81
Daniel Hakim <dhakimdahakim@gmail.com>
2007-07-13 22:05:30 +0000

    Added files for demo that activates thrusters

---
+++ sandbox/jaus/src/JAUS_active_demo.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+#include "JAUS_helpers.h"
+
+#define VEHICLE_SUBSYSTEM_ID 100
+
+#define RECEIVE_JAUS_SOCKET_ERROR -1
+#define RECEIVE_JAUS_BIND_ERROR -2
+#define RECEIVE_JAUS_RECVFROM_ERROR -3
+#define RECEIVE_JAUS_IGNORE 0
+#define RECEIVE_JAUS_RESUME 1
+#define RECEIVE_JAUS_STANDBY 2
+
+int main (int argc, char * const argv[]) {
+    int result = RECEIVE_JAUS_IGNORE;
+    while (true) {
+        result = receiveJAUS(VEHICLE_SUBSYSTEM_ID);
+        switch (result) {
+            case RECEIVE_JAUS_SOCKET_ERROR:
+                std::cout << "socket error" << std::endl;
+                break;
+            case RECEIVE_JAUS_BIND_ERROR:
+                std::cout << "bind error" << std::endl;
+                break;
+            case RECEIVE_JAUS_RECVFROM_ERROR:
+                std::cout << "recvfrom error" << std::endl;
+                break;
+            case RECEIVE_JAUS_IGNORE:
+                std::cout << "ignore packet" << std::endl;
+                break;
+            case RECEIVE_JAUS_RESUME:
+                std::cout << "resume" << std::endl;
+				//	thrusters GO!
+                break;
+            case RECEIVE_JAUS_STANDBY:
+                std::cout << "standby" << std::endl;
+				// thrusters ALL STOP!
+                break;
+        }
+    }
+    return 0;
+}
--- sandbox/jaus/src/JAUS_helpers.cpp
+++ sandbox/jaus/src/JAUS_helpers.cpp
@@ -57,8 +57,9 @@ int receiveJAUS(int vehicle_ip_last_octet) {
     std::string buf_string(buffer, rc);
     
     JAUS_Message mesg(buf_string);
-    
-    int ocu_ip_last_octet = (*((int*)(&clientaddr.sin_addr)) & 0xFF000000) >> 24;
+	
+    int ocu_ip_last_octet = (ntohl(*((int*)(&clientaddr.sin_addr))) & 0xFF000000) >> 24;
+	std::cout << "OCU IP Last Octet:" << ocu_ip_last_octet << std::endl;
 
     if (isPacketForUs(mesg, ocu_ip_last_octet, vehicle_ip_last_octet)) {
         if (mesg.field_CommandCode == JAUS_CMD_RESUME) {
