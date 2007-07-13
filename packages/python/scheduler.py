aad0cac88936c0161a376e731ff9f8c1f0c761a5
matbak87 <software@ram.umd.edu>
2007-07-13 08:51:34 +0000

    '''Python''' added single threaded busy loop based scheduler

---
+++ packages/python/scheduler.py
@@ -0,0 +1,38 @@
+#list of objects that all have an update method that takes a time step
+
+from simpleUpdatable import updatable
+import time
+
+class updatableInformation:
+    def __init__(self,object,updateInterval):
+        self.object = object
+        self.updateInterval = updateInterval
+        self.lastUpdated = time.time()
+                
+class scheduler:
+    def __init__(self,objects):
+        self.objects = objects
+        self.startTime = time.time()
+        
+    def operate(self):
+        while True:
+            currTime = time.time()
+            for element in self.objects:
+                lastUpdated = element.lastUpdated
+                if currTime - lastUpdated >= element.updateInterval:
+                    print "Sytem Time:",currTime - self.startTime
+                    element.object.update(currTime - lastUpdated)
+                    element.lastUpdated = currTime
+                    
+a = updatable("a")
+b = updatable("b")
+c = updatable("c")
+                
+aInfo = updatableInformation(a,1)
+bInfo = updatableInformation(b,4)
+cInfo = updatableInformation(c,7)
+
+objects = [aInfo,bInfo,cInfo]
+
+sched = scheduler(objects)
+sched.operate()
