41c73023910ac6d754652bb64319c9f4cc032630
Leo Singer <aronnax@umd.edu>
2007-07-14 00:57:14 +0000

    Put pause in test suite

--- sandbox/jaus/send_test_suite.sh
+++ sandbox/jaus/send_test_suite.sh
@@ -1,18 +1,33 @@
 #!/bin/sh
 
 ./send_packet 192.168.128.100 < packets/064.dat
+sleep 0.5
 ./send_packet 192.168.128.100 < packets/066.dat
+sleep 0.5
 ./send_packet 192.168.128.100 < packets/108.dat
+sleep 0.5
 ./send_packet 192.168.128.100 < packets/110.dat
+sleep 0.5
 ./send_packet 192.168.128.100 < packets/126.dat
+sleep 0.5
 ./send_packet 192.168.128.100 < packets/128.dat
+sleep 0.5
 ./send_packet 192.168.128.100 < packets/133.dat
+sleep 0.5
 ./send_packet 192.168.128.100 < packets/135.dat
+sleep 0.5
 ./send_packet 192.168.128.100 < packets/148.dat
+sleep 0.5
 ./send_packet 192.168.128.100 < packets/150.dat
+sleep 0.5
 ./send_packet 192.168.128.100 < packets/157.dat
+sleep 0.5
 ./send_packet 192.168.128.100 < packets/161.dat
+sleep 0.5
 ./send_packet 192.168.128.100 < packets/165.dat
+sleep 0.5
 ./send_packet 192.168.128.100 < packets/167.dat
+sleep 0.5
 ./send_packet 192.168.128.100 < packets/169.dat
+sleep 0.5
 ./send_packet 192.168.128.100 < packets/171.dat
