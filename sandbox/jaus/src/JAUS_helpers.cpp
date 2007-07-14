c282943ba2584dbd6f6abb2fc468c5d8bdb13f52
Leo Singer <aronnax@umd.edu>
2007-07-14 00:53:37 +0000

    Fixed Endianness problem

--- sandbox/jaus/src/JAUS_helpers.cpp
+++ sandbox/jaus/src/JAUS_helpers.cpp
@@ -58,8 +58,7 @@ int receiveJAUS(int vehicle_ip_last_octet) {
     
     JAUS_Message mesg(buf_string);
 	
-    int ocu_ip_last_octet = (ntohl(*((int*)(&clientaddr.sin_addr))) & 0xFF);
-	std::cout << "OCU IP Last Octet:" << ocu_ip_last_octet << std::endl;
+    int ocu_ip_last_octet = (ntohl(*((long*)(&clientaddr.sin_addr))) & 0xFF);
 
     if (isPacketForUs(mesg, ocu_ip_last_octet, vehicle_ip_last_octet)) {
         if (mesg.field_CommandCode == JAUS_CMD_RESUME) {
