f523414ff4f34d7e83acef1a9531119b55d674ad
Joseph Lisee <jlisee@gmail.com>
2007-07-14 05:55:13 +0000

    '''JAUS''' added simple low level thruster api demo, and integrated into build system

--- sandbox/jaus/src/JAUS_active_demo.cpp
+++ sandbox/jaus/src/JAUS_active_demo.cpp
@@ -4,9 +4,13 @@
  * and the judge's IP address MUST be 192.168.128.1.
  */
 
+// STD Includes
 #include <iostream>
+#include <cstdlib>
 
+// Project Includes
 #include "JAUS_helpers.h"
+#include "thrusterapi/include/thrusterapi.h"
 
 #define VEHICLE_SUBSYSTEM_ID 100
 
@@ -17,31 +21,54 @@
 #define RECEIVE_JAUS_RESUME 1
 #define RECEIVE_JAUS_STANDBY 2
 
+void setMotors(int fd, int value)
+{
+    for (int i = 0; i < 4; ++i)
+        setSpeed(fd, i, value);
+}
+
 int main (int argc, char * const argv[]) {
     int result = RECEIVE_JAUS_IGNORE;
+    int mcFD = openThrusters("/dev/motor");
+
+    if (mcFD < 0)
+    {
+        std::cerr << "Could not find motor controllers" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    
     while (true) {
         result = receiveJAUS(VEHICLE_SUBSYSTEM_ID);
         switch (result) {
             case RECEIVE_JAUS_SOCKET_ERROR:
                 std::cout << "socket error" << std::endl;
                 break;
+                
             case RECEIVE_JAUS_BIND_ERROR:
                 std::cout << "bind error" << std::endl;
                 break;
+                
             case RECEIVE_JAUS_RECVFROM_ERROR:
                 std::cout << "recvfrom error" << std::endl;
                 break;
+                
             case RECEIVE_JAUS_IGNORE:
                 std::cout << "ignore packet" << std::endl;
                 break;
+                
             case RECEIVE_JAUS_RESUME:
+            {
                 std::cout << "resume" << std::endl;
-				//	thrusters GO!
-                break;
+		setMotors(mcFD, 300);
+            }
+            break;
+            
             case RECEIVE_JAUS_STANDBY:
+            {
                 std::cout << "standby" << std::endl;
-				// thrusters ALL STOP!
-                break;
+		setMotors(mcFD, 0);
+            }
+            break;
         }
     }
     return 0;
