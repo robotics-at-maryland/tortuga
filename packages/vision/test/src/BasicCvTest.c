7e775b6ae38b8e0b88a463db312a5cbc41d4d246
Daniel Hakim <dhakimdahakim@gmail.com>
2007-08-29 17:58:50 +0000

    Optical Flow added, no real change to BasicCvTest

--- packages/vision/test/src/BasicCvTest.c
+++ packages/vision/test/src/BasicCvTest.c
@@ -19,30 +19,25 @@ int main() {
                                                 CV_CAP_PROP_FRAME_HEIGHT));
   printf("\tFPS:    %f\n", cvGetCaptureProperty(capture,
                                                CV_CAP_PROP_FPS));
-//  printf("\tGUID: %llx YAY\n", (long long)cvGetCaptureProperty(capture, CV_CAP_PROP_GUID));
-
-
   // Create a window in which the captured images will be presented
   cvNamedWindow( "Raw Camera Image", CV_WINDOW_AUTOSIZE );
-//  cvNamedWindow( "Processed", CV_WINDOW_AUTOSIZE );
+  // cvNamedWindow( "Processed", CV_WINDOW_AUTOSIZE );
 
   // Show the image captured from the camera in the window and repeat
-  while( 1 ) {
-    // Get one frame
-    IplImage* frame = cvQueryFrame( capture );
-    if( !frame ) {
-      fprintf( stderr, "ERROR: frame is null...\n" );
-      getchar();
-      break;
-    }
-//    cvSaveImage("testimage.png", frame);
-    
-    cvShowImage( "Raw Camera Image", frame );
-    // Do not release the frame!
-
-    //If ESC key pressed, Key=0x10001B under OpenCV 0.9.7(linux version),
-    //remove higher bits using AND operator
-    if( (cvWaitKey(10) & 255) == 27 ) break;
+  while(1) {
+		// Get one frame
+		IplImage* frame = cvQueryFrame( capture );
+		if( !frame ) 
+		{
+			fprintf( stderr, "ERROR: frame is null...\n" );
+			getchar();
+			break;
+		}
+		cvShowImage( "Raw Camera Image", frame );
+		//Do not release the frame!
+		//If ESC key pressed, Key=0x10001B under OpenCV 0.9.7(linux version),
+		//remove higher bits using AND operator
+		if( (cvWaitKey(10) & 255) == 27 ) break;
   }
 
   // Release the capture device housekeeping
