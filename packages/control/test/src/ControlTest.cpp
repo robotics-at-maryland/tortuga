02cc98bacbbba42c8132039536fd51dce2eae9a6
Steve Mo <stevenm86@gmail.com>
2007-09-14 16:29:37 +0000

    '''Control''' Hack in joystick.

--- packages/control/test/src/ControlTest.cpp
+++ packages/control/test/src/ControlTest.cpp
@@ -22,6 +22,24 @@
 #include "vehicle/include/device/Thruster.h"
 #include "vehicle/include/device/IMU.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+
+#define MYPORT 9219 /*YOUR PORT HERE*/
+                /* the port users will be connecting to */
+
+#define BACKLOG 10  /* how many pending connections queue will hold */
+
+
+
+
+
 using namespace std;
 using namespace ram;
 namespace po = boost::program_options;
@@ -99,6 +117,140 @@ int main(int argc, char** argv)
     vehicle.getDevice("StarboardThruster")->background(thrustUpdate);
     vehicle.getDevice("IMU")->background(imuUpdate);
     vehicle.background(vehicleUpdate);
+
+
+
+    int sockfd, new_fd;/*listen on sock_fd, new connection on new_fd */
+    struct sockaddr_in my_addr;    /*my address information */
+    struct sockaddr_in their_addr; /*connector's address information */
+    int sin_size;
+    int ret;
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+           perror("socket");
+           exit(1);
+    }
+
+    memset((void *)&my_addr,0,sizeof(my_addr)); /*zero*/
+    my_addr.sin_family = AF_INET;      /* host byte order */
+    my_addr.sin_port = htons(MYPORT);  /* short, network byte order */
+    my_addr.sin_addr.s_addr = INADDR_ANY; /* auto-fill with my IP */
+
+    ret = bind(sockfd,(struct sockaddr *)&my_addr,sizeof(struct sockaddr));
+    if (ret < 0)
+    {
+        perror("bind");
+        return ret;
+    }
+    ret = listen(sockfd, BACKLOG);
+    if (ret < 0)
+    {
+         perror("listen");
+         return ret;
+    }
+
+
+    while(1)
+    {  /* main accept() loop */
+        sin_size = sizeof(struct sockaddr_in);
+        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, (socklen_t *) &sin_size);
+        if (new_fd < 0) {
+               perror("accept");
+               continue;
+        }
+    
+        printf("server: got connection\n");
+        {
+		int fd = new_fd;        
+		//unsigned char buf[64];
+		unsigned char type = 0;
+	    	unsigned char num = 0;
+	        signed short val = 0;
+
+		while(1)
+		{
+			if(recv(fd, &type, 1, 0) != 1)
+				exit(1);
+
+			recv(fd, &num, 1, 0);
+			recv(fd, &val, 2, 0);   /* We're both Intel, so who cares about byte order */
+
+			printf("%d %d: %d\n", type, num, val);
+#define BTN_UP 2
+#define BTN_DOWN 3
+#define BTN_LEFT 0
+#define BTN_RIGHT 1
+
+#define BTN_SHALLOW 9
+#define BTN_DEEP 5
+
+#define MAX_DEPTH 4
+
+#define MIN_DEPTH -4
+
+#define DEPTH_ENC 1
+#define TURN_ENC 10
+#define SPEED_ENC 1
+#define MIN_SPEED -5
+#define MAX_SPEED 5
+
+			if(type == 1 && val == 1) // A button press
+			{
+				switch(num)	/* Which button? */
+				{
+					case BTN_UP:
+					{
+						if(controller.getSpeed() < MAX_SPEED)
+							controller.setSpeed(controller.getSpeed()+SPEED_ENC);
+						break;
+					}
+
+					case BTN_DOWN:
+					{
+                                                if(controller.getSpeed() > MIN_SPEED)
+							controller.setSpeed(controller.getSpeed()-SPEED_ENC);
+
+						break;
+					}
+
+					case BTN_LEFT:
+					{
+						controller.yawVehicle(-TURN_ENC);
+						break;
+					}
+
+
+                                       case BTN_RIGHT:
+                                       {
+	                                       controller.yawVehicle(TURN_ENC);
+	                                       break;
+	                               }
+
+
+                                       case BTN_SHALLOW:
+				       {
+					       if(controller.getDepth() < MAX_DEPTH)
+						       controller.setDepth(controller.getDepth()+DEPTH_ENC);
+						break;
+					}
+
+					case BTN_DEEP:
+                                       {
+				               if(controller.getDepth() < MAX_DEPTH)
+				                       controller.setDepth(controller.getDepth()+DEPTH_ENC);
+				               break;
+				       }
+
+				}
+			}
+		}
+	
+		exit(0);
+        }
+        close(new_fd);  /* parent doesn't need this */
+        while(waitpid(-1,NULL,WNOHANG) > 0); /*clean up child proc*/
+    }
+
+
 
     // Sleep for fifteen minutes
     sleep(15 * 60);
