6628811c85695cfd09205d6bc36f625afbca0f42
Steve Mo <stevenm86@gmail.com>
2007-10-12 02:51:46 +0000

    '''SensorAPI''' Change numbers to Defines (Joe), sync with board prior to sending commands (oops) and print out the result of safe/unsafe operations. Need to add command to print kill switch value, but current setup requires reflashing IC1 to accomplish this, since the kill pin moved from the power board header and to the GPIO header.

--- packages/sensorapi/lcdshow.c
+++ packages/sensorapi/lcdshow.c
@@ -22,22 +22,51 @@ int main(int argc, char ** argv)
 
     int fd = openSensorBoard("/dev/sensor");
 
+
+    if(fd == -1)
+    {
+        printf("\nCould not find device!\n");
+        close(fd);
+        return -1;
+    }
+
+    if(syncBoard(fd) != 0)
+    {
+        printf("\nCould not sync with board!\n");
+        close(fd);
+    }
+
     if(strcmp(argv[1], "-safe") == 0)
     {
-        thrusterSafety(fd, 0);
-        thrusterSafety(fd, 1);
-        thrusterSafety(fd, 2);
-        thrusterSafety(fd, 3);
+        if(thrusterSafety(fd, CMD_THRUSTER1_OFF) != SB_OK)
+            printf("Error safing thruster\n");
+
+        if(thrusterSafety(fd, CMD_THRUSTER2_OFF) != SB_OK)
+            printf("Error safing thruster\n");
+
+        if(thrusterSafety(fd, CMD_THRUSTER3_OFF) != SB_OK)
+            printf("Error safing thruster\n");
+
+        if(thrusterSafety(fd, CMD_THRUSTER4_OFF) != SB_OK)
+            printf("Error safing thruster\n");
+
         close(fd);
         return 0;
     }
 
     if(strcmp(argv[1], "-unsafe") == 0)
     {
-        thrusterSafety(fd, 4);
-        thrusterSafety(fd, 5);
-        thrusterSafety(fd, 6);
-        thrusterSafety(fd, 7);
+        if(thrusterSafety(fd, CMD_THRUSTER1_ON) != SB_OK)
+            printf("Error unsafing thruster\n");
+
+        if(thrusterSafety(fd, CMD_THRUSTER2_ON) != SB_OK)
+            printf("Error unsafing thruster\n");
+
+        if(thrusterSafety(fd, CMD_THRUSTER3_ON) != SB_OK)
+            printf("Error unsafing thruster\n");
+
+        if(thrusterSafety(fd, CMD_THRUSTER4_ON) != SB_OK)
+            printf("Error unsafing thruster\n");
         close(fd);
         return 0;
     }
@@ -48,7 +77,7 @@ int main(int argc, char ** argv)
         close(fd);
         return 0;
     }
-    
+
     if(strcmp(argv[1], "-diagoff") == 0)
     {
         setDiagnostics(fd, 0);
@@ -91,29 +120,16 @@ int main(int argc, char ** argv)
     if(strcmp(argv[1], "-s") == 0)
     {
     	displayText(fd, 1, "");
-	displayText(fd, 0, "Ready to start");
-	while((readStatus(fd) & STATUS_STARTSW) == 0);
-	displayText(fd, 0, "Running...");
-	close(fd);
-	return 0;
+	    displayText(fd, 0, "Ready to start");
+	    while((readStatus(fd) & STATUS_STARTSW) == 0);
+	    displayText(fd, 0, "Running...");
+	    close(fd);
+	    return 0;
     }
 
 
     if(strcmp(argv[1], "-t") == 0)
     {
-        if(fd == -1)
-        {
-            printf("\nCould not find device!\n");
-            close(fd);
-            return -1;
-        }
-
-        if(syncBoard(fd) != 0)
-        {
-            printf("\nCould not sync with board!\n");
-            close(fd);
-        }
-
         displayText(fd, 0, argv[2]);
 
         if(argc == 4)
