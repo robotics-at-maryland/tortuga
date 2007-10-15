58e327018e4002c9bfbb70e4de8fed3b1d47518b
Leo Singer <aronnax@umd.edu>
2007-10-15 19:44:08 +0000

    Got contourslice to work

--- sandbox/sonar/simulations/plot_crlb.m
+++ sandbox/sonar/simulations/plot_crlb.m
@@ -1,8 +1,8 @@
 function [X,Y,Z,V] = plot_crlb(filename)
-posticks = -100:20:100;
-depthticks = -10:4:10;
+posticks = -30:3:30;
+depthticks = -30:20:30;
 [X,Y,Z]=meshgrid(posticks,posticks,depthticks);
-Z = zeros(length(posticks),length(posticks),length(depthticks));
+V = zeros(length(posticks),length(posticks),length(depthticks));
 load(filename,'hydro_pos');
 
 for i = 1:length(posticks)
@@ -17,11 +17,19 @@ m = length(depthticks);
 prows = floor(sqrt(m));
 pcols = ceil(m/prows);
 
-levels = 0.01:0.02:10;
-for k = 1:length(depthticks)
-    subplot(prows,pcols,k);
-    [C,h] = contour(X(:,:,1),Y(:,:,1),V(:,:,k),levels);
-    axis square;
-    title(sprintf('Depth: %d meters',depthticks(k)));
-end
-set(h,'ShowText','on','TextStep',2);
+%levels = 0.01:0.02:10;
+%for k = 1:length(depthticks)
+%    subplot(prows,pcols,k);
+%    [C,h] = contour(X(:,:,1),Y(:,:,1),V(:,:,k),levels);
+%    axis square;
+%    title(sprintf('Depth: %d meters',depthticks(k)));
+%end
+%set(h,'ShowText','on','TextStep',2);
+clf;
+title('Projected error bounds for sonar location measurements');
+xlabel('x (meters)');
+ylabel('y (meters)');
+zlabel('z (meters)');
+contourslice(X,Y,Z,V,[0],[0],depthticks);
+axis square;
+colorbar;
--- sandbox/sonar/simulations/plot_phase_resolution.m
+++ sandbox/sonar/simulations/plot_phase_resolution.m
@@ -1,5 +1,5 @@
-snr = logspace(-1,4,30);
-freq_sample = logspace(5,8,30);
+snr = logspace(-1,4,10);
+freq_sample = logspace(5,7,10);
 phase_rez = zeros(length(snr),length(freq_sample));
 [X,Y] = meshgrid(snr, freq_sample);
 
@@ -15,10 +15,10 @@ levels = 10.^(-10:0);
 figure(1);
 axes('XScale','log');
 axes('YScale','log');
-axes('XTick',snr);
-axes('YTick',freq_sample);
-axes('XTickMode','manual');
-axes('YTickMode','manual');
+%axes('XTick',snr);
+%axes('YTick',freq_sample);
+%axes('XTickMode','manual');
+%axes('YTickMode','manual');
 [C,h] = contour(X,Y,phase_rez,levels);
 set(h,'ShowText','on');
 xlabel('Signal-to-noise ratio (unitless)');
