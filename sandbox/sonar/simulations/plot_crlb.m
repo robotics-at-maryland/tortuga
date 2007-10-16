84191e9b45a226cddc73968565c02be9b46c6434
Leo Singer <aronnax@umd.edu>
2007-10-16 18:18:59 +0000

    Very polished volumetric 3D display.

--- sandbox/sonar/simulations/plot_crlb.m
+++ sandbox/sonar/simulations/plot_crlb.m
@@ -17,19 +17,14 @@ m = length(depthticks);
 prows = floor(sqrt(m));
 pcols = ceil(m/prows);
 
-%levels = 0.01:0.02:10;
-%for k = 1:length(depthticks)
-%    subplot(prows,pcols,k);
-%    [C,h] = contour(X(:,:,1),Y(:,:,1),V(:,:,k),levels);
-%    axis square;
-%    title(sprintf('Depth: %d meters',depthticks(k)));
-%end
-%set(h,'ShowText','on','TextStep',2);
 clf;
-title('Projected error bounds for sonar location measurements');
+title('Projected lower error bound for sonar-based position estimate (meters)');
 xlabel('x (meters)');
 ylabel('y (meters)');
 zlabel('z (meters)');
 contourslice(X,Y,Z,V,[0],[0],depthticks);
-axis square;
+axis equal;
+view(3);
+axis vis3d;
 colorbar;
+print('-r600','-depsc',filename);
