24e2d281140f88d799b8d7b6b97beb7f72c8fea9
Leo Singer <aronnax@umd.edu>
2007-10-24 00:23:46 +0000

    Made corrections to some comments.

--- sandbox/sonar/simulations/crlb.m
+++ sandbox/sonar/simulations/crlb.m
@@ -13,7 +13,7 @@ phase_rez = 0.01; % phase resolution in radians
 
 % Physical Properties
 
-lambda = vs / freq;
+lambda = vs / freq; % wavelength of the sound, in meters
 
 % There is always a hydrophone at (0,0,0), but it is not listed.
 % hydro_pos = [
@@ -68,13 +68,13 @@ deriv(1:m,4:end) = deriv12;
 deriv(m+1:end,1:3) = deriv21;
 deriv(m+1:end,4:end) = deriv22;
 
-% Computing the covariance matrix %
+% Computing the Cramer-Rao lower bound %
 
 cov = zeros(4*m,4*m);
 cov(1:m,1:m) = eye(m) * sigma_x^2 + (1 - eye(m)) * 0.5 * sigma_x^2;
 cov(m+1:end,m+1:end) = eye(3*m) * sigma_s^2;
 
-% Computing the Cramer-Rao lower bound
+% Extracting and returning the x_s, y_s, z_s covariance matrix
 
 cr_lowerbound = inv(transpose(deriv)*inv(cov)*deriv);
 cr_lowerbound = cr_lowerbound(1:3,1:3);
