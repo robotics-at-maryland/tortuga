ccf6ea8dc36b443e6de8e7fbe9d6b63b9a098df3
twerner <software@ram.umd.edu>
2007-10-25 01:36:40 +0000

    Restricted Hydrophones, diasbled isWellpacked

--- sandbox/sonar/simulations/genetic.m
+++ sandbox/sonar/simulations/genetic.m
@@ -29,7 +29,7 @@ for j = -2.5:.5:2.5
     end
 end
 SUM_NEW = SUM_OLD
-for i = 1:500
+for i = 1:3
     if(SUM_NEW < SUM_OLD)
        hydro_old = hydro_new;
         SUM_OLD = SUM_NEW;
@@ -57,11 +57,11 @@ end
 			 (rand()-.1)*Format,(rand()-.1)*Format-y1,0;];
             hydro_new = hydro_old + hydro_up;
             hydro_actual = hydro_new;
-            hydro_actual(1,3) = hydro_old(1,3) + z0;
-            hydro_actual(2,3) = hydro_old(2,3) + z0;
-            hydro_actual(3,2) = hydro_old(3,2) + y0;
-            hydro_actual(4,2) = hydro_old(4,2) + y0;
-            test = sum(sum(hydro_actual > test_matrix));
+            hydro_actual(1,3) = hydro_new(1,3) + (z1 -z0);
+            hydro_actual(2,3) = hydro_new(2,3) + (z1 - z0);
+            hydro_actual(3,2) = hydro_new(3,2) + (y1 -y0);
+            hydro_actual(4,2) = hydro_new(4,2) + (y1- y0);
+test = (sum(sum(hydro_actual > test_matrix)) + sum(sum(hydro_actual < zeros(4,3))));
 	    %isReady = isWellPacked(hydro_new, lambda);
 	    if( test > 0)
                isReady = false;
