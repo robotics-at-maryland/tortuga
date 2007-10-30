f6c87a457aff12d1dea72a467f187d9d3149a7f4
Leo Singer <aronnax@umd.edu>
2007-10-30 23:32:54 +0000

    Corrected minor error in Monte Carlo sim

--- sandbox/sonar/simulations/gaussian_variant.m
+++ sandbox/sonar/simulations/gaussian_variant.m
@@ -11,9 +11,9 @@ m = m(1);
 
 % construct random variants for hydro_pos and tdoas
 
-hydro_pos = hydro_pos+randn_renormalized(0, hydro_pos_accuracy, [m 3]);
+hydro_pos_real = hydro_pos+randn_renormalized(0, hydro_pos_accuracy, [m 3]);
 range0 = sqrt(dot(source_pos, source_pos));
-range = hydro_pos - repmat(source_pos,m,1);
+range = hydro_pos_real - repmat(source_pos,m,1);
 range = sqrt(dot(range,range,2));
 range0 = range0 + randn_renormalized(0, tdoa_accuracy, 1);
 range = range + randn_renormalized(0, tdoa_accuracy, [m,1]);
