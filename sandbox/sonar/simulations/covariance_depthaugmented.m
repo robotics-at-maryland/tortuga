456409d5dcbd216aa146a5acb4968918750407f5
Leo Singer <aronnax@umd.edu>
2007-11-09 01:07:10 +0000

    Added covariance_depthaugmented.m, forgotten on last commit.

---
+++ sandbox/sonar/simulations/covariance_depthaugmented.m
@@ -0,0 +1,12 @@
+function cov_mat = covariance_depthaugmented(source_pos, hydro_pos, hydro_pos_accuracy, tdoa_accuracy, depth_accuracy, algorithm, num_trials)
+% Determine covariance of x_s, y_s, and z_s, for a particular source
+% position and hydrophone configuration, by using the Monte Carlo method.
+
+pos = zeros(num_trials,3);
+
+for i = 1:num_trials
+  [tdoas,zs] = gaussian_variant_depthaugmented(source_pos, hydro_pos, hydro_pos_accuracy, tdoa_accuracy, depth_accuracy);
+  pos(i,1:3) = feval(algorithm, hydro_pos, tdoas, zs);
+end
+
+cov_mat = cov(pos);
