169573474484bb6fc023c5c2a59fb2d474d362e2
speisj <software@ram.umd.edu>
2007-11-09 17:58:25 +0000

    -Background color fixed -Implemented GridBagSizer -Centered labels above thruster controls

--- tools/oci/DisplayPanels.py
+++ tools/oci/DisplayPanels.py
@@ -7,20 +7,47 @@ class ThrusterPanel(wx.Panel):
         self.parent = parent
         
         """ Implement two Thrusters """
-        
+        self.label1 = wx.StaticText(self,-1,"Thruster 1")
+        self.label2 = wx.StaticText(self,-1,"Thruster 2")
+        self.label3 = wx.StaticText(self,-1,"Thruster 3")
+        self.label4 = wx.StaticText(self,-1,"Thruster 4")
+
         self.thruster1 = ThrusterBar.ThrusterBar(self)
         self.thruster2 = ThrusterBar.ThrusterBar(self)
         self.thruster3 = ThrusterBar.ThrusterBar(self)
         self.thruster4 = ThrusterBar.ThrusterBar(self)
 
-        layout = wx.BoxSizer(wx.HORIZONTAL)
+        layout =  wx.GridBagSizer(10, 10)
+        layout.Add( self.label1, (1,1),flag=wx.ALIGN_CENTER_HORIZONTAL)
+        layout.Add( self.label2, (1,2), flag=wx.ALIGN_CENTER_HORIZONTAL)
+        layout.Add( self.label3, (1,3) ,flag=wx.ALIGN_CENTER_HORIZONTAL)
+        layout.Add( self.label4, (1,4) ,flag=wx.ALIGN_CENTER_HORIZONTAL)
+
+        layout.Add( self.thruster1, (2,1),flag=wx.EXPAND)
+        layout.Add( self.thruster2, (2,2), flag=wx.EXPAND)
+        layout.Add( self.thruster3, (2,3) ,flag=wx.EXPAND)
+        layout.Add( self.thruster4, (2,4) ,flag=wx.EXPAND)
+
         
-        layout.Add(self.thruster1, 1, wx.EXPAND | wx.ALL,10)       
-        layout.Add(self.thruster2, 1, wx.EXPAND | wx.ALL,10)
-        layout.Add(self.thruster3, 1, wx.EXPAND | wx.ALL,10)       
-        layout.Add(self.thruster4, 1, wx.EXPAND | wx.ALL,10)       
+        layout.AddGrowableRow(2)
+        
+        #for col in range(0,layout.GetCols()):
+        #print layout.GetCols(),layout.GetRows()
+        
+        #layout.AddGrowableCol(0)
+        layout.AddGrowableCol(1)
+        layout.AddGrowableCol(2)
+        layout.AddGrowableCol(3)
+        layout.AddGrowableCol(4)
+
+
+        #layout.Add(self.thruster1, wx.EXPAND | wx.ALL,10)       
+        #layout.Add(self.thruster2, wx.EXPAND | wx.ALL,10)
+        #layout.Add(self.thruster3, wx.EXPAND | wx.ALL,10)       
+        #layout.Add(self.thruster4, wx.EXPAND | wx.ALL,10)       
 
         self.SetSizerAndFit(layout)
+
 
 class RotationPanel(wx.Panel):
     def __init__(self, parent, *args, **kwargs):
