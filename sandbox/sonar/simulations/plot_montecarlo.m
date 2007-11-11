603ae85f59cd46cae4a1718d9abfee0bfb7717e1
Leo Singer <aronnax@umd.edu>
2007-11-11 22:49:04 +0000

    PLOT_MONTECARLO is now a fully interactive GUI for the multilateration test harness.  Also, the default TDOA_ACCURACY in config.m has been made more lenient.

--- sandbox/sonar/simulations/plot_montecarlo.m
+++ sandbox/sonar/simulations/plot_montecarlo.m
@@ -1,17 +1,34 @@
-function cov_mat = plot_montecarlo(source_pos, hydro_pos, hydro_pos_accuracy, tdoa_accuracy, algorithm, num_trials)
+function cov_mat = plot_montecarlo(source_pos, hydro_pos, ...
+                                   hydro_pos_accuracy, tdoa_accuracy, ...
+                                   algorithm, num_trials)
 % Determine covariance of x_s, y_s, and z_s, for a particular source
 % position and hydrophone configuration, by using the Monte Carlo method.
 
 pos = zeros(num_trials,3);
 
 for i = 1:num_trials
-  tdoas = gaussian_variant(source_pos, hydro_pos, hydro_pos_accuracy, tdoa_accuracy);
+  tdoas = gaussian_variant(source_pos, hydro_pos, hydro_pos_accuracy, ...
+                           tdoa_accuracy);
   pos(i,1:3) = feval(algorithm, hydro_pos, tdoas);
 end
 
-cov_mat = cov(pos);
-scatter3(pos(:,1),pos(:,2),pos(:,3),'.');
-axis equal;
-view(2);
-axis vis3d;
+scaled_hydro_pos = hydro_pos * 20;
+fov = 30;
 
+%cov_mat = cov(pos);
+av = mean(pos);
+hold on;
+cla;
+axis([-fov, fov, -fov, fov]);
+axis square;
+plot(pos(:,1),pos(:,2),'.r','MarkerSize',5);
+plot(scaled_hydro_pos(:,1),scaled_hydro_pos(:,2),'dk','MarkerFaceColor','k','MarkerSize',6);
+plot(source_pos(1),source_pos(2),'ob','MarkerFaceColor','b');
+plot(av(1),av(2),'or','MarkerFaceColor','r');
+hold off;
+source_pos(1:2) = ginput(1);
+if any(abs(source_pos(1:2))>fov)
+  return;
+end
+cov_mat = plot_montecarlo(source_pos,hydro_pos,hydro_pos_accuracy, ...
+                          tdoa_accuracy,algorithm,num_trials);
