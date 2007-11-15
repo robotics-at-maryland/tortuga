2d97bd090360bb6e8d66fd31bc4a92b86195d699
Leo Singer <aronnax@umd.edu>
2007-11-15 19:14:09 +0000

    '''SONAR''': vectorized MAKEPULSE

--- sandbox/sonar/simulations/makepulse.m
+++ sandbox/sonar/simulations/makepulse.m
@@ -1,9 +1,11 @@
 function adcdata = makepulse(duration, delay)
 config;
 T_s = 1/f_s;
-t = 0:T_s:duration;
 omega = 2 * pi * f;
-adcdata = signal_amplitude * sin(omega * (t - delay));
-envelope = (t > delay & t < (ping_duration + delay));
+t = 0:T_s:duration;
+
+[T,D] = meshgrid(t,delay);
+adcdata = signal_amplitude * sin(omega * (T - D));
+envelope = (T > D & T < (ping_duration + D));
 adcdata = adcdata .* envelope;
-adcdata = round(adcdata / adc_amplitude * 2^adc_bits);
+adcdata = round(adcdata / adc_amplitude * 2^adc_bits / 2);
