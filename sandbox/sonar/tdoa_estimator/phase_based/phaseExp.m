4da4b3b9a091ddd7897752091db59aefd8fd017b
swatson <software@ram.umd.edu>
2007-11-30 23:00:05 +0000

    first import of my phase experiment code that shows how to calculate phase from a single bin of an FFT

---
+++ sandbox/sonar/tdoa_estimator/phase_based/phaseExp.m
@@ -0,0 +1,40 @@
+function phaseExp(delayBetweenPlots,inverseNoiseLevel,loopLength);
+close all; figure;
+number_of_samples=64;
+signal_frequency=30; %in kHz
+sample_rate=500; %in kHz
+desiredBin1=5;
+desiredBin2=61;
+
+for loop=1:loopLength
+	%create signal
+	k=0:number_of_samples-1;
+	y=sin((2.*pi.*k./(sample_rate/signal_frequency))+(loop/10));
+	y=y+(randn(1,number_of_samples)/inverseNoiseLevel);
+	
+	%fft and phase computations
+	ffty=(fft(y));
+	phase1(loop)=(180/pi)*atan2(imag(ffty(desiredBin1)),real(ffty(desiredBin1)));
+	phase2(loop)=(180/pi)*atan2(imag(ffty(desiredBin2)),real(ffty(desiredBin2)));
+	phaseA(loop)=(180/pi)*atan(imag(ffty(desiredBin1))/real(ffty(desiredBin1)));
+	phaseB(loop)=(180/pi)*atan(imag(ffty(desiredBin2))/real(ffty(desiredBin2)));
+	
+	%plot signal with FFT result on left plot
+	subplot(1,2,1); hold off; plot(k,5.*y,'.-'); hold on
+	plot(k,real(ffty),'or'); plot(k,imag(ffty),'.r')
+	axis([0 64 -35 35])
+	title('input signal in blue, FFT in red (circles:real dots:imag')
+
+	%plot phase over time on the right side plot
+	subplot(1,2,2); hold on;
+	plot(1:loop,phaseA,'b.'); 
+	plot(1:loop,phaseB,'r.')
+	plot(1:loop,phase1,'ko')
+	plot(1:loop,phase2,'go')
+	plot(1:loop,(phase1-phase2)/2,'c^')
+	axis([1 loopLength -180 180])
+	title('different attempts to compute phase and plot in time')
+	
+	%delay before plotting next iteration
+	pause(delayBetweenPlots)
+end
