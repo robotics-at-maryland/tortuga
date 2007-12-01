c86cb7ea24e77e40a00071d8901051468afb7694
swatson <software@ram.umd.edu>
2007-12-01 00:36:32 +0000

    changed commenting to show well in my preferred character width.  changed phase calculations to prevent them from running too many lines as well.

--- sandbox/sonar/tdoa_estimator/phase_based/phaseExp.m
+++ sandbox/sonar/tdoa_estimator/phase_based/phaseExp.m
@@ -1,12 +1,15 @@
 function phaseExp(delayBetweenPlots,inverseNoiseLevel,loopLength);
-% PHASEXP A visualization of the phase properties of a monochromatic signal
-%   Illustrates the relationship between the positive and negative Fourier 
-%   components of a purely real, monochromatic signal rotated through a phase
-%   shift.  Demonstrates that atan(im(X_k)/re(X_k)) does not provide enough
-%   information to uniquely determine the phase angle of the original signal
-%   in the range -pi:pi.  Fixes include computing and comparing both the 
-%   negative and positive Fourier components or using the atan2 function found
-%   in C's <math.h> and other languages.
+% PHASEXP A visualization of the phase properties of a
+%	single frequency signal (for now)
+%   This function illustrates the relationship between the 
+%	positive and negative Fourier components of a purely 
+%	real, signle frequency signal rotated through a phase
+%   shift.  Demonstrates that atan(im(X_k)/re(X_k)) does not
+%	provide enough information to uniquely determine the 
+%	phase angle of the original signal in the range -pi:pi.
+%	Fixes include computing and comparing both the 
+%   negative and positive Fourier components or using the 
+%	atan2 function found in C's <math.h> and other languages.
 
 clf;
 number_of_samples=64;
@@ -23,16 +26,20 @@ for loop=1:loopLength
 	
 	%fft and phase computations
 	ffty=(fft(y));
-	phase1(loop)=(180/pi)*atan2(imag(ffty(desiredBin1)),real(ffty(desiredBin1)));
-	phase2(loop)=(180/pi)*atan2(imag(ffty(desiredBin2)),real(ffty(desiredBin2)));
-	phaseA(loop)=(180/pi)*atan(imag(ffty(desiredBin1))/real(ffty(desiredBin1)));
-	phaseB(loop)=(180/pi)*atan(imag(ffty(desiredBin2))/real(ffty(desiredBin2)));
+	imag1=imag(ffty(desiredBin1));
+	real1=real(ffty(desiredBin1));
+	imag2=imag(ffty(desiredBin2));
+	real2=real(ffty(desiredBin2));
+	phase1(loop)=(180/pi)*atan2(imag1,real1);
+	phase2(loop)=(180/pi)*atan2(imag2,real2);
+	phaseA(loop)=(180/pi)*atan(imag1/real1);
+	phaseB(loop)=(180/pi)*atan(imag2/real2);
 	
 	%plot signal with FFT result on left plot
 	subplot(1,2,1); hold off; plot(k,5.*y,'.-'); hold on;
 	plot(k,real(ffty),'or'); plot(k,imag(ffty),'.r');
 	axis([0 64 -35 35])
-	title('input signal in blue, FFT in red (circles:real dots:imag')
+	title('input signal:blue, FFT:red(circles:real dots:imag')
 
 	%plot phase over time on the right side plot
 	subplot(1,2,2); hold on;
@@ -42,7 +49,7 @@ for loop=1:loopLength
 	plot(1:loop,phase2,'go');
 	plot(1:loop,(phase1-phase2)/2,'c^')
 	axis([1 loopLength -180 180])
-	title('different attempts to compute phase and plot in time')
+	title('different attempts to compute phase') %in time
 	
 	%delay before plotting next iteration
 	pause(delayBetweenPlots)
