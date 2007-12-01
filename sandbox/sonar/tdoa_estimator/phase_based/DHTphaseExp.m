172f87cc74a3cd1435bc92af8dc632086d4d5481
swatson <software@ram.umd.edu>
2007-12-01 21:01:51 +0000

    Added a new phase experiment using the DHT - Discrete Hartley Transform... this works much the same as the FFT only it requires no complex math and two frequency bins are needed to compute phase.  this update adds an m file to do the DHT and another phase experiment m file so as not to disturb the other experiment file.

--- sandbox/sonar/tdoa_estimator/phase_based/DHTphaseExp.m
+++ sandbox/sonar/tdoa_estimator/phase_based/DHTphaseExp.m
@@ -2,15 +2,18 @@ function phaseExp(delayBetweenPlots,inverseNoiseLevel,loopLength);
 % PHASEXP A visualization of the phase properties of a
 %	single frequency signal (for now)
 %	This function illustrates the relationship between the 
-%	positive and negative Fourier components of a purely 
+%	positive and negative DHT* components of a purely 
 %	real, signle frequency signal rotated through a phase
-%	shift.  Demonstrates that atan(im(X_k)/re(X_k)) does not
+%	shift.  Demonstrates that atan(X_-k/X_+k) does not
 %	provide enough information to uniquely determine the 
 %	phase angle of the original signal in the range -pi:pi.
 %	Fixes include computing and comparing both the 
 %	negative and positive Fourier components or using the 
 %	atan2 function found in C's <math.h> and other languages.
-
+%
+%	* - DHT is the Discrete Hartley Transform, an analouge 
+%		of the FFT with no complex math 
+%
 clf;
 number_of_samples=64;
 signal_frequency=30; %in kHz
@@ -25,20 +28,19 @@ for loop=1:loopLength
 	y=y+(randn(1,number_of_samples)/inverseNoiseLevel);
 	
 	%fft and phase computations
-	ffty=fft(y);
-	imag1=imag(ffty(desiredBin1));
-	real1=real(ffty(desiredBin1));
-	imag2=imag(ffty(desiredBin2));
-	real2=real(ffty(desiredBin2));
-	phase1(loop)=(180/pi)*atan2(imag1,real1);
-	phase2(loop)=(180/pi)*atan2(imag2,real2);
-	phaseA(loop)=(180/pi)*atan(imag1/real1);
-	phaseB(loop)=(180/pi)*atan(imag2/real2);
+	dhty=DHT(y);
+	
+	real1=real(dhty(desiredBin1));
+	real2=real(dhty(desiredBin2));
+	phase1(loop)=(180/pi)*atan2(real2,real1);
+	phase2(loop)=(180/pi)*atan2(real1,real2);
+	phaseA(loop)=(180/pi)*atan(real2/real1);
+	phaseB(loop)=(180/pi)*atan(real1/real2);
 	
 	%plot signal with FFT result on left plot
-	subplot(1,2,1); hold off; plot(k,5.*y,'.-'); hold on;
-	plot(k,real(ffty),'or'); plot(k,imag(ffty),'.r');
-	axis([0 64 -35 35])
+	subplot(1,2,1); hold off; plot(k,y,'.-'); hold on;
+	plot(k,dhty,'or'); 
+	axis([0 64 -8 8])
 	title('input signal:blue, FFT:red(circles:real dots:imag')
 
 	%plot phase over time on the right side plot
@@ -47,7 +49,6 @@ for loop=1:loopLength
 	plot(1:loop,phaseB,'r.');
 	plot(1:loop,phase1,'ko');
 	plot(1:loop,phase2,'go');
-	plot(1:loop,(phase1-phase2)/2,'c^')
 	axis([1 loopLength -180 180])
 	title('different attempts to compute phase') %in time
 	
