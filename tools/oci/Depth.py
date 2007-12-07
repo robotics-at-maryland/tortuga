86398c9c03c97f02808157acced6ba5da18dedce
speisj <software@ram.umd.edu>
2007-12-07 02:39:07 +0000

    Depth Event Publisher

---
+++ tools/oci/Depth.py
@@ -0,0 +1,31 @@
+import ext.core as core
+import ext.vehicle_device as vdev
+
+class Depth(core.EventPublisher):
+    def __init__(self,forceIn=0,nameIn="AftThruster"):
+        #vdev.IThruster.__init__(self)
+        core.EventPublisher.__init__(self)
+        self.depth = forceIn
+        self.name = nameIn
+        self.stepVal = 0
+        self.PYFORCEUPDATE = "UPDATE_DEPTH"
+        
+    def getName(self):
+        return self.name
+    
+    def getDepth(self):
+        return self.depth
+        
+    def setDepth(self, val):
+        self.depth = val
+        
+    def step(self):
+        self.depth += 10
+        if self.depth >= 100:
+            self.depth = 0
+        
+    def update(self):
+        self.step()     
+        pyEvent = core.Event()
+        pyEvent.depth = self.depth
+        self.publish(self.PYFORCEUPDATE, pyEvent)
