b3ada66576335277b6793962d2e6181cb6da5a4d
speisj <software@ram.umd.edu>
2007-12-26 15:32:16 +0000

    '''GUI''' preliminary reorganization, view folder

---
+++ tools/oci/view/depthbar.py
@@ -0,0 +1,69 @@
+# Copyright (C) 2007 Maryland Robotics Club
+# Copyright (C) 2007 Jon Speiser <jspeiser@umd.edu>
+# All rights reserved.
+#
+# Author: Jon Speiser <jspeiser@umd.edu>
+import wx
+
+class DepthBar(wx.PyControl):
+    BLACK = wx.Colour(0, 0, 0, 98)
+    BLUE = wx.Colour(0, 0, 255, 98)
+
+    def __init__(self, parent,barType=None, id=wx.ID_ANY, pos=wx.DefaultPosition,
+                 size=wx.DefaultSize, style=wx.NO_BORDER, validator=wx.DefaultValidator,name="valbar"):
+        wx.PyControl.__init__(self, parent, id, pos, size, style, validator, name)
+        self.parent = parent
+        """ Default to 0% """
+        self.barValue = 50 
+        self.desiredValue = 20
+        """ The max/min values of the bar """
+        
+        self.maxValue = 0
+        self.minValue = 100
+        
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.Bind(wx.EVT_ERASE_BACKGROUND, self.OnEraseBackground)
+        
+    def setVal(self,val):
+        if val >= self.maxValue and val <= self.minValue:
+            self.barValue=val
+            self.Refresh() #Refresh the display
+        else:
+            print val
+            return None 
+            """return None if we could not process setVal due to constraints"""
+        
+    def OnPaint(self, event):
+        """ Handles the wx.EVT_PAINT event for ThrusterBar. """
+        dc = wx.PaintDC(self)
+        gc = wx.GraphicsContext.Create(dc)
+        self.Draw(gc)
+       
+    def Draw(self,gc):
+        width,height = self.GetSize()
+        """ Draw the rectangle to represent the value """
+        pen = wx.Pen(DepthBar.BLUE, 1)
+        brush = wx.Brush(DepthBar.BLUE) 
+            
+        percent = float(self.barValue) / (self.maxValue+self.minValue) 
+        heightScale = (height*percent) #-?
+        
+        gc.SetPen(pen)
+        gc.SetBrush(brush)
+        
+        path = gc.CreatePath()
+        path.AddRectangle(0, 0, width, heightScale) #innerBorder
+        gc.DrawPath(path)
+        
+        percent = float(self.desiredValue) / (self.maxValue+self.minValue) 
+        desiredScale = (height*percent)
+        pen.SetColour(DepthBar.BLACK)
+        pen.SetWidth(3)
+        pen.SetStyle(wx.DOT)
+        gc.SetPen(pen)
+        gc.StrokeLine(0,desiredScale,width,desiredScale)
+                    
+             
+    def OnEraseBackground(self, event):
+        """ Handles the wx.EVT_ERASE_BACKGROUND event. """
+        pass
