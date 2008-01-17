aa259618e6a20129bbe82a0f9d1a2391574743fd
Tom Capon <robot256@gmail.com>
2008-01-17 18:45:11 +0000

    '''Sonar PIC''' Finished most of it. 181 cycles per loop.

--- sandbox/pic_dev/dsPIC assembly/asmSlidingDFT.s
+++ sandbox/pic_dev/dsPIC assembly/asmSlidingDFT.s
@@ -1,7 +1,7 @@
 
-	; DSP Partial DFT function
+	; DSP Sliding DFT function
 	
-	; asmPartialDFT( int Log2N, fractional * pData, int Log2K, fractionl * pDest, fractcomplex * pTwid, int psvPage );
+	; asmSlidingDFT( int Log2N, fractional * pData, fractcomplex wFactor, fractional trigger );
 	
 	.include "dspcommon.inc"
 	
@@ -11,12 +11,14 @@
 .text
 
 
-.bss Nlen,2
 .bss localBuf,16
-.bss kValue,16
+.bss kValue,16		; must be in near memory
 .bss kMags,8		; must be contiguous after kValue
 .bss wFactor,4
 .bss trigger,2
+.bss timestamp,16
+.bss trigFlag,2
+
 
 ; W register arguments:
 ; W0 - N
@@ -37,136 +39,144 @@ _asmSlidingDFT:
 	
 	; Configure core for fractional operations
 	fractsetup	W7
-	; Configure core for PSV X data
-	psvaccess	W7
-
-	
-	mov		W0,Nlen			; store number of points
-	mov.d	W2,wFactor		; store complex wFactor
-	mov		W4,trigger		; store trigger value
-	
-	mov		W0,W4			; move N out of the way
-	mov		W1,W0			; store bufPtr(0)
-	add		W0,W4,W1		; generate bufPtr(1)
-	add		W1,W4,W2		; generate bufPtr(2)
-	add		W2,W4,W3		; generate bufPtr(3)
 	
 	
+	
+	
+	; Persistent WREG assignments:
+	; W0 is scratch register
+	; W1 is buffer base address
+	; W2 is buffer length N
+	; W3 is buffer index, range 0 to N-1
+	; W4 is wFactor real, dsp
+	; W5 is wFactor imag, dsp
+	; W6 is scratch register, dsp
+	; W7 is scratch register, dsp
+	; W8 is scratch register, dsp
+	; W9 is index loop mask (N-1)
+	; W10 is not used
+	; W11 is not used
+	; W12 is trigger level
+	; W13 is scratch register, dsp
+	; W14 is frame pointer
+	; W15 is stack pointer
+	
+	
+	
+	mov.d	W2,W4		; store complex wFactor
+	
+	
+	clr		W3				; start at buffer index 0
+	sl		W0,W2			; shift left one bit to get number of bytes instead of words
+	dec		W2,W9			; generate bit mask for buffer index looping
+	mov		W4,W12			; store trigger value
+	
+	mov		#0xF,W0
+	mov		W0,trigFlag		; trigger count, counting down
+		
 BigLoop:
 	
 	; Wait for data to arrive
 	btsc	D_AV_PIN
 	bra		BigLoop
 	
+	; TOTAL INSTRUCTIONS EXECUTED AFTER THIS POINT:
+	; >>> 181
+	
 	
 	; >> 32 INSTRUCTIONS
+	; >> uses W0 scratch
 	; Collect the data
 	bclr	RD_PIN
-	mov		DATA_PORT,W4
+	mov		DATA_PORT,W0
 	bset	RD_PIN
-	mov		W4,localBuf
+	mov		W0,localBuf
 	bclr	RD_PIN
-	mov		DATA_PORT,W4
+	mov		DATA_PORT,W0
 	bset	RD_PIN
-	mov		W4,localBuf+1
+	mov		W0,localBuf+1
 	bclr	RD_PIN
-	mov		DATA_PORT,W4
+	mov		DATA_PORT,W0
 	bset	RD_PIN
-	mov		W4,localBuf+2
+	mov		W0,localBuf+2
 	bclr	RD_PIN
-	mov		DATA_PORT,W4
+	mov		DATA_PORT,W0
 	bset	RD_PIN
-	mov		W4,localBuf+3
+	mov		W0,localBuf+3
 	bclr	RD_PIN
-	mov		DATA_PORT,W4
+	mov		DATA_PORT,W0
 	bset	RD_PIN
-	mov		W4,localBuf+4
+	mov		W0,localBuf+4
 	bclr	RD_PIN
-	mov		DATA_PORT,W4
+	mov		DATA_PORT,W0
 	bset	RD_PIN
-	mov		W4,localBuf+5
+	mov		W0,localBuf+5
 	bclr	RD_PIN
-	mov		DATA_PORT,W4
+	mov		DATA_PORT,W0
 	bset	RD_PIN
-	mov		W4,localBuf+6
+	mov		W0,localBuf+6
 	bclr	RD_PIN
-	mov		DATA_PORT,W4
+	mov		DATA_PORT,W0
 	bset	RD_PIN
-	mov		W4,localBuf+7
+	mov		W0,localBuf+7
 	
 	
 	
-	; >>> 21 INSTRUCTIONS
-	; Subtract the old data
+	; >>> 23 INSTRUCTIONS
+	; Subtract the old data and loop buffer index
 	; old data is stored in location bufferPtr
+	
+	; W0 is new data
+	; W1 is base pointer
+	; W2 is N
+	; W3 is index
+	; W13 is current buffer address
+	; W8 is new data - old data
+	; W7 is address of kValue
 	
 	; Load kValue pointer
 	mov		&kValue,W7
+	add		W1,W3,W13			; index off of base address for channel 0 buffer
 	
 	; channel 0
-	mov		[W7],W4			; get the real part of the current k value
-	sub		W4,[W0],W4		; subtract old data stored in buffer @ W4
-	mov		localBuf+0,W5	; get new data, put in W1
-	mov		W5,[W0++]		; store new data in buffer @ W4, increment W4.  Need to do circular buffering later.
-	add		W4,W5,[W7++]	; add new data to kValue and store
+	mov		localBuf+0,W0		; get new data
+	sub		W0,[W13],W8		; subtract old data from new data
+	add		W8,[W7],[W7++]		; add difference to kValue (pre and post increment ot skip imag part)
+	mov		W0,[W13]			; store new data in buffer
+	add		W13,W2,W13			; increment buffer address by N to next channel buffer
 	
 	; channel 1
-	mov		[++W7],W4		; get the real part of the current k value
-	sub		W4,[W1],W4		; subtract old data stored in buffer @ W4
-	mov		localBuf+2,W5	; get new data, put in W1
-	mov		W5,[W1++]		; store new data in buffer @ W4, increment W4.  Need to do circular buffering later.
-	add		W4,W5,[W7++]	; add new data to kValue and store
+	mov		localBuf+2,W0		; get new data
+	sub		W0,[W13],W8		; subtract old data from new data
+	add		W8,[++W7],[W7++]	; add difference to kValue (pre and post increment ot skip imag part)
+	mov		W0,[W13]			; store new data in buffer
+	add		W13,W2,W13			; increment buffer address by N to next channel buffer
 	
 	; channel 2
-	mov		[++W7],W4		; get the real part of the current k value
-	sub		W4,[W2],W4		; subtract old data stored in buffer @ W4
-	mov		localBuf+4,W5	; get new data, put in W1
-	mov		W5,[W2++]		; store new data in buffer @ W4, increment W4.  Need to do circular buffering later.
-	add		W4,W5,[W7++]	; add new data to kValue and store
+	mov		localBuf+4,W0		; get new data
+	sub		W0,[W13],W8		; subtract old data from new data
+	add		W8,[++W7],[W7++]	; add difference to kValue (pre and post increment ot skip imag part)
+	mov		W0,[W13]			; store new data in buffer
+	add		W13,W2,W13			; increment buffer address by N to next channel buffer
 	
 	; channel 3
-	mov		[++W7],W4		; get the real part of the current k value
-	sub		W4,[W3],W4		; subtract old data stored in buffer @ W4
-	mov		localBuf+6,W5	; get new data, put in W1
-	mov		W5,[W3++]		; store new data in buffer @ W4, increment W4.  Need to do circular buffering later.
-	add		W4,W5,[W7]		; add new data to kValue and store
+	mov		localBuf+6,W0		; get new data
+	sub		W0,[W13],W8		; subtract old data from new data
+	add		W8,[++W7],[W7]		; add difference to kValue (pre and post increment ot skip imag part)
+	mov		W0,[W13]			; store new data in buffer
 	
 	
-	; >>> 14 INSTRUCTIONS
-	; perform loop addressing
-	mov		NLen,W4			; get buffer length in W0
-	dec		W4,W5			; mask for looping bits in W1
-	
-	; channel 0
-	and		W5,W0,W6		; check buffer 0
-	btsc	Z				; check if lower bits are zero
-	sub		W0,W4,W0		; subtract loop N from pointer if overflowed
-	
-	; channel 1
-	and		W5,W1,W6		; check buffer 1
-	btsc	Z				; check if lower bits are zero
-	sub		W1,W4,W1		; subtract loop N from pointer if overflowed
-	
-	; channel 2
-	and		W5,W2,W6		; check buffer 2
-	btsc	Z				; check if lower bits are zero
-	sub		W2,W4,W2		; subtract loop N from pointer if overflowed
-	
-	; channel 3
-	and		W5,W3,W6		; check buffer 3
-	btsc	Z				; check if lower bits are zero
-	sub		W3,W4,W3		; subtract loop N from pointer if overflowed
+	; Perform looping
+	inc2	W3					; increment buffer index
+	and		W3,W9,W3			; clear high bits
 	
 	
 	
-	; >>> 26 INSTRUCTIONS
+	; >>> 24 INSTRUCTIONS
 	; Multiply new k values by wFactor
 	; Both numebers are complex
 	; The formula we need is a*c-b*d, a*d+b*c
 	; We are lucky because one operand is at a constant address, and the others are in contiguous addresses.
-	
-	; fetch wFactor
-	mov.d	wFactor+0,W4		; real and imag part of wFactor (2 cycles)
 	
 	; set operand addresses
 	mov		&kValue,W8			; real part address of kValues
@@ -201,82 +211,66 @@ BigLoop:
 	mpy		W5,W6,B							; imag = b*c
 	mac		W4,W6,A							; real += a*c
 	mac		W4,W7,B,[W13]+=2				; imag += a*d, write back real part to kValue
-	clr		A,[W13]							; write back imag part to kValue
+	clr		A,[W13]+=2						; write back imag part to kValue
 	
 	
 
 	; Add second batch of samples
-	; >>> 21 INSTRUCTIONS
-	; Subtract the old data
+	; >>> 23 INSTRUCTIONS
+	; Subtract the old data and loop buffer index
 	; old data is stored in location bufferPtr
+	
+	; W0 is new data
+	; W1 is base pointer
+	; W2 is N
+	; W3 is index
+	; W13 is current buffer address
+	; W8 is new data - old data
+	; W7 is address of kValue
 	
 	; Load kValue pointer
 	mov		&kValue,W7
+	add		W1,W3,W13			; index off of base address for channel 0 buffer
 	
 	; channel 0
-	mov		[W7],W4			; get the real part of the current k value
-	sub		W4,[W0],W4		; subtract old data stored in buffer @ W4
-	mov		localBuf+0,W5	; get new data, put in W1
-	mov		W5,[W0++]		; store new data in buffer @ W4, increment W4.  Need to do circular buffering later.
-	add		W4,W5,[W7++]	; add new data to kValue and store
+	mov		localBuf+8,W0		; get new data
+	sub		W0,[W13],W8		; subtract old data from new data
+	add		W8,[W7],[W7++]		; add difference to kValue
+	mov		W0,[W13]			; store new data in buffer
+	add		W13,W2,W13			; increment buffer address by N to next channel buffer
 	
 	; channel 1
-	mov		[++W7],W4		; get the real part of the current k value (post and pre-increment to skip imag part in array)
-	sub		W4,[W1],W4		; subtract old data stored in buffer @ W4
-	mov		localBuf+2,W5	; get new data, put in W1
-	mov		W5,[W1++]		; store new data in buffer @ W4, increment W4.  Need to do circular buffering later.
-	add		W4,W5,[W7++]	; add new data to kValue and store
+	mov		localBuf+10,W0		; get new data
+	sub		W0,[W13],W8		; subtract old data from new data
+	add		W8,[++W7],[W7++]	; add difference to kValue
+	mov		W0,[W13]			; store new data in buffer
+	add		W13,W2,W13			; increment buffer address by N to next channel buffer
 	
 	; channel 2
-	mov		[++W7],W4		; get the real part of the current k value (post and pre-increment to skip imag part in array)
-	sub		W4,[W2],W4		; subtract old data stored in buffer @ W4
-	mov		localBuf+4,W5	; get new data, put in W1
-	mov		W5,[W2++]		; store new data in buffer @ W4, increment W4.  Need to do circular buffering later.
-	add		W4,W5,[W7++]	; add new data to kValue and store
+	mov		localBuf+12,W0		; get new data
+	sub		W0,[W13],W8		; subtract old data from new data
+	add		W8,[++W7],[W7++]	; add difference to kValue
+	mov		W0,[W13]			; store new data in buffer
+	add		W13,W2,W13			; increment buffer address by N to next channel buffer
 	
 	; channel 3
-	mov		[++W7],W4		; get the real part of the current k value (post and pre-increment to skip imag part in array)
-	sub		W4,[W3],W4		; subtract old data stored in buffer @ W4
-	mov		localBuf+6,W5	; get new data, put in W1
-	mov		W5,[W3++]		; store new data in buffer @ W4, increment W4.  Need to do circular buffering later.
-	add		W4,W5,[W7]		; add new data to kValue and store
+	mov		localBuf+14,W0		; get new data
+	sub		W0,[W13],W8		; subtract old data from new data
+	add		W8,[++W7],[W7]		; add difference to kValue
+	mov		W0,[W13]			; store new data in buffer
 	
 	
-	; >>> 14 INSTRUCTIONS
-	; perform loop addressing
-	mov		NLen,W4			; get buffer length in W4
-	dec		W4,W5			; mask for looping bits in W5
-	
-	; channel 0
-	and		W5,W0,W6		; check buffer 0
-	btsc	Z				; check if lower bits are zero
-	sub		W0,W4,W0		; subtract N from pointer if overflowed
-	
-	; channel 1
-	and		W5,W1,W6		; check buffer 1
-	btsc	Z				; check if lower bits are zero
-	sub		W1,W4,W1		; subtract N from pointer if overflowed
-	
-	; channel 2
-	and		W5,W2,W6		; check buffer 2
-	btsc	Z				; check if lower bits are zero
-	sub		W2,W4,W2		; subtract N from pointer if overflowed
-	
-	; channel 3
-	and		W5,W3,W6		; check buffer 3
-	btsc	Z				; check if lower bits are zero
-	sub		W3,W4,W3		; subtract N from pointer if overflowed
+	; Perform looping
+	inc2	W3					; increment buffer index
+	and		W3,W9,W3			; clear high bits
 	
 	
 	
-	; >>> 26 INSTRUCTIONS
+	; >>> 24 INSTRUCTIONS
 	; Multiply new k values by wFactor
 	; Both numebers are complex
 	; The formula we need is a*c-b*d, a*d+b*c
 	; We are lucky because one operand is at a constant address, and the others are in contiguous addresses.
-	
-	; fetch wFactor
-	mov.d	wFactor+0,W4		; real and imag part of wFactor (2 cycles)
 	
 	; set operand addresses
 	mov		&kValue,W8			; real part address of kValues
@@ -315,48 +309,91 @@ BigLoop:
 	
 	
 	
-	; >>> 16 INSTRUCTIONS
+	; >>> 15 INSTRUCTIONS
 	; Now norm the vectors
 	
 	; load addresses
 	mov		&kValues,W8			; address of kValue array
-	mov		&kMags,W13			; write-back address for magnitudes
 	
 	; fetch first values
-	mov.d	[W8++],W4			; kValues(0) real and imag part (2 cycles)
+	mov.d	[W8++],W6			; kValues(0) real and imag part (2 cycles)
 
 	; channel 0 norm
-	mpy		W4*W4,A,[W8]+=2,W4				; square real part, prefetch next real part
-	mac		W5*W5,A,[W8]+=2,W5				; square and sum imag part, prefetch next imag part
+	mpy		W6*W6,A,[W8]+=2,W6				; square real part, prefetch next real part
+	mac		W7*W7,A,[W8]+=2,W7				; square and sum imag part, prefetch next imag part
 	clr		A,[W13]+=2						; write back to kMags
 	
 	; channel 1 norm
-	mpy		W4*W4,A,[W8]+=2,W4				; square real part, prefetch next real part
-	mac		W5*W5,A,[W8]+=2,W5				; square and sum imag part, prefetch next imag part
+	mpy		W6*W6,A,[W8]+=2,W6				; square real part, prefetch next real part
+	mac		W7*W7,A,[W8]+=2,W7				; square and sum imag part, prefetch next imag part
 	clr		A,[W13]+=2						; write back to kMags
 		
 	; channel 2 norm
-	mpy		W4*W4,A,[W8]+=2,W4				; square real part, prefetch next real part
-	mac		W5*W5,A,[W8]+=2,W5				; square and sum imag part, prefetch next imag part
+	mpy		W6*W6,A,[W8]+=2,W6				; square real part, prefetch next real part
+	mac		W7*W7,A,[W8]+=2,W7				; square and sum imag part, prefetch next imag part
 	clr		A,[W13]+=2						; write back to kMags
 	
 	; channel 3 norm
-	mpy		W4*W4,A							; square real part
-	mac		W5*W5,A							; square and sum imag part
+	mpy		W6*W6,A							; square real part
+	mac		W7*W7,A							; square and sum imag part
 	clr		A,[W13]							; write back to kMags
 	
 
-	; ^^^ 170 instruction so far above
-
-
+	; >>> 37 INSTRUCTIONS
 	; Compare the norms to the trigger level
+	mov		&kMags,W8			; address of kMags in W8
+	
+	btss	trigFlag,0			; check if this channel has triggered already
+	rcall	_apdCallOffset14	; returns at _apdChXDone label
+	cpslt	W12,[W8++]			; compare trigger to kMag for channel 0
+	rcall	_apdCallOffset10	; returns at _apdChXDone label
+	mov		TMR8,W0				; get timestamp low word
+	mov		W0,timestamp+0		; store timestamp low word
+	mov		TMR9HLD,W0			; get timestamp high word
+	mov		W0,timestamp+2		; store timestamp high word
+	bclr	trigFlag,0			; decrement trigger counter
+_apdCh0Done
+
+	btss	trigFlag,1			; check if this channel has triggered already
+	rcall	_apdCallOffset14	; returns at _apdChXDone label
+	cpslt	W12,[W8++]			; compare trigger to kMag for channel 0
+	rcall	_apdCallOffset10	; returns at _apdChXDone label
+	mov		TMR8,W0				; get timestamp low word
+	mov		W0,timestamp+4		; store timestamp low word
+	mov		TMR9HLD,W0			; get timestamp high word
+	mov		W0,timestamp+6		; store timestamp high word
+	bclr	trigFlag,1			; decrement trigger counter
+_apdCh1Done
+
+	btss	trigFlag,2			; check if this channel has triggered already
+	rcall	_apdCallOffset14	; returns at _apdChXDone label
+	cpslt	W12,[W8++]			; compare trigger to kMag for channel 0
+	rcall	_apdCallOffset10	; returns at _apdChXDone label
+	mov		TMR8,W0				; get timestamp low word
+	mov		W0,timestamp+8		; store timestamp low word
+	mov		TMR9HLD,W0			; get timestamp high word
+	mov		W0,timestamp+10		; store timestamp high word
+	bclr	trigFlag,2			; decrement trigger counter
+_apdCh2Done
+
+	btss	trigFlag,3			; check if this channel has triggered already
+	rcall	_apdCallOffset14	; returns at _apdChXDone label
+	cpslt	W12,[W8++]			; compare trigger to kMag for channel 0
+	rcall	_apdCallOffset10	; returns at _apdChXDone label
+	mov		TMR8,W0				; get timestamp low word
+	mov		W0,timestamp+12		; store timestamp low word
+	mov		TMR9HLD,W0			; get timestamp high word
+	mov		W0,timestamp+14		; store timestamp high word
+	bclr	trigFlag,3			; decrement trigger counter
+_apdCh3Done
+
+	; >>> LOOP OVERHEAD: 3 INSTRUCTIONS
+	; Check if we have all four triggers
+	cp0		trigFlag			; exit loop if all four triggers are there.
+	bra		NZ,BigLoop
 	
 	
-	
-	
-	
-	goto	BigLoop
-	
+	; OUT OF BIG LOOP!!! GOT ALL FOUR TRIGGERS
 	
 	
 
@@ -371,6 +408,25 @@ BigLoop:
 	pop.d	W8
 	
 	return
+
+
+_apdCallOffset10
+	; TOS-1 is return location
+	; we want to increment that number by 10, exactly 5 instructions later
+	pop		W0					; get return address
+	add		W0,#10,[W15++]		; add offset and push result
+	return
+
+_apdCallOffset14
+	; TOS-1 is return location
+	; we want to increment that number by 10, exactly 5 instructions later
+	pop		W0					; get return address
+	add		W0,#14,[W15++]		; add offset and push result
+	nop
+	nop
+	return
+
+
 
 .end
 
