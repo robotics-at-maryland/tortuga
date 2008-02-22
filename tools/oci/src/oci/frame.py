032bf8fda7a4ec02f2eaff19de0d27555910353f
speisj <software@ram.umd.edu>
2008-02-22 19:41:24 +0000

    '''OCI''' Changed from !AuiNotebook to !AuiManager. Window session state data, along with shell history is saved in a guidata.yml file in the wx.StandardPaths.Get().GetUserConfigDir() directory.

--- tools/oci/src/oci/frame.py
+++ tools/oci/src/oci/frame.py
@@ -5,9 +5,13 @@
 # Author: Joseph Lisee <jlisee@umd.edu>
 # File:   tools/oci/src/frame.py
 
+# STD Imports
+import os
+
 # Library Imports
 import wx
 import wx.aui
+import yaml
 
 # Project Imports
 from gui.view import IPanelProvider
@@ -28,12 +32,13 @@ class MainFrame(wx.Frame):
                    cleaned up module shutdown.
     """
     
-    
+
     panelProviders = ExtensionPoint(IPanelProvider)
     
     def __init__(self, config, subsystems):
         self._panels = []
-    
+        self.guiDataFile = os.path.abspath(os.path.join(wx.StandardPaths.Get().GetUserConfigDir(), 'guidata.yml'))
+        
         # Instantiate super class based on configuration settings
         #gui_node = config.get('GUI', {})
         #position = gui_node.get('window_position',wx.DefaultPosition)
@@ -45,15 +50,27 @@ class MainFrame(wx.Frame):
         wx.Frame.__init__(self, None, title = title, pos = position, 
                           size = size) 
         
-        self._notebook = wx.aui.AuiNotebook(self)
-        sizer = wx.BoxSizer()
-        sizer.Add(self._notebook, 1, wx.EXPAND)
-        self.SetSizer(sizer)
+        self._mgr = wx.aui.AuiManager(self)
 
         # Add panels for all the current subsystems
         self._addShell(subsystems)
         self._addSubsystemPanels(subsystems)
- 
+        self._mgr.Update()
+        
+        try:
+            stream = file(self.guiDataFile, 'r')
+            guiData = yaml.load(stream)    
+            stream.close()
+            self.SetSize(guiData["windowSize"])
+            self.SetPosition(guiData["windowPos"])
+            self._mgr.LoadPerspective(guiData["paneLayout"])
+            all_panes = self._mgr.GetAllPanes()
+            for pane in all_panes:
+                pane.Show()
+            self._mgr.Update()    
+        except Exception, e:
+            print "Could not read/load yaml layout:"  ,e
+        
         self.Bind(wx.EVT_CLOSE,self._onClose)            
     
     def _addShell(self, subsystems):
@@ -66,16 +83,31 @@ class MainFrame(wx.Frame):
             name = name[0].lower() + name[1:]
             locals[name] = subsystem
             introText += '%s: %s\n' % (name, type(subsystem))
-        
         shell = ShellPanel(self, locals = locals, introText = introText)
+        locals["shell"] = shell # for testing
+
         paneInfo = wx.aui.AuiPaneInfo().Name("Shell")
         paneInfo = paneInfo.Caption("Shell").Left()
         self._addSubsystemPanel(paneInfo, shell, [])
     
-    def _onClose(self, event):        
-        for i in range(0, self._notebook.GetPageCount()):
-            self._notebook.RemovePage(i)
-            
+    def _onClose(self, event):  
+        allPanes = self._mgr.GetAllPanes()
+        for pane in allPanes: 
+            self._mgr.ClosePane(pane)    
+
+        # Write YAML data
+        layoutStream = file(self.guiDataFile, 'w+')
+        guiData = {}
+        guiData["windowSize"] = self.GetSize()
+        guiData["windowPos"] = self.GetPosition()
+        guiData["paneLayout"] = self._mgr.SavePerspective()
+        guiData["shellHistory"] = self._shell.history
+        
+        yaml.dump(guiData,layoutStream) 
+        layoutStream.close()
+        
+        self._mgr.UnInit()
+     
         # Close all panels, if I don't do this the EVT_CLOSE handlers will 
         # never be called
         for panel in self._panels:
@@ -101,5 +133,20 @@ class MainFrame(wx.Frame):
                 self._addSubsystemPanel(paneInfo, panel, sys)
 
     def _addSubsystemPanel(self, paneInfo, panel, usedSubsystems):
-        self._notebook.AddPage(panel, paneInfo.caption)
+        if paneInfo.caption == "Thrusters":
+            paneInfo.Center()
+        elif paneInfo.caption == "Depth":
+            paneInfo.Right()
+        elif paneInfo.caption == "Shell":
+            self._shell = panel
+        elif paneInfo.caption == "Orientation":
+            paneInfo.Center()
+        elif paneInfo.caption == "Demo Sonar":
+            paneInfo.Left()
+        elif paneInfo.caption == "Demo Power":
+            paneInfo.Left()
+
+        
+        self._mgr.AddPane(panel, paneInfo, paneInfo.caption)
         self._panels.append(panel)
+        
