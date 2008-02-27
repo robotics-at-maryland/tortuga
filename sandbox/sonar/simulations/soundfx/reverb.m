a7515ac621e9b39f52b0851370d5362f5983e69c
Leo Singer <aronnax@umd.edu>
2008-02-27 02:17:42 +0000

    '''SONAR''' Added some comments to soundfx code

--- sandbox/sonar/simulations/soundfx/reverb.m
+++ sandbox/sonar/simulations/soundfx/reverb.m
@@ -4,6 +4,17 @@ function y = reverb(x, delays, scale_factors)
 %   by different time delays and modulated by different scale factors.
 %   x may be an MxN vector, where M is the number of samples, and N is the 
 %   number of channels.
+%
+%   Example:
+%     >> fs = 100;             % sampling rate of 100 Hz
+%     >> dt = 1/fs;            % sample period of 0.01 seconds
+%     >> t  = 0:dt:10;         % 10 second time period
+%     >> f  = 2;               % signal frequency 2 Hz
+%     >> y  = sin(2*pi*f*t);   % generate a sine wave
+%     >> y  = (t > 1 & t < 3) .* y;  % add a rectangular envelope
+%     >> delays = [0,1.5,4] * dt;    % original signal + two echoes
+%     >> scale_factors = [1,0.5,0.1] % decreasing in amplitude
+%     >> yWithReverb = reverb(y,delays,scale_factors);
 
 y = x;
 
