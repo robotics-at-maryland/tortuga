5ac5c6e2a968785935370751aa1c167415a83346
Leo Singer <aronnax@umd.edu>
2008-03-02 04:47:03 +0000

    '''SONAR''' Simple stupid embedded demo now COMPILES, but we can't tell if it WORKS yet

--- sandbox/sonar/embedded/dft/main.c
+++ sandbox/sonar/embedded/dft/main.c
@@ -1,4 +1,22 @@
+#include "dft.h"
+
 int main(void)
 {
+	int nchannels = 4, k = 10, N = 100;
+	int niters = 1000; /* number of iterations */
+	sliding_dft_t *dft;
+	adcdata_t zerosamp[nchannels];
+	int i;
+	
+	/* As a simple, stupid test, we'll feed the DFT a bunch of zeros every time */
+	for (i = 0 ; i < nchannels ; i ++)
+		zerosamp[i] = 0;
+	
+	dft = dft_alloc(nchannels, k, N);
+	
+	for (i = 0 ; i < niters ; i ++)
+		dft_update(dft, zerosamp);
+	
+	dft_free(dft);
 }
 
