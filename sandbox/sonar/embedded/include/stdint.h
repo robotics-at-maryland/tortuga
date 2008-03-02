0828802d3e853a428b94bb1747665dc071772527
Leo Singer <aronnax@umd.edu>
2008-03-02 04:04:29 +0000

    '''SONAR''' Still trying for a working makefile -- had to provide my own stdint.h

---
+++ sandbox/sonar/embedded/include/stdint.h
@@ -0,0 +1,22 @@
+/*
+ *  stdint.h
+ *  sonarController
+ *
+ *  Created by Leo Singer on 02/01/08.
+ *  Copyright 2007 Robotics@Maryland. All rights reserved.
+ *
+ *  <stdint.h> replacement for Steve Moskovchenko's dsPIC toolchain, which does not supply it
+ * 
+ */
+
+
+#ifndef RAM_PIC_REPLACEMENT_STDINT_H
+#define RAM_PIC_REPLACEMENT_STDINT_H
+
+
+/* This is not a complete list of integer types, but we will add to it as necessary */
+typedef signed char int8_t;
+typedef signed int  int32_t;
+
+
+#endif
