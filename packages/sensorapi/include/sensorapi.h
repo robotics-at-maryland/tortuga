1587f9663be1be89dd69ccfacb51db46b9d9a052
Steve Mo <stevenm86@gmail.com>
2008-03-15 03:34:39 +0000

    '''SensorAPI''' This was backwards.

--- packages/sensorapi/include/sensorapi.h
+++ packages/sensorapi/include/sensorapi.h
@@ -56,23 +56,23 @@ struct powerInfo
 #define CMD_THRUSTER6_ON      11
 
 /* Inputs to the bar command */
-#define CMD_BAR1_ON     0x00
-#define CMD_BAR2_ON     0x01
-#define CMD_BAR3_ON     0x02
-#define CMD_BAR4_ON     0x03
-#define CMD_BAR5_ON     0x04
-#define CMD_BAR6_ON     0x05
-#define CMD_BAR7_ON     0x06
-#define CMD_BAR8_ON     0x07
+#define CMD_BAR1_OFF     0x00
+#define CMD_BAR2_OFF     0x01
+#define CMD_BAR3_OFF     0x02
+#define CMD_BAR4_OFF     0x03
+#define CMD_BAR5_OFF     0x04
+#define CMD_BAR6_OFF     0x05
+#define CMD_BAR7_OFF     0x06
+#define CMD_BAR8_OFF     0x07
 
-#define CMD_BAR1_OFF    0x08
-#define CMD_BAR2_OFF    0x09
-#define CMD_BAR3_OFF    0x0A
-#define CMD_BAR4_OFF    0x0B
-#define CMD_BAR5_OFF    0x0C
-#define CMD_BAR6_OFF    0x0D
-#define CMD_BAR7_OFF    0x0E
-#define CMD_BAR8_OFF    0x0F
+#define CMD_BAR1_ON    0x08
+#define CMD_BAR2_ON    0x09
+#define CMD_BAR3_ON    0x0A
+#define CMD_BAR4_ON    0x0B
+#define CMD_BAR5_ON    0x0C
+#define CMD_BAR6_ON    0x0D
+#define CMD_BAR7_ON    0x0E
+#define CMD_BAR8_ON    0x0F
 
 
 /* Bits of the thruster state response */
