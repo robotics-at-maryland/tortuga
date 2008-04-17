a786e720e2e43e5ecc8b6937e42f6aa81d202e64
Joseph Lisee <jlisee@gmail.com>
2008-04-17 00:25:32 +0000

    '''Vehicle''' updated thruster demo to allow quiting early.

--- packages/vehicle/test/src/ThrusterDemo.cpp
+++ packages/vehicle/test/src/ThrusterDemo.cpp
@@ -31,9 +31,19 @@ using namespace ram;
 using namespace ram::core;
 using namespace ram::vehicle::device;
 
-static const int FORCE = 15;
+static const int FORCE = 10;
 static const int APPLY_SLEEP = 7;
 static const int POST_CMD_SLEEP = 1;
+
+/** Quits if the sleep ends early */
+void quitSleep(vehicle::Vehicle* vehicle, unsigned int seconds);
+
+/** Applies the force to the vehicle, and then does a APPLY_SLEEP */
+void testForce(vehicle::Vehicle* vehicle, math::Vector3 force,
+               math::Vector3 torque);
+
+/** Stops thrusters and then does a POST_CMD_SLEEP */
+void stopThrusters(vehicle::Vehicle* vehicle);
 
 int main(int argc, char** argv)
 {
@@ -77,75 +87,87 @@ int main(int argc, char** argv)
     std::cout << "Applying forward force (move's vehicle forward): " << force 
 	      << std::endl;
     std::cout << "Air/Water goes back" << std::endl;
-    vehicle.applyForcesAndTorques(force,
-                                  math::Vector3::ZERO);
-    vehicle.update(0);
-    sleep(APPLY_SLEEP);
-    std::cout << "Stoping thrusters" << std::endl;
-    vehicle.applyForcesAndTorques(math::Vector3::ZERO, math::Vector3::ZERO);
-    vehicle.update(0);
-    sleep(POST_CMD_SLEEP);
-    std::cout << "Thrusters stopped" << std::endl;
+
+    testForce(&vehicle, force, math::Vector3::ZERO);
+    stopThrusters(&vehicle);
     
     // Apply a upward force
     force = math::Vector3(0, 0, FORCE);
     std::cout << "Applying upward force (moves vehicle up): " << force 
 	      << std::endl;
     std::cout << "Air/Water goes down" << std::endl;
-    vehicle.applyForcesAndTorques(force,
-                                  math::Vector3::ZERO);
-    vehicle.update(0);
-    sleep(APPLY_SLEEP);
-    std::cout << "Stoping thrusters" << std::endl;
-    vehicle.applyForcesAndTorques(math::Vector3::ZERO, math::Vector3::ZERO);
-    vehicle.update(0);
-    sleep(POST_CMD_SLEEP);
-    std::cout << "Thrusters stopped" << std::endl;
 
-    // Apply a sideways force
-    force = math::Vector3(0, FORCE, 0);
-    std::cout << "Applying right sideways force (moves vehicle right): "
-              << force  << std::endl;
-    std::cout << "Air/Water goes left" << std::endl;
-    vehicle.applyForcesAndTorques(force,
-                                  math::Vector3::ZERO);
-    vehicle.update(0);
-    sleep(APPLY_SLEEP);
-    std::cout << "Stoping thrusters" << std::endl;
-    vehicle.applyForcesAndTorques(math::Vector3::ZERO, math::Vector3::ZERO);
-    vehicle.update(0);
-    sleep(POST_CMD_SLEEP);
-    std::cout << "Thrusters stopped" << std::endl;
+    testForce(&vehicle, force, math::Vector3::ZERO);
+    stopThrusters(&vehicle);
     
     // Apply a positive pitch
     math::Vector3 torque = math::Vector3(0, FORCE, 0);
     std::cout << "Applying a positive pitch (vehicle nose down): " << torque 
 	      << std::endl;
     std::cout << "Air/Water above fore, Air/Water below aft" << std::endl;
-    vehicle.applyForcesAndTorques(math::Vector3::ZERO, torque);
 
-    vehicle.update(0);
-    sleep(APPLY_SLEEP);
-    std::cout << "Stoping thrusters" << std::endl;
-    vehicle.applyForcesAndTorques(math::Vector3::ZERO, math::Vector3::ZERO);
-    vehicle.update(0);
-    sleep(POST_CMD_SLEEP);
-    std::cout << "Thrusters stopped" << std::endl;
+    testForce(&vehicle, math::Vector3::ZERO, torque);
+    stopThrusters(&vehicle);
 
+    // Apply a sideways force
+    force = math::Vector3(0, FORCE, 0);
+    std::cout << "Applying left force (moves vehicle left): "
+              << force  << std::endl;
+    std::cout << "Air/Water goes right" << std::endl;
+
+    testForce(&vehicle, force, math::Vector3::ZERO);
+    stopThrusters(&vehicle);
+    
     // Apply a positive roll
     torque = math::Vector3(FORCE, 0, 0);
     std::cout << "Applying a positive roll (vehicle roll right): " << torque 
 	      << std::endl;
     std::cout << "Air/Water left top, Air/Water right bottom" << std::endl;
-    vehicle.applyForcesAndTorques(math::Vector3::ZERO, torque);
 
-    vehicle.update(0);
-    sleep(APPLY_SLEEP);
-    std::cout << "Stoping thrusters" << std::endl;
-    vehicle.applyForcesAndTorques(math::Vector3::ZERO, math::Vector3::ZERO);
-    vehicle.update(0);
-    sleep(POST_CMD_SLEEP);
-    std::cout << "Thrusters stopped" << std::endl;
+    testForce(&vehicle, math::Vector3::ZERO, torque);
+    stopThrusters(&vehicle);
     
-    return 0;
+    return EXIT_SUCCESS;
 }
+
+void quitSleep(vehicle::Vehicle* vehicle, unsigned int seconds)
+{
+    // Guard against re-entrance
+    static bool failedSleep = false;
+    
+    unsigned int sleepTime = sleep(seconds);
+    if (0 != sleepTime)
+    {
+        if (failedSleep)
+        {
+            std::cout << "Force quit, no wait for thruster stop" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        
+        std::cout << "Sleep stopped early, quiting" << std::endl;
+
+        failedSleep = true;
+        stopThrusters(vehicle);
+        failedSleep = false;
+        
+        exit(EXIT_SUCCESS);
+    }
+}
+
+void testForce(vehicle::Vehicle* vehicle, math::Vector3 force,
+               math::Vector3 torque)
+{
+    vehicle->applyForcesAndTorques(force, torque);
+    vehicle->update(0);
+    quitSleep(vehicle, APPLY_SLEEP);
+}
+
+void stopThrusters(vehicle::Vehicle* vehicle)
+{
+    std::cout << "Stoping thrusters" << std::endl;
+    vehicle->applyForcesAndTorques(math::Vector3::ZERO, math::Vector3::ZERO);
+    vehicle->update(0);
+    quitSleep(vehicle, POST_CMD_SLEEP);
+    std::cout << "Thrusters stopped" << std::endl;
+}
+
