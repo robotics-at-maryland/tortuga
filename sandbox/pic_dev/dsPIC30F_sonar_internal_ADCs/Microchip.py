e7ca051ef47ceb295a27a95017b744c3eb75db9a
Leo Singer <aronnax@umd.edu>
2008-04-21 18:18:00 +0000

    '''SONAR''' Generate construction tool for MPLAB C30

--- sandbox/pic_dev/dsPIC30F_sonar_internal_ADCs/Microchip.py
+++ sandbox/pic_dev/dsPIC30F_sonar_internal_ADCs/Microchip.py
@@ -1,10 +1,12 @@
-from SCons.Environment import Environment
+import os
+import SCons.Builder
 
-def MPLAB_C30(*args, **kw):
-	env = apply(Environment, args, kw)
-	hex = env.Builder(action = '/opt/local/bin/wine \'C:\\Program Files\\Microchip\\MPLAB C30\\bin\\pic30-elf-bin2hex\' < $SOURCE > $TARGET')
+def generate(env):
+	hex = SCons.Builder.Builder(action = '/opt/local/bin/wine \'C:\\Program Files\\Microchip\\MPLAB C30\\bin\\pic30-elf-bin2hex\' < $SOURCE > $TARGET')
 	env.Append(BUILDERS = {'Hex': hex})
 	env.Replace(CC = '/opt/local/bin/wine \'C:\\Program Files\\Microchip\\MPLAB C30\\bin\\pic30-elf-gcc\'')
 	env.Append(CFLAGS = ['-mcpu=$MCPU', '-Wall'])
 	env.Append(LINKFLAGS = '-Wl,--script="C:\\Program Files\\Microchip\\MPLAB C30\\support\\gld\\p${MCPU}.gld",--report-mem')
-	return env
+
+def exists(env):
+	return os.exists('~/.wine/drive_c')
