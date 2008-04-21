aaa826acfb997587fb61744b518b2554dfd99e98
Leo Singer <aronnax@umd.edu>
2008-04-21 17:05:26 +0000

    Grrr

--- sandbox/pic_dev/dsPIC30F_sonar_internal_ADCs/Microchip.py
+++ sandbox/pic_dev/dsPIC30F_sonar_internal_ADCs/Microchip.py
@@ -1,9 +1,8 @@
-from SCons.Script.SConscript import SConsEnvironment
-from SCons.Script.SConscript import SConsBuilder
+from SCons.Environment import Environment
 
 def MPLAB_C30(*args, **kw):
-	env = apply(SConsEnvironment, args, kw)
-	hex = SConsBuilder(action = '/opt/local/bin/wine \'C:\\Program Files\\Microchip\\MPLAB C30\\bin\\pic30-elf-bin2hex\' < $SOURCE > $TARGET')
+	env = apply(Environment, args, kw)
+	hex = env.Builder(action = '/opt/local/bin/wine \'C:\\Program Files\\Microchip\\MPLAB C30\\bin\\pic30-elf-bin2hex\' < $SOURCE > $TARGET')
 	env.Append(BUILDERS = {'Hex': hex})
 	env.Replace(CC = '/opt/local/bin/wine \'C:\\Program Files\\Microchip\\MPLAB C30\\bin\\pic30-elf-gcc\'')
 	env.Append(CFLAGS = ['-mcpu=$MCPU', '-Wall'])
