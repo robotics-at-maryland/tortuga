67a77e4e588d7857211006cae5b5beb52ea4f75e
ajdupree <software@ram.umd.edu>
2008-04-26 23:25:19 +0000

    16F starter code. This one Peter wrote to flash an LED.

---
+++ sandbox/pic_dev/16F_starterStuff/flashingLed.c
@@ -0,0 +1,22 @@
+#include <pic.h>
+ __CONFIG(FCMDIS & IESODIS & BORDIS & UNPROTECT & MCLRDIS & PWRTEN &
+ WDTDIS & INTIO);
+
+ int main(void)
+ {
+	int x;
+	 PORTA=0;
+	 //CMCON0 = 7;
+	 ANSEL=0;
+	 TRISA4=0;
+	 TRISA5=0;
+     TRISA0=0;
+	while (1){
+		RA0=1;
+		for (x=0;x<5000;x++);
+		RA0=0;
+		for (x=0;x<5000;x++);
+	}
+	 
+	return 0;
+}
