ae5a6cb3b6fe9a5f4d4a5f7486e17cc874ea5ac1
ajdupree <software@ram.umd.edu>
2008-04-28 20:13:08 +0000

    Modified the time setting so the blinking would be slower and more distinguishable. Also uncommented some lines which could have been throwing things off.

--- sandbox/pic_dev/16F_starterStuff/flashingLed.c
+++ sandbox/pic_dev/16F_starterStuff/flashingLed.c
@@ -4,18 +4,18 @@
 
  int main(void)
  {
-	int x;
+	long x;
 	 PORTA=0;
 	 //CMCON0 = 7;
 	 ANSEL=0;
-	 //TRISA4=0;
-	 //TRISA5=0;
+	 TRISA4=0;
+	 TRISA5=0;
      TRISA0=0;
 	while (1){
 		RA0=1;
-		for (x=0;x<5000;x++);
+		for (x=0;x<20000;x++);
 		RA0=0;
-		for (x=0;x<5000;x++);
+		for (x=0;x<20000;x++);
 	}
 	 
 	return 0;
