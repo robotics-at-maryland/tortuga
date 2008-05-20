9ec782cf3c0115857590e2739bded3b7ff421bed
Steve Mo <stevenm86@gmail.com>
2008-05-20 01:55:47 +0000

    '''Blackfin''' PIC firmware to reset it.

--- packages/embedded/sbr5/bfin.c
+++ packages/embedded/sbr5/bfin.c
@@ -16,14 +16,17 @@ _FWDT ( WDT_OFF );
 #define LED_ON      1
 
 /* LED pin definitions */
-#define LAT_LED1    _LATE3
-#define TRIS_LED1   _TRISE3
+#define LAT_LED_GREEN    _LATE3
+#define TRIS_LED_GREEN   _TRISE3
 
-#define LAT_LED2    _LATE4
-#define TRIS_LED2   _TRISE4
+#define LAT_LED_YELLOW    _LATE4
+#define TRIS_LED_YELLOW   _TRISE4
 
-#define LAT_LED3    _LATE5
-#define TRIS_LED3   _TRISE5
+#define LAT_LED_RED    _LATE5
+#define TRIS_LED_RED   _TRISE5
+
+
+/* BF and FPGA reset pins */
 
 /* THIS SHOULD NEVER BE DRIVEN HIGH */
 #define LAT_PIC_FPGA_RESET  _LATD1
@@ -33,21 +36,53 @@ _FWDT ( WDT_OFF );
 #define LAT_BF_RESET    _LATD3
 #define TRIS_BF_RESET   _TRISD3
 
+
+/* UART Pin specifications */
+#define LAT_U1_TX   _LATF3
+#define TRIS_U1_TX  _TRISF3
+#define LAT_U1_RX   _LATF2
+#define TRIS_U1_RX  _TRISF2
+
+#define LAT_U2_TX   _LATF5
+#define TRIS_U2_TX  _TRISF5
+#define LAT_U2_RX   _LATF4
+#define TRIS_U2_RX  _TRISF4
+
+#define IN_U1_RX    _RF2
+
 void main()
 {
+    long l;
     LAT_BF_RESET = 0;
     TRIS_BF_RESET = TRIS_OUT;
+
+    TRIS_U1_TX = TRIS_IN;
+    TRIS_U1_RX = TRIS_IN;
+    TRIS_U2_TX = TRIS_IN;
+    TRIS_U2_RX = TRIS_IN;
+
 
     LAT_PIC_FPGA_RESET = 0;
     TRIS_PIC_FPGA_RESET = TRIS_IN;
 
-    TRIS_LED1 = TRIS_OUT;
-    TRIS_LED2 = TRIS_OUT;
-    TRIS_LED3 = TRIS_OUT;
+    TRIS_LED_GREEN = TRIS_OUT;
+    TRIS_LED_YELLOW = TRIS_OUT;
+    TRIS_LED_RED = TRIS_OUT;
 
-    LAT_LED1 = LED_ON;
-    LAT_LED2 = LED_ON;
-    LAT_LED3 = LED_ON;
+    LAT_LED_GREEN = ~LED_ON;
+    LAT_LED_YELLOW = LED_ON;
+    LAT_LED_RED = LED_ON;
+
+
+    while(IN_U1_RX != 0);            /* Wait for FPGA to initialize */
+    LAT_LED_RED = ~LED_ON;
+
+    for(l=0; l<200000; l++);     /* Wait a little bit more */
+    LAT_LED_YELLOW = ~LED_ON;
+    LAT_LED_GREEN = LED_ON;
+
+
+    TRIS_BF_RESET = TRIS_IN;    /* Start the Blackfin */
 
     while(1);
 }
