9c8d49f3ddb2a0d41dfd599b58a34e741b332df6
Joseph Galante <glandjm@gmail.com>
2008-05-29 22:15:55 +0000

    '''control''' matlab rotational sim runs with Egeland/Godhaven controller.  no drag, no buoyant moment yet

--- packages/control/matlab/rotationalControlSims/rotationalSimDynamics.m
+++ packages/control/matlab/rotationalControlSims/rotationalSimDynamics.m
@@ -1,1 +1,63 @@
 function dx=rotationalSimDynamics(t,x)
+
+%% grab constants from SimMain
+
+%system inertia
+global H;
+
+%controller gain
+global Kd;
+global lambda;
+
+
+%% unpack ODE data
+
+q=x(1:4);
+w=x(5:7);
+qd=x(8:11);
+wd=x(12:14);
+
+%fix numerical quaternion drift
+q=q/norm(q,2);
+qd=qd/norm(qd,2);
+
+%% measurement
+%perfect measurement for now
+qmeas=q;
+wmeas=w;
+
+%% controller
+
+%propagate desired states
+%desire constant angular rate for now
+dwd=zeros(3,1);
+%desired angular position varies
+dqd=(1/2)*Q(qd)*wd;
+
+%compute attitude error qc_tilde (controller quaternion)
+qc_tilde=quaternionProduct(qmeas,qd);
+
+%compute composite error metric s
+wr=R(qc_tilde)*wd-lambda*qc_tilde(1:3);
+shat=wmeas-wr;
+
+%compute angular rate error wc_tilde
+wc_tilde=wmeas-R(qc_tilde)*wd;
+
+%d/dt(wrhat)=alpharhat
+dwr=R(qc_tilde)*dwd-S(wc_tilde)*R(qc_tilde)*wd-lambda*Q1(qc_tilde)*wc_tilde;
+
+u=-Kd*shat+H*dwr-S(H*wmeas)*wr;
+
+%% dynamics
+
+%propagate actual vehicle dynamics
+dw=inv(H)*(S(H*w)*w+u);
+
+%propagate actual vehicle kinematics
+dq=(1/2)*Q(q)*w;
+
+
+
+%put output states into vector for ode45
+dx=[dq; dw; dqd; dwd];
--- packages/control/matlab/rotationalControlSims/rotationalSimMain.m
+++ packages/control/matlab/rotationalControlSims/rotationalSimMain.m
@@ -8,12 +8,18 @@ clear;
 %initial position
 axis0=[0 0 1]';
 angle0=10*pi/180;
-q0=[axis0*sin(angle0/2); cos(angle0/2)]';
+q0=[axis0*sin(angle0/2); cos(angle0/2)];
 
 %initial angular rate
-w0=(pi/180)*[0 0 0]';
+w0=(pi/180)*[0 0 360/10]';
 
-x0=[q0; w0];
+%initial desired position
+qd0=q0;
+
+%initial desired angular rate
+wd0=w0;
+
+x0=[q0; w0; qd0; wd0];
 
 
 %% constants
@@ -31,12 +37,13 @@ H=(0.45359/39.37^2)*H;
 %controller gain
 global Kd;
 Kd=1*eye(3);
-
+global lambda;
+lambda=1;
 
 
 %% timing
 t0=0;
-te=100;
+te=10;
 
 %% simulation
 options=odeset;
@@ -50,4 +57,50 @@ options=odeset(options,'AbsTol',1e-3,'MaxStep',0.05);
 %grab results
 q=x(:,1:4);%actual angular position
 w=x(:,5:7);%actual angular rate
+qd=x(:,8:11);%desired angular position
+wd=x(:,12:14);%desired angular rate
 
+%% plot errors
+%storage arrays for error metrics
+attitude_error=zeros(length(time),1);
+
+for i=1:length(time)
+    qc_tilde=quaternionProduct(q(i,:)',qd(i,:)');
+    attitude_error(i)=norm(qc_tilde(1:3));
+end
+
+figure(1)
+plot(time,(180/pi)*attitude_error)
+xlabel('time (sec)')
+ylabel('|attitude error| (deg)')
+
+%% plot position response
+figure(2)
+subplot(4,1,1)
+plot(time,q(:,1),time,qd(:,1))
+ylabel('q_1')
+legend('actual','desired')
+subplot(4,1,2)
+plot(time,q(:,2),time,qd(:,2))
+ylabel('q_2')
+subplot(4,1,3)
+plot(time,q(:,3),time,qd(:,3))
+ylabel('q_3')
+subplot(4,1,4)
+plot(time,q(:,4),time,qd(:,4))
+ylabel('q_4')
+xlabel('time (s)')
+
+%% plot velocity response
+figure(3)
+subplot(3,1,1)
+plot(time,w(:,1)*180/pi,time,wd(:,1)*180/pi)
+ylabel('w_1 in deg/s')
+legend('actual','desired')
+subplot(3,1,2)
+plot(time,w(:,2)*180/pi,time,wd(:,2)*180/pi)
+ylabel('w_2 in deg/s')
+subplot(3,1,3)
+plot(time,w(:,3)*180/pi,time,wd(:,3)*180/pi)
+ylabel('w_3 in deg/s')
+xlabel('time (s)')
