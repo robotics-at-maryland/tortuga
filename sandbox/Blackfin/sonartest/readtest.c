00c14ded9771bda4bf719cde904c4deb5f8f5fda
Steve Mo <stevenm86@gmail.com>
2008-06-21 04:00:53 +0000

    '''Blackfin''' Sample for 2.6 seconds but still transferring using ASCII mode.

--- sandbox/Blackfin/sonartest/readtest.c
+++ sandbox/Blackfin/sonartest/readtest.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include "spartan.h"
 
+#define BLOCK_SIZE 0xA0000
+
+
 int main(int argc, char ** argv)
 {
     signed short val;
@@ -10,7 +13,6 @@ int main(int argc, char ** argv)
     register int i;
 
     initADC();
-
 
     signed short * samples1 = NULL;
     signed short * samples2 = NULL;
@@ -22,88 +24,102 @@ int main(int argc, char ** argv)
     signed short * samples8 = NULL;
 
 
+    count = strtol(argv[1], NULL, 16);
 
-//     printf("alloc 1\n");
-    samples1 = malloc(1280 * 1024 * 1);
-//     printf("alloc 2\n");
-    samples2 = malloc(1280 * 1024 * 1);
-//     printf("alloc 3\n");
-    samples3 = malloc(1280 * 1024 * 1);
-//     printf("alloc 4\n");
-    samples4 = malloc(1280 * 1024 * 1);
-/*
-    printf("alloc 5\n");
-    samples5 = malloc(1280 * 1024 * 1);
-    printf("alloc 6\n");
-    samples6 = malloc(1280 * 1024 * 1);
-    printf("alloc 7\n");
-    samples7 = malloc(1280 * 1024 * 1);
-    printf("alloc 8\n");
-    samples8 = malloc(1280 * 1024 * 1);
-
-
-*/
+    fprintf(stderr, "alloc 1\n");
+    samples1 = malloc(BLOCK_SIZE * 2);
+    fprintf(stderr, "alloc 2\n");
+    samples2 = malloc(BLOCK_SIZE * 2);
+    fprintf(stderr, "alloc 3\n");
+    samples3 = malloc(BLOCK_SIZE * 2);
+    fprintf(stderr, "alloc 4\n");
+    samples4 = malloc(BLOCK_SIZE * 2);
     if(samples1 == NULL || samples2 == NULL || samples3 == NULL || samples4 == NULL)
     {
-        printf("couldn't malloc\n");
+        fprintf(stderr, "couldn't malloc\n");
         return -1;
     }
-/*
-    if(samples5 == NULL || samples6 == NULL || samples7 == NULL || samples8 == NULL)
+
+    if(count > BLOCK_SIZE)
     {
-        printf("couldn't malloc\n");
-        return -1;
+        fprintf(stderr, "alloc 5\n");
+        samples5 = malloc(BLOCK_SIZE * 2);
+        fprintf(stderr, "alloc 6\n");
+        samples6 = malloc(BLOCK_SIZE * 2);
+        fprintf(stderr, "alloc 7\n");
+        samples7 = malloc(BLOCK_SIZE * 2);
+        fprintf(stderr, "alloc 8\n");
+        samples8 = malloc(BLOCK_SIZE * 2);
+
+        if(samples5 == NULL || samples6 == NULL || samples7 == NULL || samples8 == NULL)
+        {
+            printf("couldn't malloc\n");
+            return -1;
+        }
     }
-*/
-
-    if(argc < 2)
-    {
-        printf("readtest addr count\n");
-        printf("all values are in hex. addr is 32bit. count is 32bit\n");
-        return -1;
-    }
-
-    addr = strtol(argv[1], NULL, 16);
-    count = strtol(argv[2], NULL, 16);
-
-//     printf("reading for %d iterations from address 0x%08x\n", count, addr);
-
-    register unsigned short lastCount, sampleCount;
 
 
     REG(ADDR_LED) = 0x02;
 
-    int sd = 0;
-
     for(i=0; i<count; i++)
     {
-        samples1[i] = REG(ADDR_FIFO_OUT1A); //*((volatile unsigned short *) addr);
-        samples2[i] = REG(ADDR_FIFO_OUT1B);
-        samples3[i] = REG(ADDR_FIFO_OUT2A); //REG(ADDR_ADC4);
-        samples4[i] = REG(ADDR_FIFO_OUT2B); //REG(ADDR_ADC6);
-
+        if(i < BLOCK_SIZE)
+        {
+            samples1[i] = REG(ADDR_FIFO_OUT1A);
+            samples2[i] = REG(ADDR_FIFO_OUT1B);
+            samples3[i] = REG(ADDR_FIFO_OUT2A);
+            samples4[i] = REG(ADDR_FIFO_OUT2B);
+        } else
+        {
+            samples5[i-BLOCK_SIZE] = REG(ADDR_FIFO_OUT1A);
+            samples6[i-BLOCK_SIZE] = REG(ADDR_FIFO_OUT1B);
+            samples7[i-BLOCK_SIZE] = REG(ADDR_FIFO_OUT2A);
+            samples8[i-BLOCK_SIZE] = REG(ADDR_FIFO_OUT2B);
+        }
         while(REG(ADDR_FIFO_EMPTY1A) != 0);
     }
 
     REG(ADDR_LED) = 0x01;
 
-
-    if(argc == 3)
+    if(argc == 2)
     {
-        for(i=0; i<count; i++)
-            printf("A%d\n", samples1[i]);
-        for(i=0; i<count; i++)
-            printf("B%d\n", samples2[i]);
-        for(i=0; i<count; i++)
-            printf("C%d\n", samples3[i]);
-        for(i=0; i<count; i++)
-            printf("D%d\n", samples4[i]);
+        int t;
+        if(count > BLOCK_SIZE)
+            t = BLOCK_SIZE;
+        else
+            t = count;
+
+        for(i=0; i<t; i++)
+            printf("A %d\n", samples1[i]);
+        for(i=0; i<t; i++)
+            printf("B %d\n", samples2[i]);
+        for(i=0; i<t; i++)
+            printf("C %d\n", samples3[i]);
+        for(i=0; i<t; i++)
+            printf("D %d\n", samples4[i]);
+
+        if(count > BLOCK_SIZE)
+        {
+            t = count - BLOCK_SIZE;
+            for(i=0; i<t; i++)
+                printf("A %d\n", samples5[i]);
+            for(i=0; i<t; i++)
+                printf("B %d\n", samples6[i]);
+            for(i=0; i<t; i++)
+                printf("C %d\n", samples7[i]);
+            for(i=0; i<t; i++)
+                printf("D %d\n", samples8[i]);
+        }
+    } else
+    {
+
+
     }
+
 
 //     printf("samples dropped: %d\n", sd);
 
     REG(ADDR_LED) = 0x00;
 
     return 0; // hi joe
-
 }
