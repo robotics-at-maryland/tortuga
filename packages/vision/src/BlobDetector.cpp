8539a4a6a9528dc113a1bf55c3c9c63ef5dceb68
Leo Singer <aronnax@umd.edu>
2008-06-21 18:44:48 +0000

    '''Vision''' Partially done blob detector, still getting wrong max Y

--- packages/vision/src/BlobDetector.cpp
+++ packages/vision/src/BlobDetector.cpp
@@ -46,6 +46,8 @@ void BlobDetector::processImage(Image* input, Image* output)
     // Do debug stuff soon
     if (0 != output)
     {
+        output->copyFrom(input);
+
         CvPoint boundUR;
         CvPoint boundLL;
         
@@ -93,22 +95,22 @@ int BlobDetector::histogram(IplImage* img)
 	//Might need longs for these two if we increase image size above 1024x1024, otherwise a full white image might overflow
 	int* totalX=(int*) calloc(254,sizeof(int));
 	int* totalY=(int*) calloc(254,sizeof(int));
-
-        // To keep track of mins and maxes of the bounds of blobs
-        int* totalMaxX=(int*) calloc(254, sizeof(int));
-        int* totalMaxY=(int*) calloc(254, sizeof(int));
-        int* totalMinX=(int*) malloc(254 * sizeof(int));
-        int* totalMinY=(int*) malloc(254 * sizeof(int));
-        
+  
+  // To keep track of mins and maxes of the bounds of blobs
+	int* totalMaxX=(int*) calloc(254, sizeof(int));
+	int* totalMaxY=(int*) calloc(254, sizeof(int));
+	int* totalMinX=(int*) malloc(254 * sizeof(int));
+	int* totalMinY=(int*) malloc(254 * sizeof(int));
+  
 	unsigned char* joins=(unsigned char*) malloc(254*sizeof(unsigned char));
 	for (unsigned char i=0; i<254; i++)
-        {
+    {
 		joins[i]=i;
-                totalMinX[i] = img->width;
-                totalMinY[i] = img->height;
-        }
+		totalMinX[i] = img->width;
+		totalMinY[i] = img->height;
+	}
 	joins[0]=255;
-	
+  
 	int index=1;
 	int count=0;
 	for (int x=0;x<width;x++)
@@ -136,28 +138,36 @@ int BlobDetector::histogram(IplImage* img)
 					pixelCounts[index]=1;
 					totalX[index]=x;
 					totalY[index]=y;
+					totalMinX[index] = x;
+					totalMaxX[index] = x;
+					totalMinY[index] = y;
+					totalMaxY[index] = y;
 					data[count]=(unsigned char)(index++);
-//					cout<<index<<endl;				
+					//					cout<<index<<endl;				
 					if (index==254)
 					{
 						free(pixelCounts);
 						free(totalX);
 						free(totalY);
 						free(joins);
+						free(totalMinX);
+						free(totalMaxX);
+						free(totalMinY);
+						free(totalMaxY);
 						return -1;
 					}
 				}
 				else 
 				{
-//					cout<<"I'm in part 2"<<endl;
+					//					cout<<"I'm in part 2"<<endl;
 					unsigned char above2=above;
 					unsigned char left2=left;
 					if (above2==0)
 						above2=255;
 					else
 					{
-					while (above2!=joins[above2])
-						above2=joins[above2];
+						while (above2!=joins[above2])
+							above2=joins[above2];
 					}
 					if (left2==0)
 						left2=255;
@@ -173,41 +183,54 @@ int BlobDetector::histogram(IplImage* img)
 					++pixelCounts[data[count]];
 //					cout<<"end of part 2"<<endl;
                                         
-                                        // Min/Max
-                                        if (x < totalMinX[data[count]])
-                                            totalMinX[data[count]] = x;
-                                        else if (x > totalMaxX[data[count]])
-                                            totalMaxX[data[count]] = x;
-                                        if (y < totalMinY[data[count]])
-                                            totalMinY[data[count]] = y;
-                                        else if (y > totalMaxY[data[count]])
-                                            totalMaxY[data[count]] = y;
+		    // Min/Max
+					if (x < totalMinX[data[count]])
+						totalMinX[data[count]] = x;
+					else if (x > totalMaxX[data[count]])
+						totalMaxX[data[count]] = x;
+					if (y < totalMinY[data[count]])
+						totalMinY[data[count]] = y;
+					else if (y > totalMaxY[data[count]])
+						totalMaxY[data[count]] = y;
 				}
 			}
 			count+=3;
 		}
 	}
-//	cout<<"Made it through image, now creating useful data from arrays"<<endl;
+	//	cout<<"Made it through image, now creating useful data from arrays"<<endl;
 	int maxCount=0;
-
+  
         // Work from the top to bottom, collapsing the pixel clusters together
 	for (int i=index-1;i>0;i--)
 	{
-                
 		if (joins[i]!=i)
 		{
-                    // "Unfinished" cluster of pixels, ie part of bigger cluster
-                    // So add all of its information to the parents information
+			// "Unfinished" cluster of pixels, ie part of bigger cluster
+			// So add all of its information to the parents information
 			totalX[joins[i]]+=totalX[i];
 			totalY[joins[i]]+=totalY[i];
+			
+			// Mins
+			if (totalMinX[i] < totalMinX[joins[i]])
+				totalMinX[joins[i]] = totalMinX[i];
+			if (totalMinY[i] < totalMinY[joins[i]])
+				totalMinY[joins[i]] = totalMinY[i];
+
+			// Maxs
+			if (totalMaxX[i] > totalMaxX[joins[i]])
+				totalMaxX[joins[i]] = totalMaxX[i];
+			if (totalMaxY[i] > totalMaxY[joins[i]])
+				totalMaxY[joins[i]] = totalMaxY[i];
+
 			pixelCounts[joins[i]]+=pixelCounts[i];
 			pixelCounts[i]=0;
 		}
 		else
 		{
-                    // Found a final cluster
-                    m_blobs.push_back(
-                        BlobDetector::Blob(pixelCounts[i], totalX[i]/maxCount,
+			maxCount=pixelCounts[i];
+			// Found a final cluster
+			m_blobs.push_back(
+     			  BlobDetector::Blob(pixelCounts[i], totalX[i]/maxCount,
                              totalY[i]/maxCount, totalMaxX[i], totalMinX[i],
                              totalMaxY[i], totalMinY[i])
                                     );
@@ -219,11 +242,11 @@ int BlobDetector::histogram(IplImage* img)
 	free(totalX);
 	free(totalY);
 	free(joins);
-        free(totalMinX);
-        free(totalMaxX);
-        free(totalMinY);
-        free(totalMaxY);
-//	cout<<"Happily reaching the end of histogram"<<endl;
+	free(totalMinX);
+	free(totalMaxX);
+	free(totalMinY);
+	free(totalMaxY);
+	//	cout<<"Happily reaching the end of histogram"<<endl;
 	return maxCount;
 }
 
---
+++ packages/vision/test/src/TestBlobDetector.cxx
@@ -0,0 +1,74 @@
+
+/*
+ * Copyright (C) 2008 Robotics at Maryland
+ * Copyright (C) 2008 Daniel Hakim <dhakim@umd.edu>
+ * All rights reserved.
+ *
+ * Author: Daniel Hakim <dhakim@umd.edu>
+ * File:  packages/vision/test/src/TestOrangePipeDetector.cxx
+ */
+
+// Library Includes
+#include <UnitTest++/UnitTest++.h>
+
+// Project Includes
+#include "vision/include/BlobDetector.h"
+#include "vision/include/OpenCVImage.h"
+
+#include "vision/test/include/Utility.h"
+
+using namespace ram;
+
+struct BlobDetectorFixture
+{
+    BlobDetectorFixture() :
+        input(640, 480),
+        detector(core::ConfigNode::fromString("{}"))
+    {
+    }
+
+    vision::OpenCVImage input;
+    vision::BlobDetector detector;
+};
+
+SUITE(BlobDetector) {
+
+TEST_FIXTURE(BlobDetectorFixture, noBlobs)
+{
+    // Empty black image
+    vision::makeColor(&input, 0, 0, 0);
+
+    // Process it
+    vision::OpenCVImage output(640, 480);
+    detector.processImage(&input, &output);
+    //vision::Image::showImage(&output);
+
+    // Make sure we found no blobs
+    CHECK_EQUAL(0u, detector.getBlobs().size());
+}
+
+TEST_FIXTURE(BlobDetectorFixture, simpleBlobs)
+{
+    // Make image black
+    vision::makeColor(&input, 0, 0, 0);
+
+    // Add some white rectangles
+    drawSquare(&input, 200, 200, 100, 200, 0, CV_RGB(255,255,255));
+    //    drawSquare(&input, 350, 400, 100, 10, 0, CV_RGB(255,255,255));
+
+    // Process it
+    vision::OpenCVImage output(640, 480);
+    //    vision::Image::showImage(&input);
+    detector.processImage(&input, &output);
+	//    vision::Image::showImage(&output);
+
+    // Make sure we found no blobs
+    CHECK_EQUAL(1u, detector.getBlobs().size());
+    vision::BlobDetector::Blob blob = detector.getBlobs()[0];
+    CHECK_EQUAL(100, blob.getMaxX() - blob.getMinX());
+	// Broken right now!
+	//    CHECK_EQUAL(200, blob.getMaxY() - blob.getMinY());
+	CHECK_CLOSE(100 * 200, blob.getSize(), 500);
+}
+
+} // SUITE(BlobDetector)
