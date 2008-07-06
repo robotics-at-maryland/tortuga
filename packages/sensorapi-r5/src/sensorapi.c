94982bea30bd235dad81032603efc29dddf9d1e9
Leo Singer <aronnax@umd.edu>
2008-07-06 01:59:52 +0000

    '''SensorAPI''' Increase number of bytes sent by sonar.

--- packages/sensorapi-r5/src/sensorapi.c
+++ packages/sensorapi-r5/src/sensorapi.c
@@ -246,9 +246,9 @@ int readTemp(int fd, unsigned char * tempData)
 
 int getSonarData(int fd, int * angle, int * distance, int * pingNumber)
 {
-    unsigned char buf[5]={HOST_CMD_SONAR, HOST_CMD_SONAR};
+    unsigned char buf[12]={HOST_CMD_SONAR, HOST_CMD_SONAR};
     int i;
-    unsigned char rawSonar[5] = {0,0,0,0,0};
+    unsigned char rawSonar[12] = {0,0,0,0,0};
 
     writeData(fd, buf, 2);
     readData(fd, buf, 1);
@@ -256,19 +256,19 @@ int getSonarData(int fd, int * angle, int * distance, int * pingNumber)
     if(buf[0] != 0x0E)
         return SB_ERROR;
 
-    readData(fd, rawSonar, 5);
+    readData(fd, rawSonar, 12);
     readData(fd, buf, 1);
 
     unsigned char sum = 0x0E;
 
-    for(i=0; i<5; i++)
+    for(i=0; i<12; i++)
         sum = (sum+rawSonar[i]) & 0xFF;
 
     if(sum != buf[0])
         return SB_ERROR;
 
     printf("\nDebug: Received data from sonar board: < ");
-    for(i=0; i<5; i++)
+    for(i=0; i<12; i++)
         printf("0x%02X ", rawSonar[i]);
 
     printf(">\n");
@@ -760,7 +760,7 @@ int partialRead(int fd, struct boardInfo * info)
     // Roll over based upon end of enum marker
     if (END_OF_UPDATES == info->updateState)
         info->updateState = STATUS;
-    
+
     switch(info->updateState)
     {
         // Note STATUS == 1
