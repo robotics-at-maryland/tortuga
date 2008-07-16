657d549cd4593b8b68a9f435659e8d0c9bd303cd
Steve Mo <stevenm86@gmail.com>
2008-07-16 22:28:32 +0000

    '''Blackfin''' Add a test program.

--- sandbox/Blackfin/sonartest/testreport.c
+++ sandbox/Blackfin/sonartest/testreport.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <time.h>
 #include "report.h"
 
 
-int main()
+int main(int argc, char ** argv)
 {
+    int fd = openDevice();
+    if(fd < 0)
+    {
+        printf("Could not open device!\n");
+        return -1;
+    }
 
+    int ret = reportPing(fd, argc, 0.123456, 0.0, -0.98765,
+                         43210, time(0), 0xDEADBEEF);
+
+    printf("result was %d\n", ret);
+
+    closeDevice(fd);
+    return 0;
 }
