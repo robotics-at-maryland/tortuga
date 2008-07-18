0ccb2177d9627802b28e031ca7ef985b67d573ac
Leo Singer <aronnax@umd.edu>
2008-07-18 16:30:11 +0000

    '''SONAR''' Documentation for adctypes.h

--- packages/sonar/include/adctypes.h
+++ packages/sonar/include/adctypes.h
@@ -5,11 +5,54 @@
  * @author Leo Singer
  * @author Copyright 2007 Robotics@Maryland. All rights reserved.
  * 
+ * How to use:
+ * 
+ *   Choose the bit depth of your ADC -- say, 4 bits.
+ *   To declare variables that stores ADC values, do the following:
+ *
+ *     adctype<4>::SIGNED myNum;     // equivalent to char
+ *     adctype<4>::UNSIGNED myNum2;  // equivalent to unsigned char
+ *   
+ *   You may want to have another data type for numbers that are twice as wide
+ *   as your ADC for intermediate calculations.
+ *
+ *     adctype<4>::DOUBLE_WIDE::SIGNED myNum3;    // equivalent to char
+ *     adctype<4>::DOUBLE_WIDE::UNSIGNED myNum4;  // equivalent to unsigned char
+ *
+ *   Or, you might even want a quadruple wide type.
+ *   
+ *     adctype<4>::QUADRUPLE_WIDE::SIGNED myNum5;   // equivalent to short on most platforms
+ *     adctype<4>::QUADRUPLE_WIDE::UNSIGNED myNum6; // equivalent to unsigned short on most platforms
+ *   
+ *   You don't have to limit yourself to bit depths that are multiples of two.
+ *
+ *     adctype<3>::SIGNED myNum7;   // char
+ *     adctype<22>::DOUBLE_WIDE::UNSIGNED myNum8;    // unsigned int
+ *
+ *   If you request a type that cannot be created on your machine, then an error
+ *   will be generated.
+ *
+ *     adctype<32>::SIGNED myNum9;                    // works
+ *     adctype<32>::DOUBLE_WIDE::SIGNED myNum10;      // works
+ *     adctype<32>::QUADRUPLE_WIDE::SIGNED myNum11;   // FAILS
+ *     adctype<33>::SIGNED myNum12;                   // works
+ *     adctype<33>::DOUBLE_WIDE::SIGNED myNum13;      // FAILS
+ *     adctype<128>::SIGNED myNum14;                  // FAILS
+ *
+ *   You can get useful information about your ADC-specific types;
+ *
+ *     adctype<5>::SIGNED_MIN   // -32
+ *     adctype<5>::SIGNED_MAX   // 31
+ *     adctype<5>::DOUBLE_WIDE::UNSIGNED_MAX  // 1023
+ *     adctype<5>::QUADRUPLE_WIDE::BITDEPTH   // 20
+ *     adctype<5>::QUADRUPLE_WIDE::MACHINE_SIZE  // 4 (sizeof(int))
+ *
  */
 
 #ifndef _RAM_SONAR_ADCTYPES_H
 #define _RAM_SONAR_ADCTYPES_H
 
+#include <stdlib.h>
 #include <stdint.h>
 #include <limits.h>
 
@@ -67,6 +110,7 @@ public:
 	static const SIGNED SIGNED_MIN = -((int64_t)1 << (_bitDepth - 1));
 	static const SIGNED SIGNED_MAX =  ((int64_t)1 << (_bitDepth - 1)) - 1;
 	static const UNSIGNED UNSIGNED_MAX = ((~((uint64_t)0)) >> (64 - _bitDepth));
+	static const size_t MACHINE_SIZE = sizeof(SIGNED);
 };
 
 }} // namespace ram::sonar
