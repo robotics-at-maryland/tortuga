22695f42f136275d1f5a8ad9d8916019370e4320
Steve Mo <stevenm86@gmail.com>
2008-07-20 20:27:48 +0000

    '''sonar'''

---
+++ packages/drivers/bfin_spartan/testreport.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include "report.h"
+
+
+int main(int argc, char ** argv)
+{
+    int fd = openDevice();
+    if(fd < 0)
+    {
+        printf("Could not open device!\n");
+        return -1;
+    }
+
+	while(1)
+	{
+
+    int ret = reportPing(fd, argc, 0.123456, 0.0, -0.98765,
+                         43210, time(0), 0xDEADBEEF);
+
+    printf("result was %d\n", ret);
+		sleep(1);
+	}
+    closeDevice(fd);
+    return 0;
+}
