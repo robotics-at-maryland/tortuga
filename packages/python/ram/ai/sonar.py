d778998660c630ea9f507cac2e545964bf36bb44
Daniel Hakim <dhakimdahakim@gmail.com>
2008-07-21 03:40:49 +0000

    '''AI''' first SONAR ai, basic far tracking

---
+++ packages/python/ram/ai/sonar.py
@@ -0,0 +1,64 @@
+# Copyright (C) 2008 Maryland Robotics Club
+# Copyright (C) 2008 Joseph Lisee <jlisee@umd.edu>
+# All rights reserved.
+#
+# Author: Joseph Lisee <jlisee@umd.edu>
+# File:  packages/python/ram/ai/sonar.py
+  
+# STD Imports
+import math
+  
+# Project Imports
+import ext.core as core
+import ext.vehicle as vehicle
+import ext.vehicle.device
+import ext.math
+
+import ram.ai.state as state
+import ram.motion as motion
+#import ram.motion.search
+import ram.motion.pipe
+
+class FarSeeking(state.State):
+    CLOSE = core.declareEventType('CLOSE')
+    
+    @staticmethod
+    def transitions():
+        return {vehicle.device.ISonar.UPDATE : FarSeeking,
+                FarSeeking.CLOSE : End } 
+    
+    def UPDATE(self, event):
+        """Update the state of the light, this moves the vehicle"""
+        pingerOrientation = ext.math.Vector3.UNIT_X.getRotationTo(
+            event.direction)
+        
+        # x = 0, no sideways motion
+        # y = 1, full forward speed
+        self._pipe.setState(0, 1, pingerOrientation.getYaw(True))
+        
+        if math.fabs(event.direction.z) > math.fabs(self._closeZ):
+            self.publish(FarSeeking.CLOSE, core.Event())
+
+    def enter(self):
+        self._pipe = ram.motion.pipe.Pipe(0, 0, 0)
+
+        self._closeZ = self._config.get('closeZ', 0.8)
+        speedGain = self._config.get('speedGain', 5)
+        yawGain = self._config.get('yawGain', 1)
+        maxSpeed = self._config.get('forwardSpeed', 1)
+        
+        motion = ram.motion.pipe.Hover(pipe = self._pipe, 
+                                       maxSpeed = maxSpeed, 
+                                       maxSidewaysSpeed = 0, 
+                                       sidewaysSpeedGain = 0, 
+                                       speedGain = speedGain, 
+                                       yawGain = yawGain)
+        self.motionManager.setMotion(motion)
+
+    def exit(self):
+        #print '"Exiting Seek, going to follow"'
+        self.motionManager.stopCurrentMotion()
+        
+class End(state.State):
+    def enter(self):
+        print "End"
---
+++ packages/python/ram/test/ai/sonar.py
@@ -0,0 +1,60 @@
+# Copyright (C) 2008 Maryland Robotics Club
+# Copyright (C) 2008 Joseph Lisee <jlisee@umd.edu>
+# All rights reserved.
+#
+# Author: Joseph Lisee <jlisee@umd.edu>
+# File:  packages/python/ram/test/ai/pipe.py
+
+# Python Imports
+import unittest
+
+# Project Imports
+import ram.ai.sonar as sonar
+import ext.core as core
+import ext.vehicle as vehicle
+import ext.vehicle.device
+
+import ram.motion as motion
+import ram.motion.pipe
+
+import ram.test.ai.support as aisupport
+
+class TestSeeking(aisupport.AITestCase):
+    def setUp(self):
+        aisupport.AITestCase.setUp(self)
+        self.machine.start(sonar.FarSeeking)
+    
+    def testStart(self):
+        self.assertCurrentMotion(motion.pipe.Hover)
+        
+        
+    def testUpdate(self):
+        # To the left of the vehicle
+        self.injectEvent(vehicle.device.ISonar.UPDATE, vehicle.SonarEvent, 
+                         direction = ext.math.Vector3(0.5, 0.8, -0.1))
+        
+        self.assertGreaterThan(self.controller.speed, 0)
+        self.assertAlmostEqual(0, self.controller.sidewaysSpeed, 0)
+        self.assertGreaterThan(self.controller.yawChange, 0)
+        
+        self.assertCurrentState(sonar.FarSeeking)
+        
+        # To the right
+        self.injectEvent(vehicle.device.ISonar.UPDATE, vehicle.SonarEvent, 
+                         direction = ext.math.Vector3(0.8, -0.5, -0.1))
+        
+        self.assertGreaterThan(self.controller.speed, 0)
+        self.assertAlmostEqual(0, self.controller.sidewaysSpeed, 0)
+        self.assertLessThan(self.controller.yawChange, 0)
+        
+        self.assertCurrentState(sonar.FarSeeking)
+        
+    def testClose(self):
+        self.assertCurrentState(sonar.FarSeeking)
+        
+        # Be so close as to trigger the transition
+        self.injectEvent(vehicle.device.ISonar.UPDATE, vehicle.SonarEvent, 
+                         direction = ext.math.Vector3(-0.29, 0.3, -0.85))
+        self.qeventHub.publishEvents()
+        # Make sure we hit the end state
+        self.assert_(self.machine.complete)
