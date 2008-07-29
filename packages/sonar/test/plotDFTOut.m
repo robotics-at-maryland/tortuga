ec22d7dc3b98a6bcaf2f38cca6f6367d3a4c38a0
Leo Singer <aronnax@umd.edu>
2008-07-29 05:41:52 +0000

    '''SONAR''' Minor changes to spectrogram plotter

--- packages/sonar/test/plotDFTOut.m
+++ packages/sonar/test/plotDFTOut.m
@@ -1,14 +1,26 @@
-function plotDFTOut
-  fid = fopen('dft.out');
+function plotDFTOut(fname, kBands)
+% Display a spectrogram of the given file named fname.
+% The optional kBands parameter selects the (C-style indexed) Fourier
+% bins to plot.
+  fid = fopen(fname);
   dat = fread(fid, Inf, 'int32');
   fclose(fid);
-  M = length(dat)/512;
-  dat = reshape(dat, 512, M);
+  N = 512;
+  M = length(dat)/N;
+  dat = reshape(dat, N, M);
+  
+  if nargin == 0
+    kBands = 0:(N-1);
+  end
 
-  imagesc(dat);
+  fs = 500000;
+  Ts = 1/fs;
+  T = 1:M;
+  F = kBands/N*fs;
+  imagesc(T, F/1000, dat(1+kBands,:));
   xlabel('Sample index','FontSize',14);
-  ylabel('Frequency bin','FontSize',14);
-  title('Anomaly between fixed point sliding DFT and FFTW, N=512', ...
-	'FontSize',14);
+  ylabel('Frequency (kHz)','FontSize',14);
+  title('Spectrogram', ...
+        'FontSize',14);
   colorbar;
 end
