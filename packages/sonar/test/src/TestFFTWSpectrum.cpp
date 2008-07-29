ff42240fc8b77b7a0edf7b2e8308b74c8a499631
Leo Singer <aronnax@umd.edu>
2008-07-29 05:15:03 +0000

    '''SONAR''' Corrected typecasts in unit test

--- packages/sonar/test/src/TestFFTWSpectrum.cpp
+++ packages/sonar/test/src/TestFFTWSpectrum.cpp
@@ -26,11 +26,11 @@ TEST(FFTWSpectrumGivesSameResultsAsSDFT)
 	FFTWSpectrum<myadc, N, nChannels> refSpectrum;
 	SDFTSpectrum<myadc, N, nChannels> sbjSpectrum;
 	
-	for (int i = 0 ; i < nSamples ; i ++)
+	for (long i = 0 ; i < nSamples ; i ++)
 	{
-		adcdata_t sample[nChannels];
+        myadc::SIGNED sample[nChannels];
 		for (int channel = 0 ; channel < nChannels ; channel ++)
-			sample[channel] = (adcdata_t)(((double)rand() / RAND_MAX) * (1 << 15));
+			sample[channel] = (myadc::SIGNED)(((double)rand() / RAND_MAX) * myadc::SIGNED_MAX);
 		
 		refSpectrum.update(sample);
 		sbjSpectrum.update(sample);
