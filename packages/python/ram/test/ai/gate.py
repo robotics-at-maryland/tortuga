1a4ee2f66f218fe3ce73bc340640f69d4124ef72
Joseph Lisee <jlisee@gmail.com>
2008-08-01 15:29:00 +0000

    '''AI''' tested to make sure parameters are read properly

--- packages/python/ram/test/ai/gate.py
+++ packages/python/ram/test/ai/gate.py
@@ -16,6 +16,8 @@ import ram.motion.basic
 
 import ram.test.ai.support as support
 
+from ram.test.motion.support import MockTimer
+
 class TestGate(support.AITestCase):
     def setUp(self):
         cfg = {
@@ -39,17 +41,25 @@ class TestGate(support.AITestCase):
         
         # Ensure we actually started diving
         self.assertCurrentMotion(motion.basic.RateChangeDepth)
+        
+        # Test the config settings
+        m = self.motionManager.currentMotion
+        self.assertEqual(7, m._desiredDepth)
+        self.assertEqual(2, m._speed)
                 
         # Now make sure we go to forward after diving
         self.injectEvent(motion.basic.Motion.FINISHED)   
         self.assertCurrentState(gate.Forward)   
-                
-        #self.assert_(self.motionManager.stopped)
+
         
     def testForward(self):
         # Make sure we start driving forward
         self.machine.start(gate.Forward)
+        
+        # Check config settings
         self.assertEqual(5, self.controller.speed)
+        self.assertEqual(15, MockTimer.LOG[gate.Forward.DONE]._sleepTime)
+        
         
         # Now make sure we stop
         self.releaseTimer(gate.Forward.DONE)
