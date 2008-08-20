ed552bc1871043c726c961564a8965d05799137d
Leo Singer <aronnax@umd.edu>
2008-08-20 03:34:18 +0000

    '''ICE''' Added vehicle math libraries

---
+++ src/math/include/Events.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2008 Robotics at Maryland
+ * Copyright (C) 2008 Joseph Lisee <jlisee@umd.edu>
+ * All rights reserved.
+ *
+ * Author: Joseph Lisee <jlisee@umd.edu>
+ * File:  packages/math/include/Events.h
+ */
+
+#ifndef RAM_MATH_EVENTS_01_08_2008
+#define RAM_MATH_EVENTS_01_08_2008
+
+// Project Includes
+#include "core/include/Event.h"
+#include "math/include/Quaternion.h"
+#include "math/include/Vector3.h"
+
+namespace ram {
+namespace math {
+
+struct OrientationEvent : public core::Event
+{
+    Quaternion orientation;
+};
+
+typedef boost::shared_ptr<OrientationEvent> OrientationEventPtr;
+
+struct Vector3Event : public core::Event
+{
+    Vector3 vector3;
+};
+
+typedef boost::shared_ptr<Vector3Event> Vector3EventPtr;
+    
+struct NumericEvent : public core::Event
+{
+    double number;
+};
+
+typedef boost::shared_ptr<NumericEvent> NumericEventPtr;
+    
+} // namespace math
+} // namespace ram
+
+#endif // RAM_MATH_EVENTS_01_08_2008
