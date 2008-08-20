b7d2b477a4cd50587ae4c7f582e4d760f774791b
cgiles17 <software@ram.umd.edu>
2008-08-20 05:40:01 +0000

    worked on shader

--- src/sim/Sim.h
+++ src/sim/Sim.h
@@ -1,7 +1,29 @@
 #ifndef SIM_H
 #define SIM_H
 
+// Core
+
+#include "math/include/Vector2.h"
+#include "math/include/Vector3.h"
+#include "math/include/Vector4.h"
+#include "math/include/Matrix2.h"
+#include "math/include/Matrix3.h"
+#include "math/include/Matrix4.h"
+#include "math/include/Quaternion.h"
+
+typedef ram::math::Vector2 Vec2;
+typedef ram::math::Vector3 Vec3;
+typedef ram::math::Vector4 Vec4;
+typedef ram::math::Matrix2 Mat2;
+typedef ram::math::Matrix3 Mat3;
+typedef ram::math::Matrix4 Mat4;
+typedef ram::math::Quaternion Quat;
+
+// Simulation
+
 #include "vehicle/SimVehicleFactory.h"
+
+// Graphics
 
 #include "Shader.h"
 
