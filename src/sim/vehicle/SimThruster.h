e8e45f9da7929af3c31137f75b3f5984c86c24f2
Leo Singer <aronnax@umd.edu>
2008-08-23 17:09:02 +0000

    '''ICE''' Added more details to Thruster stub

--- src/sim/vehicle/SimThruster.h
+++ src/sim/vehicle/SimThruster.h
@@ -8,45 +8,59 @@
 
 namespace ram {
     namespace sim {
-        class SimThruster : virtual public vehicle::IDepthSensor, public SimDevice
+        class SimThruster : virtual public vehicle::IThruster, public SimDevice, public BuoyantBody
         {
         private:
             btTransform localTransform;
+            btDouble force;
+            bool enabled;
             SimVehicle& parent;
         public:
             SimThruster(SimVehicle& mParent, std::string mName) : SimDevice(mName), parent(mParent), localTransform() {}
             
-            virtual void setForce(::Ice::Double, const ::Ice::Current& = ::Ice::Current())
+            virtual void setForce(::Ice::Double mForce, const ::Ice::Current&c = ::Ice::Current())
             {
+                if (mForce < getMinForce())
+                    force = getMinForce();
+                else if (mForce > getMaxForce())
+                    force = getMaxForce();
+                else
+                    force = mForce;
             }
             
             virtual ::Ice::Double getForce(const ::Ice::Current& = ::Ice::Current())
             {
+                return force;
             }
             
             virtual ::Ice::Double getMaxForce(const ::Ice::Current& = ::Ice::Current())
             {
+                return 100; /* TODO */
             }
             
             virtual ::Ice::Double getMinForce(const ::Ice::Current& = ::Ice::Current())
             {
+                return -100; /* TODO */
             }
             
             virtual ::Ice::Double getOffset(const ::Ice::Current& = ::Ice::Current())
             {
+                return 0;
             }
             
             virtual bool isEnabled(const ::Ice::Current& = ::Ice::Current())
             {
+                return enabled;
             }
             
-            virtual void setEnabled(bool, const ::Ice::Current& = ::Ice::Current())
+            virtual void setEnabled(bool state, const ::Ice::Current& = ::Ice::Current())
             {
+                enabled = state;
             }
             
             virtual void stepSimulation(SimWorld& world, btScalar timeStep)
             {
-                depth = world.getFluidLevel() + (parent.getCenterOfMassTransform() * localPos).dot(world.getGravity().normalized());
+                /* TODO */
             }
         };
     }
