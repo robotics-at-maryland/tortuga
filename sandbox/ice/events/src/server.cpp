01ff6d4a62876089577d8429d138cce49a7b6cb1
Leo Singer <aronnax@umd.edu>
2008-09-02 00:49:33 +0000

    '''ICE''' Made things just a little bit more readable

--- sandbox/ice/events/src/server.cpp
+++ sandbox/ice/events/src/server.cpp
@@ -5,27 +5,35 @@
 
 class Server : public ClientOrServer {
 public:
+	
+	timeval start;
+	
+	ram::events::SonarEvent randomPing()
+	{
+		timeval now;
+		gettimeofday(&now, NULL);
+		ram::events::SonarEvent evt;
+		evt.range = (double)rand() / RAND_MAX * 100.0;
+		evt.pingTimeSec = now.tv_sec - start.tv_sec;
+		evt.pingTimeUSec = now.tv_usec - start.tv_usec;
+		return evt;
+	}
+		
 	virtual int run(int argc, char* argv[])
 	{
 		ClientOrServer::run(argc, argv);
+		gettimeofday(&start, NULL);
 		
-		ram::events::SonarEventListenerPrx sonarListener =
+		// Get a proxy to the publisher for this event
+		ram::events::SonarEventListenerPrx publisher =
 		ram::events::SonarEventListenerPrx::uncheckedCast(sonarTopic->getPublisher());
 		
-		timeval start;
-		gettimeofday(&start, NULL);
 		while (true)
 		{
-			timeval now;
-			gettimeofday(&now, NULL);
-			ram::events::SonarEvent evt;
-			evt.range = (double)rand() / RAND_MAX * 100.0;
-			evt.pingTimeSec = now.tv_sec - start.tv_sec;
-			evt.pingTimeUSec = now.tv_usec - start.tv_usec;
 			cerr << "Ping!" << endl;
-			sonarListener->pingReceived(evt);
+			publisher->pingReceived(randomPing());
 			
-			// Keep going until the program is killed
+			// wait 1 second
 			IceUtil::ThreadControl::sleep(IceUtil::Time::seconds(1));
 		}
 		
