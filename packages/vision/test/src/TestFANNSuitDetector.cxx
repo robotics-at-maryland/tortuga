9d56b556acb1b7c2def58641b539bfc069aaf4b4
loved <software@ram.umd.edu>
2009-02-20 04:38:58 +0000

    '''Vision'''  - Added unit test for FANNSuitDetector

---
+++ packages/vision/test/src/TestFANNSuitDetector.cxx
@@ -0,0 +1,73 @@
+
+/*
+ * Copyright (C) 2008 Robotics at Maryland
+ * Copyright (C) 2008 David Love
+ * All rights reserved.
+ *
+ * Author: David Love <loved@umd.edu>
+ * File:  packages/vision/test/src/TestFANNSuitDetector.cpp
+ */
+
+// STD Includes
+#include <cmath>
+#include <set>
+#include <iostream>
+
+// Library Includes
+#include <UnitTest++/UnitTest++.h>
+
+#include <boost/bind.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/foreach.hpp>
+
+// Project Includes
+#include "core/include/ConfigNode.h"
+#include "vision/include/Image.h"
+#include "vision/include/OpenCVImage.h"
+#include "vision/include/FANNSuitDetector.hpp"
+
+using namespace ram;
+
+static boost::filesystem::path getImagesDir()
+{
+    boost::filesystem::path root(getenv("RAM_SVN_DIR"));
+    return root / "packages" / "vision" / "test" / "data" / "images" / "testfann";
+}
+
+static std::string getSuitNetworkFile()
+{
+    boost::filesystem::path root(getenv("RAM_SVN_DIR"));
+    return (root / "packages" / "vision" / "test" / "data" / "suits.irn").file_string();
+}
+
+struct FANNSuitDetectorFixture
+{
+};
+
+SUITE(FANNSuitDetector) {
+
+TEST_FIXTURE(FANNSuitDetectorFixture, FANNSuitDetectorTest)
+{
+    core::ConfigNode config = core::ConfigNode::fromString ("{ 'SavedImageIdentifierNetwork' : '" + getSuitNetworkFile() + "' }");
+    vision::FANNSuitDetector detector = vision::FANNSuitDetector (config);
+    std::cout << "Starting FANNSuitDetectorTest";
+    vision::Image* img;
+	boost::filesystem::directory_iterator end;
+    boost::filesystem::path dir = getImagesDir();
+    CHECK (boost::filesystem::exists (dir));
+    int results[4] = { vision::Suit::CLUB, vision::Suit::DIAMOND, vision::Suit::HEART, vision::Suit::SPADE };
+    unsigned int i = 0;
+	for (boost::filesystem::directory_iterator itr (dir); itr != end; ++itr) {
+        if (boost::filesystem::extension (itr->path()) == "jpg") {
+            img = vision::Image::loadFromFile(itr->path().file_string());
+            if (img) {
+                detector.processImage(img);
+            }
+            CHECK(detector.getSuit() == results[i++]);
+        }
+        std::cout << ".";
+	}
+    std::cout << " done\n";
+}
+    
+} // SUITE(FANNSuitDetector)
