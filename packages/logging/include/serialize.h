d5607e918ada34375d381ad973d3482ce951b32c
jedmonds <software@ram.umd.edu>
2009-03-06 00:23:00 +0000

    Adding serialization code.

---
+++ packages/logging/include/serialize.h
@@ -0,0 +1,61 @@
+#ifndef LOGGING_SERIALIZE_H
+#define LOGGING_SERIALIZE_H
+
+#include<boost/serialization/serialization.hpp>
+
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+#include<fstream>
+
+#include "vision/include/Events.h"
+
+
+template <class Archive> void serialize(Archive &ar, ram::vision::ImageEventPtr &t, const unsigned int file_version)
+{
+  ar & t->image;
+}
+template <class Archive> void serialize(Archive &ar, ram::vision::RedLightEventPtr &t, const unsigned int file_version)
+{
+  ar & t->y;
+  ar & t->azimuth;
+  ar & t->elevation;
+  ar & t->range;
+  ar & t->x;
+  ar & t->y;
+  ar & t->pixCount;
+}
+template <class Archive> void serialize(Archive &ar, ram::vision::PipeEventPtr &t, const unsigned int file_version)
+{
+  ar & t->y;
+  ar & t->x;
+  ar & t->y;
+  ar & t->angle;
+}
+template <class Archive> void serialize(Archive &ar, ram::vision::BinEventPtr &t, const unsigned int file_version)
+{
+  ar & t->y;
+  ar & t->y;
+  ar & t->x;
+  ar & t->y;
+  ar & t->id;
+  ar & t->suit;
+  ar & t->angle;
+}
+template <class Archive> void serialize(Archive &ar, ram::vision::DuctEventPtr &t, const unsigned int file_version)
+{
+  ar & t->y;
+  ar & t->x;
+  ar & t->y;
+  ar & t->range;
+  ar & t->alignment;
+  ar & t->aligned;
+  ar & t->visible;
+}
+template <class Archive> void serialize(Archive &ar, ram::vision::SafeEventPtr &t, const unsigned int file_version)
+{
+  ar & t->x;
+  ar & t->y;
+}
+
+#endif
---
+++ packages/logging/test/src/TestSerialize.cxx
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2007 Robotics at Maryland
+ * Copyright (C) 2007 Joseph Lisee <jlisee@umd.edu>
+ * All rights reserved.
+ *
+ * Author: Joseph Lisee <jlisee@umd.edu>
+ * File:  packages/math/test/src/TestChecks.cxx
+ */
+
+// Library Includes
+#include <UnitTest++/UnitTest++.h>
+
+// Project Includes
+#include "math/test/include/MathChecks.h"
+#include "logging/include/serialize.h"
+
+// Bring Vector3, and other math classes into scope
+using namespace ram::math;
+
+TEST(ChangeMe)
+{
+  /*ofstream outfile('LoggingTestChangeMeOutput');
+  outfile << ram::vision::DuctEventPtr(ram::vision::DuctEvent(1.0, 1.0, 1.0, true, true));
+  outfile.close();
+  double a = 1;
+  double b = 1.2;
+  CHECK_CLOSE(a, b, 0.6);*/
+}
+
