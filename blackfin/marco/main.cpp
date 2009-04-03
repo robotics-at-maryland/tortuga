5c2af23ae24bedd67c4b145ef92c0ed08dd01dcf
Leo Singer <aronnax@umd.edu>
2009-04-03 03:40:25 +0000

    First attempt at compiling Ice code on Blackfin

---
+++ blackfin/marco/main.cpp
@@ -0,0 +1,14 @@
+/*
+ *  main.cpp
+ *  
+ *
+ *  Created by auvsi on 4/2/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+
+int main(int argc, const char* argv[])
+{
+	return 0;
+}
