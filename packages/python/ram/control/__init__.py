c0c9bf3d86f10a36159d4053543145881d8f5720
Joseph Lisee <jlisee@gmail.com>
2009-04-16 23:49:16 +0000

    '''Python & Control''' Added directory for control

---
+++ packages/python/ram/control/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2009 Maryland Robotics Club
+# Copyright (C) 2009 Joseph Lisee <jlisee@umd.edu>
+# All rights reserved.
+#
+# Author: Joseph Lisee <jlisee@umd.edu>
+# File:  packages/python/ram/control/__init__.py
