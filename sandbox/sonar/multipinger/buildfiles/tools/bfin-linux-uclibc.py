bf851ec7f55e7582920eff1ff0e10f41b81ab643
Leo Singer <aronnax@umd.edu>
2009-04-18 21:23:42 +0000

    '''SONAR''' First commit of dump.c

---
+++ sandbox/sonar/multipinger/buildfiles/tools/bfin-linux-uclibc.py
@@ -0,0 +1,28 @@
+import sys
+import os.path
+import SCons.Builder
+import SCons.Tool.cc
+import SCons.Tool.ar
+import SCons.Tool.link
+
+toolfamily = 'bfin-linux-uclibc-'
+
+compilers = [toolfamily+'gcc']
+
+def exists(env):
+    checkEnv = env.Clone()
+    return(checkEnv.Detect(toolfamily+'gcc'))
+
+def generate(env):
+    SCons.Tool.cc.generate(env)
+    SCons.Tool.ar.generate(env)
+    SCons.Tool.link.generate(env)
+    env['CC'] = toolfamily+'gcc'
+    env['AS'] = toolfamily+'gcc'
+    env['RANLIB'] = toolfamily+'ranlib'
+    env['AR'] = toolfamily+'ar'
+    env['BIN2HEX'] = toolfamily+'bin2hex'
+    env['CCFLAGS'] = []
+    env['OBJSUFFIX'] = '.o'
+    env['PROGSUFFIX'] = '.coff'
+    env['ASCOM']     = '$AS $ASFLAGS -o $TARGET -c $SOURCES'
