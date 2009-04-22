ecc95933e7ad6d8ad9a49800f00705f5b3d09888
Joseph Galante <glandjm@gmail.com>
2009-04-22 00:49:43 +0000

    '''SLAM sims''' fixed EKF sim    it works

--- packages/control/matlab/SLAMsims/ekfSLAM/ekfSLAMsimMain.m
+++ packages/control/matlab/SLAMsims/ekfSLAM/ekfSLAMsimMain.m
@@ -17,21 +17,32 @@ c=11.5;%robot drag (vehicle assumed to have symmetric drag)
 m=28;%robot mass (in kg)
 
 
-t_end = 75; % Seconds
+t_end = 200; % Seconds
 Ts=1;%seconds
 %k=1:floor(t_end/Ts);
 time=Ts:Ts:Ts*floor(t_end/Ts);
 
 %% dynamics for state ESTIMATES
+% for xhatdot = A*xhat
+% A=[0 0 1 0 0 0 0 0;
+%    0 0 0 1 0 0 0 0;
+%    0 0 -c/m 0 0 0 0 0;
+%    0 0 0 -c/m 0 0 0 0;
+%    0 0 0 0 -.000001 0 0 0;
+%    0 0 0 0 0 -.000001 0 0;
+%    0 0 0 0 0 0 -.000001 0;
+%    0 0 0 0 0 0 0 -.000001];
+
 A=[0 0 1 0 0 0 0 0;
    0 0 0 1 0 0 0 0;
    0 0 -c/m 0 0 0 0 0;
    0 0 0 -c/m 0 0 0 0;
-   0 0 0 0 -.1 0 0 0;
-   0 0 0 0 0 -.1 0 0;
-   0 0 0 0 0 0 -.1 0;
-   0 0 0 0 0 0 0 -.1];
+   0 0 0 0 0 0 0 0;
+   0 0 0 0 0 0 0 0;
+   0 0 0 0 0 0 0 0;
+   0 0 0 0 0 0 0 0];
 
+% for xhatdot = A*xhat+B*random_forces
 B=[0 0;
    0 0;
    1 0;
@@ -49,10 +60,10 @@ D = [0 0; 0 0];
 
 
 %covariance of process noise (arbitrarily chosen) 
-%Rv_cont=diag([8 8]);%in N   CONTINUOUS TIME, needs discretization
-Rv_cont=1e-8*diag([8 8]);
+Rv_cont=diag([8 8]);%in N   CONTINUOUS TIME, needs discretization
+%Rv_cont=1e-8*diag([8 8]);
 %covariance of sensor noise
-Rn=(pi/180)*diag([0.02 0.02]);%in radian
+Rn=(pi/180)*diag([2 2]);%in radian
 
 %[n,m]=size(A);
 n=length(A);%SHOULD produce the # of columns
@@ -89,8 +100,8 @@ Ak_prev = Ak; % "A" matrix is LTI so Ak_prev and Ak wont change
 %initial state estimate
 xhat0 = [ 6;  % x       
        -4;  % y            
-       -2;  % x_dot
-       2;  % y_dot
+       10;  % x_dot
+       -10;  % y_dot
        -50;  % x1  (Pinger 1)
        100;  % y1  (Pinger 1)
        50;   % x2 (Pinger 2)
@@ -99,8 +110,8 @@ xhat0 = [ 6;  % x
 %true initial state   
 x0 = [3; %x
       -2; %y
-      -10; %x_dot
-      10; %y_dot
+      0; %x_dot
+      0; %y_dot
       -50; %x1
       100; %y1
       50; %x2
@@ -138,8 +149,8 @@ for k=1:floor(t_end/Ts)
     %angle to pinger 2
     %theta2=atan2(y2-y,x2-x)
     theta2=atan2(x(8,k)-x(2,k),x(7,k)-x(1,k));
-    %y(:,k)=[theta1; theta2]+noise_sensor;
-    y(:,k)=[theta1; theta2];
+    y(:,k)=[theta1; theta2]+noise_sensor;
+    %y(:,k)=[theta1; theta2];
     
     %%%%%%%%%%%%% run EKF
     %predict step
@@ -150,15 +161,15 @@ for k=1:floor(t_end/Ts)
     %
     %update step
     %compute C matrix (measurement model)
-    C=iterateMeasModel(x(:,k));
+    Citer=iterateMeasModel(x(:,k));
     %compute kalman gain
-    K=P_pred*C'*inv(C*P_pred*C'+Rn);%kalman gain
+    K=P_pred*Citer'*inv(Citer*P_pred*Citer'+Rn);%kalman gain
     %K=P_pred*C'*inv(C*P_pred*C')%kalman gain
     %state update
     y1est=atan2(xhat_pred(6)-xhat_pred(2),xhat_pred(5)-xhat_pred(1));
     y2est=atan2(xhat_pred(8)-xhat_pred(2),xhat_pred(7)-xhat_pred(1));
     xhat(:,k)=xhat_pred+K*(y(:,k)-[y1est y2est ]');
-    P_t=(I-K*C)*P_pred;
+    P_t=(I-K*Citer)*P_pred;
     
     %%%%%%%%%%%%% bookkeeping
     P(:,k)=P_t(:);
@@ -168,27 +179,59 @@ for k=1:floor(t_end/Ts)
 end
 
 %position plot
-figure(1)
+figure('name','Simulation Map')
 %plot(x(1,:),x(2,:),xhat(1,:),xhat(2,:))
-plot(x(1,:),x(2,:),'*',xhat(1,:),xhat(2,:),'-')
-legend('x','xhat')
+plot(x(1,:),x(2,:),'-',xhat(1,:),xhat(2,:),'-',x0(5),x0(6),'b*',x0(7),x0(8),'r*')
+legend('x','xhat','Pinger 1','Pinger 2')
 xlabel('x')
 ylabel('y')
 
+
 %estimates and true states of robot
-figure(2)
+figure('name','Robot States')
 subplot(4,1,1)
-plot(time,x(1,:),time,xhat(1,:))
+plot(time,x(1,:),time,xhat(1,:),time,xhat(1,:)+sqrt(P(1,:)),'r-.',time,xhat(1,:)-sqrt(P(1,:)),'r-.')
+ylabel('x')
+legend('true','estimated','\sigma')
+subplot(4,1,2)
+plot(time,x(2,:),time,xhat(2,:),time,xhat(2,:)+sqrt(P(10,:)),'r-.',time,xhat(2,:)-sqrt(P(10,:)),'r-.')
+ylabel('y')
+subplot(4,1,3)
+plot(time,x(3,:),time,xhat(3,:),time,xhat(3,:)+sqrt(P(19,:)),'r-.',time,xhat(3,:)-sqrt(P(19,:)),'r-.')
+ylabel('xdot')
+subplot(4,1,4)
+plot(time,x(4,:),time,xhat(4,:),time,xhat(4,:)+sqrt(P(28,:)),'r-.',time,xhat(4,:)-sqrt(P(28,:)),'r-.')
+ylabel('ydot')
+
+% error in xhat
+% figure('name','xHat Error')
+% subplot(4,1,1)
+% plot(time,P(1,:))
+% subplot(4,1,2)
+% plot(time,P(10,:))
+% subplot(4,1,3)
+% plot(time,P(19,:))
+% subplot(4,1,4)
+% plot(time,P(28,:))
+
+%estimates and true states of pinger locations
+figure('name','Pinger States')
+subplot(4,1,1)
+plot(time,x(5,:),time,xhat(5,:),time,xhat(5,:)+sqrt(P(37,:)),'r-.',time,xhat(5,:)-sqrt(P(37,:)),'r-.')
+ylabel('x1')
 legend('true','estimated')
 subplot(4,1,2)
-plot(time,x(2,:),time,xhat(2,:))
+plot(time,x(6,:),time,xhat(6,:),time,xhat(6,:)+sqrt(P(46,:)),'r-.',time,xhat(6,:)-sqrt(P(46,:)),'r-.')
+ylabel('y1')
 subplot(4,1,3)
-plot(time,x(3,:),time,xhat(3,:))
+plot(time,x(7,:),time,xhat(7,:),time,xhat(7,:)+sqrt(P(55,:)),'r-.',time,xhat(7,:)-sqrt(P(55,:)),'r-.')
+ylabel('x2')
 subplot(4,1,4)
-plot(time,x(4,:),time,xhat(4,:))
+plot(time,x(8,:),time,xhat(8,:),time,xhat(8,:)+sqrt(P(64,:)),'r-.',time,xhat(8,:)-sqrt(P(64,:)),'r-.')
+ylabel('y2')
 
 %measurement plot
-figure(3)
+figure('name','Measurements')
 subplot(2,1,1)
 plot(time,y(1,:))
 ylabel('\theta_1')
