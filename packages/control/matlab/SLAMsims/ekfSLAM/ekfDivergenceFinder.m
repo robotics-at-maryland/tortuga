aece9d1147e0a61578c02cb42bec0d8e6f7d97d1
ajanas <software@ram.umd.edu>
2009-05-09 00:06:27 +0000

    Updated to include a simulation for random forces and a color plot indicating divergence amounts

--- packages/control/matlab/SLAMsims/ekfSLAM/ekfDivergenceFinder.m
+++ packages/control/matlab/SLAMsims/ekfSLAM/ekfDivergenceFinder.m
@@ -21,21 +21,37 @@ Rv_cont=1e-2*diag([2 2]);%in N   CONTINUOUS TIME, needs discretization
             50; %x2
             100]; %y2
 
+xtotal = -124:2:124;
+ytotal = 80:2:120;
+DivergeArray = zeros(length(xtotal)*2,length(ytotal)*2); % This defines an x,y position and the value at each x,y is the # of cases it failed        
+        
+        
 figure('name','Divergence Map')
-AXIS([-100 100 50 150])
+AXIS([-150 150 50 150])
 hold on
-plot(x0(5,end),x0(6,end),'ro')
-plot(x0(7,end),x0(8,end),'ro')
+AXIS square
+rectangle ('position', [x0(5,end)-2.5,x0(6,end)-2.5, 5, 5], 'curvature', [1, 1],'LineWidth',2)
+rectangle ('position', [x0(7,end)-2.5,x0(8,end)-2.5, 5, 5], 'curvature', [1, 1],'LineWidth',2)
+rectangle ('position', [min(xtotal),min(ytotal), max(xtotal)-min(xtotal), max(ytotal)-min(ytotal)],'LineWidth',2)
 wbar = waitbar(0,'Plotting the end of humanity...');
 
-xtotal = -70:2:70;
-ytotal = 80:2:120;
-DivergeArray = zeros(length(xtotal)*2,length(ytotal)*2); % This defines an x,y position and the value at each x,y is the # of cases it failed
-size(DivergeArray)
 
 
-for x = -70:2:70
-    waitbar((x+70)/140,wbar);
+color = [1 1 1;  %This uses RGB values to give intensity for the # of failures
+         0.7 1 1;  
+         0.3 1 1;  
+         0 0.5 1;  
+         0.3 0.3 1; 
+         1 0.9 0.2; 
+         1 0.7 0; 
+         1 0.5 0; 
+         1 0.3 0; 
+         1 0 0]; 
+
+
+
+for x = -124:2:124
+    waitbar((x+max(xtotal))/(2*max(xtotal)),wbar);
     for y = 80:2:120
         %true initial state
         x0 =[x; %x
@@ -62,16 +78,34 @@ for x = -70:2:70
                 Divergence = ekfSLAMFunc(xhat0,x0,plotData,Rv_cont,divergence_criteria);
                 if Divergence == 1
                     j = j+1;
-                    DivergeArray(x+length(xtotal),y-min(ytotal)+1) = j; %Counts # of diverged pts
-                    %plot(x,y,'x')                 
+                    %plot(x,y,'x')
                 end
-                
+
             end
         end
+        DivergeArray(x+length(xtotal),y-min(ytotal)+1) = j; %Counts # of diverged pts
+
+        plot(x,y,'s',...
+            'MarkerEdgeColor',color(j+1,:),...
+            'MarkerFaceColor',color(j+1,:),...
+            'MarkerSize',10)
+
+    
+    
+    
     end
 end
 
+
+
+
+
+
+rectangle ('position', [x0(5,end)-2.5,x0(6,end)-2.5, 5, 5], 'curvature', [1, 1],'LineWidth',2)
+rectangle ('position', [x0(7,end)-2.5,x0(8,end)-2.5, 5, 5], 'curvature', [1, 1],'LineWidth',2)
+rectangle ('position', [min(xtotal),min(ytotal), max(xtotal)-min(xtotal), max(ytotal)-min(ytotal)],'LineWidth',2)
 close(wbar);
+AXIS square
 hold off
 warning on all;
 
