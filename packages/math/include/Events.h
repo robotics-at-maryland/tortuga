bf8aff662c36d9145c8307dcc605e502db6882fa
Joseph Lisee <jlisee@gmail.com>
2009-06-25 02:20:08 +0000

    '''Math''' Added a Vector2Event

--- packages/math/include/Events.h
+++ packages/math/include/Events.h
@@ -13,6 +13,7 @@
 // Project Includes
 #include "core/include/Event.h"
 #include "math/include/Quaternion.h"
+#include "math/include/Vector2.h"
 #include "math/include/Vector3.h"
 
 namespace ram {
@@ -25,6 +26,13 @@ struct OrientationEvent : public core::Event
 
 typedef boost::shared_ptr<OrientationEvent> OrientationEventPtr;
 
+struct Vector2Event : public core::Event
+{
+    Vector2 vector2;
+};
+
+typedef boost::shared_ptr<Vector2Event> Vector2EventPtr;
+    
 struct Vector3Event : public core::Event
 {
     Vector3 vector3;
