d561461ff79d4881c01f75c4210453f8112e3d81
Leo Singer <aronnax@umd.edu>
2009-07-27 22:17:04 +0000

    Adapter working now

--- scoped/src/main.cpp
+++ scoped/src/main.cpp
@@ -4,6 +4,43 @@
 
 using namespace std;
 
+class OscilloscopeImpl : virtual public ram::sonar::scope::Oscilloscope {
+public:
+    virtual void SetViewer(const ::ram::sonar::scope::ViewerPrx&, const ::Ice::Current&)
+    {
+    }
+    
+    virtual void SetTriggerMode(::ram::sonar::scope::TriggerMode, const ::Ice::Current&)
+    {
+    }
+    
+    virtual void SetTriggerChannel(::Ice::Short, const ::Ice::Current&)
+    {
+    }
+    
+    virtual void SetTriggerLevel(::Ice::Short, const ::Ice::Current&)
+    {
+    }
+    
+    virtual void SetTriggerHoldoff(::Ice::Int, const ::Ice::Current&)
+    {
+    }
+    
+    virtual void SetHorizontalPosition(::Ice::Int, const ::Ice::Current&)
+    {
+    }
+    
+    virtual void SetHorizontalZoom(::Ice::Short, const ::Ice::Current&)
+    {
+    }
+    
+    virtual ::ram::sonar::scope::OscilloscopeCapture GetLastCapture(const ::Ice::Current&)
+    {
+    }
+    
+};
+
+
 int main(int argc, char* argv[])
 {
     int status = 0;
@@ -13,7 +50,13 @@ int main(int argc, char* argv[])
         
         ic = Ice::initialize(argc, argv);
         
-        shutdownOnInterrupt();
+        Ice::ObjectAdapterPtr adapter = ic->createObjectAdapterWithEndpoints(
+            "scoped", "default -p 10000");
+        
+        IceUtil::Handle<OscilloscopeImpl> osc = new OscilloscopeImpl;
+        adapter->add(osc, ic->stringToIdentity("osc"));
+        adapter->activate();
+        ic->waitForShutdown();
         
     } catch (const Ice::Exception& e) {
         cerr << e << endl;
