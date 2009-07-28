f11edffc69d5632f2d5b488603215c179c42267e
Nicholas Limparis <nlimparis@ram.umd.edu>
2009-07-28 09:44:02 +0000

    More LED Stuff

--- packages/embedded/sbr5/LEDBoard.c
+++ packages/embedded/sbr5/LEDBoard.c
@@ -17,24 +17,24 @@
 
 
 //Register mapping for LED colors
-#define LED0 0x02//00010
-#define LED0 0x03//00011
-#define LED0 0x04//00100
-#define LED1 0x05//00101
-#define LED1 0x06//00110
-#define LED1 0x07//00111
-#define LED2 0x08//01000
-#define LED2 0x09//01001
-#define LED2 0x0A//01010
-#define LED3 0x0B//01011
-#define LED3 0x0C//01100
-#define LED3 0x0D//01101
-#define LED4 0x0E//01110
-#define LED4 0x0F//01111
-#define LED4 0x10//10000
-#define LED5 0x11//10001
-#define LED5 0x12//10010
-#define LED5 0x13//10011
+#define LED0RED   0x02//00010
+#define LED0GREEN 0x03//00011
+#define LED0BLUE  0x04//00100
+#define LED1RED   0x05//00101
+#define LED1GREEN 0x06//00110
+#define LED1BLUE  0x07//00111
+#define LED2RED   0x08//01000
+#define LED2GREEN 0x09//01001
+#define LED2BLUE  0x0A//01010
+#define LED3RED   0x0B//01011
+#define LED3GREEN 0x0C//01100
+#define LED3BLUE  0x0D//01101
+#define LED4RED   0x0E//01110
+#define LED4GREEN 0x0F//01111
+#define LED4BLUE  0x10//10000
+#define LED5RED   0x11//10001
+#define LED5GREEN 0x12//10010
+#define LED5BLUE  0x13//10011
 
 #define AllLEDDriveMode 11011  //Only really useful for driving all colors and in single reg write mode
 /*
@@ -53,17 +53,108 @@ BYTE AddressMaskLED ( BYTE Address)
 }
     
 //The 16th btye of ledVals is 0x00 since it does not drive an led
-unsigned int  SetSystem(BYTE NumBars, BYTE * ledVals [16], BYTE * Packet, BYTE * ledAddrs, BYTE * SplitByte, BYTE numSplits, LEDBoardGroupType * LEDgroups[16])
-  unsigned int count, temp;
-  count = 0;
+unsigned int  SetSystem(BYTE NumBars, BYTE * ledVals [16], BYTE * Packet, BYTE * ledAddrs, BYTE * SplitByte){//, BYTE numSplits){
+  unsigned int countBar;//, temp;
+  //count = 0;
   countBar = 0;
 
   for( countBar = 0; countBar < NumBars; countBar++){
     Packet[countBar] = AddressMaskLED(ledAddrs[countBar]);
-    Packet[countBar + 1] = (AutoIncBrightOnly | 
-    Packet[countBar + 2] = AddressMaskLED(Address); //(1101 1110)&(
+    Packet[countBar + 1] = (AutoIncBrightOnly | LED0RED);
+    Packet[countBar + 2] = ledVals[0][countBar];
+    Packet[countBar + 3] = ledVals[1][countBar];
+    Packet[countBar + 4] = ledVals[2][countBar];
+    Packet[countBar + 5] = ledVals[3][countBar];
+    Packet[countBar + 6] = ledVals[4][countBar];
+    Packet[countBar + 7] = ledVals[5][countBar];
+    Packet[countBar + 8] = ledVals[6][countBar];
+    Packet[countBar + 9] = ledVals[7][countBar];
+    Packet[countBar + 10] = ledVals[8][countBar];
+    Packet[countBar + 11] = ledVals[9][countBar];
+    Packet[countBar + 12] = ledVals[10][countBar];
+    Packet[countBar + 13] = ledVals[11][countBar];
+    Packet[countBar + 14] = ledVals[12][countBar];
+    Packet[countBar + 15] = ledVals[13][countBar];
+    Packet[countBar + 16] = ledVals[14][countBar];
+    Packet[countBar + 17] = ledVals[15][countBar];
+  };
+  return (NumBars*18);
+} 
 
-
-
-
-//The Packet to write to all LED drivers is 
+     
+void SetLightVal(BYTE NumBars, BYTE * ledVals[16],BYTE Red, BYTE Green, BYTE Blue){
+  unsigned int index =0;
+  for(unsigned int CountBars = 0; CountBars <NumBars; CountBars++){
+    for (index = 0; index <15; index =index +3){
+      ledVals[index][CountBars] = Red;
+      ledVals[index+1][CountBars] = Green;
+      ledVals[index+2][CountBars] = Blue;
+    }
+    ledVals[15][CountBars] = 0x00;
+  }
+  return;
+}
+//to enable a channel set Red, Green, or Blue to 0xFF and disable to 0x00
+void PulseLights(BYTE NumBars, BYTE * ledVals[16], BYTE Red, BYTE Green, BYTE Blue, unsigned int delay){
+  unsigned int index = 0;
+  unsigned int CountBars = 0;
+  BYTE step = 0;
+  BYTE RedLight = 0;
+  BYTE GreenLight = 0;
+  BYTE BlueLight = 0;
+  for(CountBars = 0; CountBars < NumBars; CountBars++){
+    for(index = 0; index <=15; index++){
+      ledVals[index][CountBars] = 0x00;
+    }
+  }
+  for(step = 0; step <=255; step++){
+    for(delayCnt = 0; delayCnt <=delay; delayCnt++){};  //Spin for a delay 
+    for(CountBars = 0; CountBars < NumBars; CountBars++){
+      ledVals[0][CountBars] = (step & Red);
+      ledVals[1][CountBars] = (step & Green);
+      ledVals[2][CountBars] = (step & Blue);
+      ledVals[3][CountBars] = (step & Red);
+      ledVals[4][CountBars] = (step & Green);
+      ledVals[5][CountBars] = (step & Blue);
+      ledVals[6][CountBars] = (step & Red);
+      ledVals[7][CountBars] = (step & Green);
+      ledVals[8][CountBars] = (step & Blue);
+      ledVals[9][CountBars] = (step & Red);
+      ledVals[10][CountBars] = (step & Green);
+      ledVals[11][CountBars] = (step & Blue);
+      ledVals[12][CountBars] = (step & Red);
+      ledVals[13][CountBars] = (step & Green);
+      ledVals[14][CountBars] = (step & Blue);
+      ledVals[15][CountBars] = 0x00;
+    }
+    ///////////////////////////////////////////////
+    //WIRITE I2C HERE!!!!!!!!!!!!!!
+    ///////////////////////////////////////////////
+  }
+  //step Down now
+  for(step = 255; step >=0; step--){
+    for(delayCnt = 0; delayCnt <=delay; delayCnt++){};  //Spin for a delay 
+    for(CountBars = 0; CountBars < NumBars; CountBars++){
+      ledVals[0][CountBars] = (step & Red);
+      ledVals[1][CountBars] = (step & Green);
+      ledVals[2][CountBars] = (step & Blue);
+      ledVals[3][CountBars] = (step & Red);
+      ledVals[4][CountBars] = (step & Green);
+      ledVals[5][CountBars] = (step & Blue);
+      ledVals[6][CountBars] = (step & Red);
+      ledVals[7][CountBars] = (step & Green);
+      ledVals[8][CountBars] = (step & Blue);
+      ledVals[9][CountBars] = (step & Red);
+      ledVals[10][CountBars] = (step & Green);
+      ledVals[11][CountBars] = (step & Blue);
+      ledVals[12][CountBars] = (step & Red);
+      ledVals[13][CountBars] = (step & Green);
+      ledVals[14][CountBars] = (step & Blue);
+      ledVals[15][CountBars] = 0x00;
+    }
+    ///////////////////////////////////////////////
+    //WIRITE I2C HERE!!!!!!!!!!!!!!
+    ///////////////////////////////////////////////
+  }
+  return;
+}
