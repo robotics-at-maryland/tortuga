7ff799cd4d52ef1dbf216af2679d77295878b03f
Jonathan Sternberg <jonathan.sternberg@gmail.com>
2009-07-29 21:29:45 +0000

    '''Login''' Added a script to add the ability to login with no password.

---
+++ scripts/quick_login.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Press enter 3 times"
+ssh-keygen -t rsa
+echo "Enter tortuga3 password twice"
+ssh jlisee@192.168.10.11 mkdir -p .ssh
+~/.ssh/id_rsa.pub | ssh jlisee@192.168.10.11 'cat >> .ssh/authorized_keys'"
+ssh jlisee@192.168.10.11
