6303930eb9a8f12a1ac23c4c15b00da48139066a
Joseph Lisee <jlisee@gmail.com>
2009-08-01 15:00:48 +0000

    '''Config''' Updated scripts before qual and qual practice

--- scripts/run_qual.sh
+++ scripts/run_qual.sh
@@ -2,6 +2,7 @@
 lcdshow -c
 #lcdshow -noblink
 #lcdshow -setbars 12
+echo "Running: Pipe -> Light -> SONAR mission"
 lcdshow -s
 lcdshow -t "Get ready..."
 #lcdshow -redgreen
@@ -13,7 +14,7 @@ lcdshow -t "Running..."
 scripts/reload_cameras.sh
 lcdshow -unsafe
 source scripts/setenv
-python tools/acs/src/main.py -c data/config/trans_qual_practice.yml -s ram.ai.course.Gate
+python tools/acs/src/main.py -c data/config/trans_qual.yml -s ram.ai.course.Gate
 
 
 
