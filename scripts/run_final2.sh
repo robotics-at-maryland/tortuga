dbf407f8ae6e1843708ad434f7ac4bae466f76da
Jonathan Sternberg <jonathan.sternberg@gmail.com>
2009-08-02 22:16:03 +0000

    '''Run File'''

---
+++ scripts/run_final2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Running GATE -> PIPEGATE -> BUOY -> SONAR"
+scripts/reload_cameras.sh
+lcdshow -unsafe
+source scripts/setenv
+
+lcdshow -c
+#lcdshow -noblink
+#lcdshow -setbars 12
+lcdshow -s
+lcdshow -t "Get ready..."
+#lcdshow -redgreen
+sleep 2
+#lcdshow -noblink
+#lcdshow -setbars 240
+lcdshow -t "Running..."
+
+python tools/acs/src/main.py -c data/config/transdec_final_competition2.yml -s ram.ai.course.Gate
+
+#lcdshow -noblink
+#lcdshow -setbars 12
+lcdshow -t "Stopped"
