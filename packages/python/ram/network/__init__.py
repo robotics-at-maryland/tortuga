60f84500fd11e22333ba3f12de13ec338e5e44b6
Jonathan Sternberg <jonathan.sternberg@gmail.com>
2009-09-08 14:30:19 +0000

    '''RemoteAi''' New code for a server that uses Ice to transmit data. The server reads all state transition data and stores them.

---
+++ packages/python/ram/network/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (C) 2009 Maryland Robotics Club
+# Copyright (C) 2009 Jonathan Sternberg <jsternbe@umd.edu>
+# All rights reserved.
+#
+# Author: Jonathan Sternberg <jsternbe@umd.edu>
+# File:  packages/python/ram/network/__init__.py
