4ac6cbdbd0a2cf7c86ab7ad00ee080eaa78a5ada
Jonathan Sternberg <jonathan.sternberg@gmail.com>
2009-11-05 19:11:08 +0000

    '''Scripts''' Small error in register computer script. Fixed it. This is finished now.

--- scripts/register_computer.sh
+++ scripts/register_computer.sh
@@ -2,10 +2,9 @@
 # This script will be called by another script when appropriate!
 # DO NOT USE THIS SCRIPT ON YOUR OWN!
 
-echo $1
-if [ ! -n "$1" ]; then
+if [ ! -n "$3" ]; then
     echo "Do NOT run this script, jackass."
 else
     mkdir -p .ssh
-    echo $1 | cat >> .ssh/authorized_keys2
+    echo $1 $2 $3 | cat >> .ssh/authorized_keys2
 fi
