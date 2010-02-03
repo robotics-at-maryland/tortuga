48e11db2e9e9c5707fe62c4dbdbc3e9d7b05ff8f
Jonathan Sternberg <jonathan.sternberg@gmail.com>
2010-02-03 04:05:40 +0000

    '''Vision''' Improved color filter and started region detector.

--- packages/vision/matlab/colorfilter.m
+++ packages/vision/matlab/colorfilter.m
@@ -5,7 +5,6 @@
 
 function A = colorfilter(IMG, R, G, B)
 % Load the variables
-A = [];
 lowerRed = R(1);
 upperRed = R(2);
 lowerGreen = G(1);
@@ -13,24 +12,11 @@ upperGreen = G(2);
 lowerBlue = B(1);
 upperBlue = B(2);
 % Load the size of the image, depth is not used
-[height, width, depth] = size(IMG);
-% Horribly inefficient embedded for loop
-for I = 1:height
-    for Y = 1:width
-        red = IMG(I, Y, 1);
-        green = IMG(I, Y, 2);
-        blue = IMG(I, Y, 3);
-        % Check if the current RGB values of this pixel are in the restrictions
-        if lowerRed <= red && red <= upperRed...
-                && lowerGreen <= green...
-                && green <= upperGreen...
-                && lowerBlue <= blue...
-                && blue <= upperBlue
-            % If they are, sets the pixel to white, otherwise, black
-            A(I, Y, 1:3) = [255 255 255];
-        else
-            A(I, Y, 1:3) = [0 0 0];
-        end
-    end
-end
+
+redFilter = (IMG(:,:,1) <= upperRed) & (IMG(:,:,1) >= lowerRed);
+greenFilter = (IMG(:,:,2) <= upperGreen) & (IMG(:,:,2) >= lowerGreen);
+blueFilter = (IMG(:,:,3) <= upperBlue) & (IMG(:,:,3) >= lowerBlue);
+
+A = redFilter & greenFilter & blueFilter;
+
 end
