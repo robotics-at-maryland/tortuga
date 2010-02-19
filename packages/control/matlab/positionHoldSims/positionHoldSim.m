bfad3aea8f495919e4a338f46a4364f767d0d218
Jonathan Wonders <jwonders88@gmail.com>
2010-02-19 00:55:10 +0000

    fixed dvl measurement frame added dvl noise

--- packages/control/matlab/positionHoldSims/positionHoldSim.m
+++ packages/control/matlab/positionHoldSims/positionHoldSim.m
@@ -12,7 +12,7 @@ clc
 %position (m) and velocity (m/s) in inertial frame
 %orientation (rad) is angle from inertial frame to body frame
 %state = [r1 r2 r1Dot r2Dot theta thetaDot]
-x0=[1 2 2 2 0 0]';
+x0=[0 0 0 0 0 1]';
 
 
 
@@ -25,6 +25,9 @@ time=ti:0.04:tf;
 state_storage = 666*ones(length(x0),length(time));
 dvl_storage = 666*ones(2,length(time));
 control_storage = 666*ones(3,length(time));
+
+dvl_variance_1 = 0;
+dvl_variance_2 = 0;
 
 %system constants
 global m;%mass (kg)
@@ -48,9 +51,17 @@ Tn_controller=0;
 %% simulation
 state_storage(:,1) = x0;
 for t = 2:length(time)
-%simulate DVL here
-[tmp, est] = ode45(@positionHoldSimDynamics,[time(t-1),time(t)],state_storage(:,t-1));
-state_storage(:,t) = est(end,:);
+    % simulate measurement
+    % [dvl_x1_dot dvl_x2_dot]
+    dvl_storage(:,t-1) = bRn(state_storage(5,t-1))*[state_storage(3,t-1) + random('norm',0,dvl_variance_1); state_storage(4,t-1) + random('norm',0,dvl_variance_2)];
+    
+    % do control
+    Fb_controller = [10 0]';
+    Fn_controller = bRn(state_storage(5,t-1))'*Fb_controller;
+    
+    % integrate dynamics (simulate the world)
+    [tmp, est] = ode45(@positionHoldSimDynamics,[time(t-1),time(t)],state_storage(:,t-1));
+    state_storage(:,t) = est(end,:);
 end
 
 %[time_ode,x]=ode45(@positionHoldSimDynamics,time,x0);
@@ -58,5 +69,39 @@ end
 figure(1)
 subplot(2,1,1)
 plot(time,state_storage(1,:))
+xlabel('time');
+ylabel('x_1');
 subplot(2,1,2)
 plot(time,state_storage(2,:))
+xlabel('time');
+ylabel('x_2');
+
+figure(2)
+subplot(2,1,1)
+plot(time,state_storage(3,:))
+xlabel('time');
+ylabel('xdot_1');
+subplot(2,1,2)
+plot(time,state_storage(4,:))
+xlabel('time');
+ylabel('xdot_2');
+
+figure(3)
+subplot(2,1,1)
+plot(time,state_storage(5,:))
+xlabel('time');
+ylabel('\theta');
+subplot(2,1,2)
+plot(time,state_storage(6,:))
+xlabel('time');
+ylabel('\thetadot');
+
+figure(4)
+subplot(2,1,1)
+plot(time(1:end-1),dvl_storage(1,1:end-1))
+xlabel('time');
+ylabel('dvl_1');
+subplot(2,1,2)
+plot(time(1:end-1),dvl_storage(2,1:end-1))
+xlabel('time');
+ylabel('dvl_2');
