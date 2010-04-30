909d8f7a843ed32c8cd98b39380a0f978b16fb40
Jonathan Wonders <jwonders88@gmail.com>
2010-04-30 04:46:18 +0000

    added new estimation module templates

---
+++ packages/vehicle/estimator/include/modules/BasicDepthEstimationModule.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2010 Robotics at Maryland
+ * Copyright (C) 2010 Jonathan Wonders <jwonders@umd.edu>
+ * All rights reserved.
+ *
+ * Author: Jonathan Wonders <jwonders@umd.edu>
+ * File:  packages/vehicle/estimator/include/modules/BasicDepthEstimationModule.h
+ */
+
+/* This is currently a template for a Depth EstimationModule implementation */
+
+
+#ifndef RAM_VEHICLE_ESTIMATOR_BASICDEPTHESTIMATIONMODULE_H
+#define RAM_VEHICLE_ESTIMATOR_BASICDEPTHESTIMATIONMODULE_H
+
+// Library Includes
+
+// Project Includes
+#include "core/include/ConfigNode.h"
+#include "core/include/Event.h"
+#include "vehicle/estimator/include/EstimatedState.h"
+#include "vehicle/estimator/include/EstimationModule.h"
+
+namespace ram {
+namespace estimator {
+
+class BasicDepthEstimationModule : public EstimationModule
+{
+public:
+    BasicDepthEstimationModule(core::ConfigNode config);
+    ~BasicDepthEstimationModule(){};
+
+    /* The Depth Estimation routine goes here.  It should store the new estimated
+       state in estimatedState. */
+    virtual void update(core::EventPtr event, EstimatedStatePtr estimatedState);
+
+private:
+    /* any necessary persistent variables should be declared here */
+};
+
+} // namespace estimatior
+} // namespace ram
+
+#endif // RAM_VEHICLE_ESTIMATOR_BASICDEPTHESTIMATIONMODULE_H
--- packages/vehicle/estimator/include/modules/IncludeAllModules.h
+++ packages/vehicle/estimator/include/modules/IncludeAllModules.h
@@ -13,3 +13,5 @@
 */
 
 #include "vehicle/estimator/include/modules/BasicDVLEstimationModule.h"
+#include "vehicle/estimator/include/modules/BasicIMUEstimationModule.h"
+#include "vehicle/estimator/include/modules/BasicDepthEstimationModule.h"
