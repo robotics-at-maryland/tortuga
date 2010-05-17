cf2964e7795e0e4df3e09d55cd7ef556bc1de357
Jonathan Sternberg <jonathan.sternberg@gmail.com>
2010-05-17 05:31:31 +0000

    '''Vision''' Beginning work on a hedge detector prototype.

--- packages/vision/python/rectangle.py
+++ packages/vision/python/rectangle.py
@@ -34,7 +34,7 @@ class Display(object):
     def __init__(self, name, img):
         self._name = name
         self._img = img
-        self._x, self._y, self._r = (0, 0, 0)
+        self._x1, self._y1, self._x2, self._y2 = (0, 0, 0, 0)
         # Possible states: none, move, select, mousedown
         self._state = 'none'
         self._update = False
@@ -44,45 +44,60 @@ class Display(object):
         cvNamedWindow('Image')
 
         def callback(event, x, y, flags, param):
-            global config
             if event == CV_EVENT_LBUTTONDOWN:
                 if self._state == 'none':
-                    self._x = x
-                    self._y = y
+                    self._x1 = x
+                    self._y1 = y
+                    self._x2 = x
+                    self._y2 = y
                     dest = clone_image(self._img)
-                    cvCircle(dest, (x, y), 2, cvScalar(0,0,255), 3)
+                    cvRectangle(dest, (self._x1, self._y1),
+                                (self._x2, self._y2), cvScalar(0,0,255), 3)
                     cvShowImage('Image', dest)
                     self._state = 'mousedown'
                 elif self._state == 'select':
-                    if math.fabs(math.sqrt((self._x - x)**2 + \
-                                               (self._y - y)**2)) < self._r:
+                    width, height = abs(self._x2 - self._x1), \
+                        abs(self._y2 - self._y1)
+                    x1 = min(self._x1, self._x2)
+                    y1 = min(self._y1, self._y1)
+                    if x <= x1 + width and x >= x1 and \
+                            y <= y1 + height and y >= y1:
                         self._state = 'move'
+                        self._movex = x
+                        self._movey = y
                     else:
                         self._state = 'none'
                         cvShowImage('Image', self._img)
                         
             elif event == CV_EVENT_LBUTTONUP:
                 if self._state == 'mousedown':
-                    self._r = math.sqrt((self._x - x)**2 + \
-                                                 (self._y - y)**2)
+                    self._x2, self._y2 = x, y
                     self._state = 'select'
                 elif self._state == 'move':
+                    transx, transy = x - self._movex, y - self._movey
+                    self._x1, self._y1 = self._x1 + transx, self._y1 + transy
+                    self._x2, self._y2 = self._x2 + transx, self._y2 + transy
                     self._state = 'select'
             elif event == CV_EVENT_MOUSEMOVE:
                 if self._state == 'mousedown':
-                    radius = math.sqrt((self._x - x)**2 + \
-                                           (self._y - y)**2)
+                    self._x2, self._y2 = x, y
                     dest = clone_image(self._img)
-                    cvCircle(dest, (self._x, self._y), 2, cvScalar(0,0,255), 3)
-                    cvCircle(dest, (self._x, self._y), cvRound(radius),
-                             cvScalar(0,255,0), 5)
+                    cvRectangle(dest, (self._x1, self._y1),
+                                (self._x2, self._y2), cvScalar(0,0,255), 3)
+                    cvCircle(dest, (self._x1 + cvRound((self._x2-self._x1)/2),
+                                    self._y1 + cvRound((self._y2-self._y1)/2)),
+                             3, cvScalar(0,255,0), -1)
                     cvShowImage('Image', dest)
                 elif self._state == 'move':
-                    self._x, self._y = (x, y)
+                    transx, transy = x - self._movex, y - self._movey
+                    x1, y1 = self._x1 + transx, self._y1 + transy
+                    x2, y2 = self._x2 + transx, self._y2 + transy
                     dest = clone_image(self._img)
-                    cvCircle(dest, (self._x, self._y), 2, cvScalar(0,0,255), 3)
-                    cvCircle(dest, (self._x, self._y), cvRound(self._r),
-                             cvScalar(0,255,0), 5)
+                    cvRectangle(dest, (x1, y1),
+                                (x2, y2), cvScalar(0,0,255), 3)
+                    cvCircle(dest, (x1 + cvRound((x2 - x1) / 2),
+                                    y1 + cvRound((y2 - y1) / 2)),
+                             3, cvScalar(0,255,0), -1)
                     cvShowImage('Image', dest)
             elif event == CV_EVENT_RBUTTONDOWN:
                 if self._state == 'none':
@@ -91,9 +106,14 @@ class Display(object):
                         del config[self._name]
 
                 elif self._state == 'select':
-                    config[self._name] = { 'x' : self._x,
-                                           'y' : self._y,
-                                           'r' : self._r }
+                    width = self._x2 - self._x1
+                    height = self._y2 - self._y1
+                    centerx = self._x1 + cvRound(width / 2)
+                    centery = self._y1 + cvRound(height / 2)
+                    config[self._name] = { 'x' : centerx,
+                                           'y' : centery,
+                                           'width' : abs(width),
+                                           'height' : abs(height) }
                     self._stall = False
                     self._state = 'none'
             
@@ -105,7 +125,7 @@ class Display(object):
 
 def main():
     if len(sys.argv) < 2:
-        print 'Usage: python circle.py FOLDER'
+        print 'Usage: python rectangle.py FOLDER'
         sys.exit(1)
 
     cvStartWindowThread()
