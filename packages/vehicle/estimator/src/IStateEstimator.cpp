8481d1f21e95c993270281d5941774680823b94e
Jonathan Wonders <jwonders88@gmail.com>
2010-06-05 18:50:25 +0000

    added IStateEstimator src file that I forgot

---
+++ packages/vehicle/estimator/src/IStateEstimator.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2010 Robotics at Maryland
+ * Copyright (C) 2010 Jonathan Wonders <jwonders@umd.edu>
+ * All rights reserved.
+ *
+ * Author: Jonathan Wonders <jwonders@umd.edu>
+ * File:  packages/vehicle/estimator/src/IStateEstimator.c
+ */
+
+#include "vehicle/estimator/include/IStateEstimator.h"
+
+RAM_CORE_EVENT_TYPE(ram::estimator::IStateEstimator, ESTIMATED_DEPTH_UPDATE);
+RAM_CORE_EVENT_TYPE(ram::estimator::IStateEstimator, ESTIMATED_ORIENTATION_UPDATE);
+RAM_CORE_EVENT_TYPE(ram::estimator::IStateEstimator, ESTIMATED_VELOCITY_UPDATE);
+RAM_CORE_EVENT_TYPE(ram::estimator::IStateEstimator, ESTIMATED_POSITION_UPDATE);
