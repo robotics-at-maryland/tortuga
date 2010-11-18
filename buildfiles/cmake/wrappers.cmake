01769bc6953635e471ec3ee07001488b0995f391
Jonathan Sternberg <jonathan.sternberg@gmail.com>
2010-11-18 22:46:20 +0000

    '''Build System''' CMake build system generates the XML files.

--- buildfiles/cmake/wrappers.cmake
+++ buildfiles/cmake/wrappers.cmake
@@ -3,13 +3,28 @@ find_package(PythonLibs)
 include_directories(${PYTHON_INCLUDE_PATH})
 set(WRAPPER_DIR ${CMAKE_SOURCE_DIR}/build_ext)
 
-macro(generate_wrappers _module)
-  set(_src ${_module}/${_module}.i)
-  set(SWIG_MODULE_${_module}_EXTRA_FLAGS "-DRAM_EXPORT")
-  make_directory(${CMAKE_BINARY_DIR}/wrappers/${_module})
-  include_directories(${CMAKE_SOURCE_DIR}/packages/${_module}/include)
-  set_source_files_properties(${_src} PROPERTIES CPLUSPLUS ON)
-  set_source_files_properties(${_src} PROPERTIES SWIG_FLAGS "-Wall")
-  swig_add_module(${_module} python ${_src})
-  swig_link_libraries(${_module} ram_${_module} ${PYTHON_LIBRARIES})
+find_package(GCCXML)
+
+macro(gccxml MODULE HEADERS)
+  set(DIRECTORY "${CMAKE_SOURCE_DIR}/packages/${MODULE}/include")
+  make_directory(generated)
+  set (XMLFILES "")
+  set (GCCXML_FLAGS "-I${CMAKE_SOURCE_DIR}/packages" "-I${CMAKE_SOURCE_DIR}" "-I/opt/ram/local/include/boost-1_37" "-I${PYTHON_INCLUDE_PATH}" "-I/opt/ram/local/include" "-I${CMAKE_SOURCE_DIR}/packages/${MODULE}" "-DRAM_POSIX" "-DRAM_LINUX" "-DBOOST_PYTHON_NO_PY_SIGNATURES")
+  if (UNIX)
+    set(GCCXML_FLAGS ${GCCXML_FLAGS} "--gccxml-compiler" "gcc-4.2")
+  endif (UNIX)
+
+  foreach (HEADER ${HEADERS})
+    string (REGEX REPLACE "\\.h$" ".xml" XMLNAME ${HEADER})
+    set (XMLOUTPUT generated/${XMLNAME})
+
+    add_custom_command(
+      OUTPUT ${XMLOUTPUT}
+      COMMAND ${GCCXML}
+      ARGS ${DIRECTORY}/${HEADER} ${GCCXML_FLAGS} -fxml=${XMLOUTPUT}
+      MAIN_DEPENDENCY ${DIRECTORY}/${HEADER}
+      )
+    set (XMLFILES ${XMLFILES} ${XMLOUTPUT})
+  endforeach ()
+  add_custom_target(ram_${MODULE}_wrapper ALL DEPENDS ${XMLFILES})
 endmacro ()
