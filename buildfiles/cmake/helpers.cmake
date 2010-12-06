781210ec00794854a53addb63b133e368bdf48d0
Jonathan Sternberg <jonathan.sternberg@gmail.com>
2010-12-06 21:04:01 +0000

    '''Build System''' Ability to turn off continuous integration tests added to the build system. Continuous integration tests on by default.

--- buildfiles/cmake/helpers.cmake
+++ buildfiles/cmake/helpers.cmake
@@ -1,5 +1,6 @@
 
 set(RAM_TESTS ON CACHE BOOL "Build and run unittests")
+set(RAM_CONTINUOUS_INTEGRATION ON CACHE BOOL "Run unittests during the build")
 if (RAM_TESTS)
   enable_testing()
 endif (RAM_TESTS)
@@ -7,7 +8,9 @@ endif (RAM_TESTS)
 macro(test_module _name _link_libs)
   if (RAM_TESTS)
     test_module_base(${_name} "${_link_libs}" ${ARGV})
-    add_custom_target(ram_${_name}_tests ALL DEPENDS Tests_${_name}.success)
+    if (RAM_CONTINUOUS_INTEGRATION)
+      add_custom_target(ram_${_name}_tests ALL DEPENDS Tests_${_name}.success)
+    endif (RAM_CONTINUOUS_INTEGRATION)
   endif (RAM_TESTS)
 endmacro ()
 
@@ -21,8 +24,11 @@ macro(test_wrapper _name _link_libs)
     # Run macro to set up C++ tests
     set(${_name}_wrapper_DEPENDENCIES ${WRAPPER_TEST_DEPENDS})
     test_module_base(${_name}_wrapper "${_link_libs}" ${ARGV})
-    add_custom_target(ram_${_name}_wrapper_tests ALL DEPENDS
-      ${WRAPPER_TEST_DEPENDS})
+
+    if (RAM_CONTINUOUS_INTEGRATION)
+      add_custom_target(ram_${_name}_wrapper_tests ALL DEPENDS
+        ${WRAPPER_TEST_DEPENDS})
+    endif (RAM_CONTINUOUS_INTEGRATION)
 
     list(APPEND WRAPPER_TEST_DEPENDS Tests_${_name}_wrapper.success _${_name})
 
@@ -39,8 +45,10 @@ macro(test_wrapper _name _link_libs)
       )
     add_test(python_${_name}
       python ${CMAKE_SOURCE_DIR}/scripts/pytester.py ${${_name}_PYTESTS})
-    add_custom_target(${_name}_pywrapper_tests ALL DEPENDS
-      ${CMAKE_SOURCE_DIR}/build_ext/ext/_${_name}Tests.success)
+    if (RAM_CONTINUOUS_INTEGRATION)
+      add_custom_target(${_name}_pywrapper_tests ALL DEPENDS
+        ${CMAKE_SOURCE_DIR}/build_ext/ext/_${_name}Tests.success)
+    endif (RAM_CONTINUOUS_INTEGRATION)
   endif (RAM_TESTS)
 endmacro ()
 
