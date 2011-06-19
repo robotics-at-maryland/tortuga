0eb04ddbb7d6c004c8edc3136fed0264ab4786ca
Jonathan Wonders <jwonders88@gmail.com>
2011-06-19 22:52:47 +0000

    '''Math''' Removing old helpers that are no longer needed

--- packages/math/include/Helpers.h
+++ packages/math/include/Helpers.h
@@ -16,61 +16,29 @@
 // Must Be Included last
 #include "math/include/Export.h"
 #include "math/include/Matrix2.h"
+#include "math/include/Vector2.h"
+#include "math/include/Vector3.h"
+#include "math/include/Quaternion.h"
 
 namespace ram {
 namespace math {
 
-void RAM_EXPORT tilde(double w[3], double * ptilde);
-double RAM_EXPORT sign(double variable);
-void RAM_EXPORT createOmega(double w[3], double * pOMEGA);
-
-void RAM_EXPORT normalize3x1(double* pvec);
-void RAM_EXPORT normalize4x1(double* pvec);
-
-double RAM_EXPORT magnitude3x1(double vec[3]);
-double RAM_EXPORT magnitude4x1(double vec[4]);
-
-double RAM_EXPORT det2x2(double a[2][2]);
-double RAM_EXPORT det3x3(double a[3][3]);
-
-void RAM_EXPORT invert3x3(double mat[3][3], double * pInverted);
-void RAM_EXPORT matrixAdd3x1and3x1(double a[3], double b[3], double* const presult);
-void RAM_EXPORT matrixAdd4x1and4x1(double a[4], double b[4], double* const presult);
-void RAM_EXPORT matrixMult3x1byScalar(double vec[3], double scalar,
-                           double* const presult);
-void RAM_EXPORT matrixMult4x1byScalar(double vec[4], double scalar,
-                           double* const presult);
-void RAM_EXPORT matrixMult3x3byScalar(double matrix[3][3], double scalar,
-                           double* const presult);
-void RAM_EXPORT matrixMult3x1by3x3(double bigMatrix[3][3], double littleMatrix[3],
-                        double* const presult);
-void RAM_EXPORT matrixMult4x4by4x1(const double big[4][4],
-                                   const double little[4],
-                                   double* const presult);
-void RAM_EXPORT matrixMult3x3by3x3(double a[3][3], double b[3][3], double* presult);
-void RAM_EXPORT crossProduct3x1by3x1(double a[3], double b[3], double * presult);
-
-void RAM_EXPORT eulerAxisToQuaternion(double e[3], double phi, double* const pq);
-void RAM_EXPORT quaternionToEulerAxis(double q[4], double* pphi, double* pe);
-void RAM_EXPORT findErrorQuaternion(double qc[4], double q[4], double * pqe);
-void RAM_EXPORT rotationMatrixFromQuaternion(double q[4], double * pRot);
-void RAM_EXPORT quaternionFromEulerAxis(double e[3], double et, double * pQ);
-void RAM_EXPORT quaternionCrossProduct(const double q1[4],
-                                       const double q2[4], double * pQ);
-
-double RAM_EXPORT getDistance(double a[3], double b[3]);
-void RAM_EXPORT getQuaternionErrorMatrix(double q[4], double * pMatrix);
-void RAM_EXPORT quaternionFromnCb(double nCb[3][3], double * pQ);
-
-void RAM_EXPORT rotationRoll(double phi, double * pMatrix);
-void RAM_EXPORT rotationPitch(double theta, double * pMatrix);
-void RAM_EXPORT rotationYaw(double psi, double * pMatrix);
-
-Matrix2 RAM_EXPORT bRn(double radians);
-Matrix2 RAM_EXPORT nRb(double radians);
+int sign(double variable);
 
 // factorial for smallish numbers
 int factorial(int num);
+
+
+math::Vector2 world2img(math::Vector3 worldCoordinates,
+                        math::Vector3 cameraPosition,
+                        math::Quaternion cameraOrientation,
+                        math::Matrix3 intrinsicParameters);
+
+math::Vector3 img2world(math::Vector2 imgCoordinates,
+                        double distance,
+                        math::Vector3 cameraPosition,
+                        math::Quaternion cameraOrientation,
+                        math::Matrix3 invIntrinsicParameters);
 
 } // namespace math
 } // namespace ram
--- packages/math/test/src/TestHelpers.cxx
+++ packages/math/test/src/TestHelpers.cxx
@@ -20,68 +20,29 @@
 
 using namespace ram::math;
 
-// This is an attempt to understand the function
-TEST(quaternionFromnCb)
+TEST(sign)
 {
-    Matrix3 mat;
-    Quaternion expected;
-    Quaternion result;
-    
-    // Simple rotation
-    mat.FromAxisAngle(ram::math::Vector3::UNIT_X, ram::math::Degree(90));    
-    expected.FromRotationMatrix(mat);
-    
-    quaternionFromnCb((double (*)[3])(mat[0]), result.ptr());
-    CHECK_ARRAY_CLOSE(expected.ptr(), result.ptr(), 4, 0.0001);
+    double x = -3.572;
 
-    // Full rotation
-    mat.FromAxisAngle(ram::math::Vector3::UNIT_Z, ram::math::Degree(180));    
-    expected.FromRotationMatrix(mat);
-    
-    quaternionFromnCb((double (*)[3])(mat[0]), result.ptr());
-    CHECK_ARRAY_CLOSE(expected.ptr(), result.ptr(), 4, 0.0001);
-    
-    // Rotation around a "complex" axis
-    Vector3 axis(87, 9, 28);
-    axis.normalise();
-    mat.FromAxisAngle(axis, ram::math::Degree(56));
-    expected.FromRotationMatrix(mat);
-    
-    quaternionFromnCb((double (*)[3])(mat[0]), result.ptr());
-    CHECK_ARRAY_CLOSE(expected.ptr(), result.ptr(), 4, 0.0001);
+    CHECK(sign(x) == -1);
 
-    // Another complex axis
-    axis = Vector3(0.365148, 0.912871, 0.182574);
-    axis.normalise();
-    mat.FromAxisAngle(axis, ram::math::Degree(33));
-    expected.FromRotationMatrix(mat);
+    x = 1.256;
 
-    quaternionFromnCb((double (*)[3])(mat[0]), result.ptr());
-    CHECK_ARRAY_CLOSE(expected.ptr(), result.ptr(), 4, 0.0001);
+    CHECK(sign(x) == 1);
 
-    
+    x = 0;
+    CHECK(sign(x) == 0);
 
-    // Result: this appears to do this same thing as "FromRotationMatrix"
 }
 
-TEST(rotationMatrixFromQuaternion)
+TEST(factorial)
 {
-    // Pitch downward by 45 degrees
-    Quaternion orien(Degree(45), Vector3::UNIT_Y);
-    
-    Vector3 start(0, 0, -1);
-    Vector3 expected(-0.707106781, 0, -0.707106781);
+    int n = 4;
+    int exp = 4 * 3 * 2 * 1;
+    CHECK(factorial(n) == exp);
 
-    // This answer should be correct
-    Vector3 resultOgre = orien * start;
-    CHECK_CLOSE(expected, resultOgre, 0.0001);
 
-    // Now find the answer from the function we are testing
-    Vector3 resultHelpers;
-    
-    double rotationMatrix[3][3];
-    rotationMatrixFromQuaternion(orien.ptr(), &rotationMatrix[0][0]);
-    matrixMult3x1by3x3(rotationMatrix, start.ptr(), resultHelpers.ptr());
-
-    CHECK_CLOSE(expected, resultHelpers, 0.0001);
+    n = 6;
+    exp = 6 * 5 * 4 * 3 * 2 * 1;
+    CHECK(factorial(n) == exp);
 }
