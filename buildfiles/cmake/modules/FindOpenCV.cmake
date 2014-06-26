d1fe59237932f3befa1e8ed84c2d0b2082b381bd
erudnick-cohen <erudnick@umd.edu>
2014-06-26 10:35:26 -0400

    Changed my find opencv.cmake, this is going to cause issues when merging into main repo

--- buildfiles/cmake/modules/FindOpenCV.cmake
+++ buildfiles/cmake/modules/FindOpenCV.cmake
@@ -48,7 +48,7 @@
 #----------------------------------------------------------
 
 
-FIND_PATH(OPENCV_DIR OpenCVConfig.cmake /home/tortuga/opencv-2.4.5/build NO_DEFAULT_PATH)
+FIND_PATH(OPENCV_DIR OpenCVConfig.cmake /home/eliot/opencv-2.4.5/release NO_DEFAULT_PATH)
 message("OpenCV Firectory!!! : " ${OPENCV_DIR})
 
 ##====================================================
