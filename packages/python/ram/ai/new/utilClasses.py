5ded26942d0d94cc298630acfe698f39f6f721dd
mxlan <j.xianyimao@gmail.com>
2014-07-18 18:37:51 -0400

    added the opposite of hasQueryBeenFalse

--- packages/python/ram/ai/new/utilClasses.py
+++ packages/python/ram/ai/new/utilClasses.py
@@ -30,9 +30,9 @@ class VisionObject(object):
         return self.seen
 
 #hack vision object that  tracks a Red buoy in the old simulator
-class OldSimulatorHackVisionObject(VisionObject):
-    def __init__(self,oldStatePtr):
-        super(OldSimulatorHackVisionObject,self).__init__()
+class BuoyVisionObject(VisionObject):
+    def __init__(self, oldStatePtr):
+        super(BuoyVisionObject,self).__init__()
         oldStatePtr.queuedEventHub.subscribeToType(vision.EventType.BUOY_FOUND,self.callback)
         oldStatePtr.queuedEventHub.subscribeToType(vision.EventType.BUOY_LOST,self.seeit)
 
@@ -49,9 +49,9 @@ class OldSimulatorHackVisionObject(VisionObject):
             self.seen = False
 
 #hack vision object that  tracks a pipe in the old simulator
-class OldSimulatorHackPipe(VisionObject):
+class PipeVisionObject(VisionObject):
     def __init__(self,oldStatePtr):
-        super(OldSimulatorHackPipe,self).__init__()
+        super(PipeVisionObject,self).__init__()
         oldStatePtr.queuedEventHub.subscribeToType(vision.EventType.PIPE_FOUND,self.callback)
         oldStatePtr.queuedEventHub.subscribeToType(vision.EventType.PIPE_LOST,self.seeit)
 
@@ -64,6 +64,7 @@ class OldSimulatorHackPipe(VisionObject):
             
     def seeit(self,event):
         self.seen = False
+
         
 #checks if a vision object is in the range specified
 class ObjectInVisionRangeQuery(object):
@@ -82,18 +83,36 @@ class ObjectInVisionRangeQuery(object):
         return self._obj.seen and ((abs(obj.x - self._x_center) <= self._x_range) and (abs(obj.y - self._y_center) <= self._y_range) and (abs(obj.range - self._range_center) <= self._range_range))
         
 
-#this class transforms a query into a  query which only becomes false if the the query has only returned false under a certain amount of time(such that all queries made in that time returned true, does not account for queries that weren't actually made)
-#this effectively introduces a delay into a query, it is useful for dealing with things like vision where an object might disappear for 1 or 2 frames, but then reappear immediately afterwards
-#TLDR; this query returns if "query" was true in the last "timeout" seconds, as long as you call it continously
+# this class transforms a query into a  query which only becomes false if the the query has 
+# only returned false under a certain amount of time(such that all queries made in that time 
+# returned true, does not account for queries that weren't actually made)
+# this effectively introduces a delay into a query, it is useful for dealing with things like 
+# vision where an object might disappear for 1 or 2 frames, but then reappear immediately afterwards
+# TLDR; this query returns if "query" was true in the last "timeout" seconds, as long as you call it continously
 class hasQueryBeenFalse(object):
     def __init__(self, timeout, query):
-        self._timer(timeout)
+        self._query = query
+        self._timer = Timer(timeout)
 
     def query(self):
         #if query true, reset timer and return true
-        if(query()):
+        if(self._query()):
             self._timer.reset()
             return True
         else:
             #if query false return if the timer has triggered yet
+            return self._timer.check()
+
+class hasQueryBeenTrue(object):
+    def __init__(self, timeout, query):
+        self._query = query
+        self._timer = Timer(timeout)
+
+    def query(self):
+        #if query false, reset timer and return true
+        if(not self._query()):
+            self._timer_reset()
+            return True
+        else:
+            #if query true return if the timer as triggered yet
             return self._timer.check()
