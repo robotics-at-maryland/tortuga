1c2fa39b6997526ff004337c2a86f3f9ba79991d
Stephen C <stphnchristian@gmail.com>
2014-07-28 23:37:02 -0700

    Added new config files for qualifying run.

--- packages/python/ram/ai/new/mainState.py
+++ packages/python/ram/ai/new/mainState.py
@@ -21,6 +21,11 @@ MACHINE_COMPLETE = core.declareEventType('MACHINE_COMPLETE')
 TERMINATE = core.declareEventType('TERMINATE')
 COMPLETE = core.declareEventType('COMPLETE')
 
+class Intro(state.State):
+    @staticmethod
+    def transitions():
+        return {'Start' : Body}
+
 class Body(oldState.State):
     """
     Just like the body of an essay I contain the meat of this routine
