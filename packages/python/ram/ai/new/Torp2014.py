90b430ef81d2d6f0f864b2591c02efe1e5a721d2
erudnick-cohen <erudnick@umd.edu>
2014-08-01 11:18:43 -0400

    enabled forwards center to use constraints, made torpedos use this constraint to enforce a limit on time spent centering

--- packages/python/ram/ai/new/Torp2014.py
+++ packages/python/ram/ai/new/Torp2014.py
@@ -61,12 +61,14 @@ class TorpedoMachine(stateMachine.StateMachine):
         holeSearchDist = configNode.get('holeSearchDist',1)
         squareCenteringRange = configNode.get('squareCenteringRange',30) 
         squareCenteringRangeBound = configNode.get('squareCenteringRangeBound', 50)
-        squareCenteringXYBound = configNode.get('squareCenteringXYBound', .1)
+        squareCenteringXYBound = configNode.get('squareCenteringXYBound', .2)
         holeCenteringRange = configNode.get('holeCenteringRange', 50)
-        holeCenteringRangeBound = configNode.get('holeCenteringRangeBound', 8)
+        holeCenteringRangeBound = configNode.get('holeCenteringRangeBound', 12)
         holeCenteringXYBound = configNode.get('holeCenteringXYBound', .1) 
         reverseRefindDistance = configNode.get('reverseRefindDistance', 1)
+        holeSearchDuration  = configNode.get('holeSearchDuration', 45)
         
+
     #later on, put code in here that picks what torpedos we're after
     #for now, we're going buoy thunting
         #not a hack anymore, this ones real!
@@ -98,12 +100,16 @@ class TorpedoMachine(stateMachine.StateMachine):
         holePassCount = self.addState('holePassCount', utilStates.PassCounter('holeSwitch'))
         holeSwitch = self.addState('holeSwitch', utilStates.Switch('hole1Search','hole2Search',holePassCount.getPassChecker(2)))
         hole1Search = self.addState('hole1Search', search.ForwardsSearchPattern(holeSearchDist, hole1.isSeen,'hole1Center', 'backUp'))
-        hole1Center = self.addState('hole1Center', approach.ForwardsCenter(hole1, 'fireLeft', 'backUp', holeCenteringRange, holeCenteringXYBound, holeCenteringXYBound, holeCenteringRangeBound))
+        #competition day 4, made hole centering have timeout and just fire anyway when the state fails
+        self.timer = utilClasses.Timer(holeSearchDuration)
+        hole1Center = self.addState('hole1Center', approach.ForwardsCenter(hole1, 'fireLeft', 'fireLeft', holeCenteringRange, holeCenteringXYBound, holeCenteringXYBound, holeCenteringRangeBound), None, self.timer.check)
+        hole1Center.setEnterCallback(self.timer.reset)
         fireLeft = self.addState('fireLeft', FireLeft('backUp'))
         backUp = self.addState('backUp', motion.Forward(-reverseRefindDistance))
         backUp.setTransition('next', 'holePassCount')
         hole2Search = self.addState('hole2Search', search.ForwardsSearchPattern(holeSearchDist, hole1.isSeen,'hole2Center', 'backUp'))
-        hole2Center = self.addState('hole2Center', approach.ForwardsCenter(hole1, 'fireRight', 'backUp', holeCenteringRange, holeCenteringXYBound, holeCenteringXYBound, holeCenteringRangeBound))
+        hole2Center = self.addState('hole2Center', approach.ForwardsCenter(hole1, 'fireRight', 'fireRight', holeCenteringRange, holeCenteringXYBound, holeCenteringXYBound, holeCenteringRangeBound,None, self.timer.check))
+        hole2Center.setEnterCallback(self.timer.reset)
         fireRight = self.addState('fireRight', FireRight('taskSuccess'))
     #Now that manipulator is dropped, this probably just needs to move to the side to prepare for the sonar task
     
